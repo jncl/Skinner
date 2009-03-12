acce1b219954383d187f05d7a936fba5837356e3
Jncl <jncl@mac.com>
2009-03-12 09:48:59 +0000

    Updated AutoProfit skin Obsoleted AutoProfitX skin

--- AddonSkins/AutoProfit.lua
+++ AddonSkins/AutoProfit.lua
@@ -2,7 +2,10 @@
 function Skinner:AutoProfit()
 	if not self.db.profile.MerchantFrames then return end
 
-	self:moveObject(TreasureModel, nil, nil, "+", 28)
-	self:moveObject(AutosellButton, nil, nil, "+", 28)
+	self:moveObject(AutoProfit_SellButton, nil, nil, "+", 28)
 
+	if self.db.profile.Tooltips.skin then
+		if self.db.profile.Tooltips.style == 3 then AutoProfit_Tooltip:SetBackdrop(self.backdrop) end
+		self:skinTooltip(AutoProfit_Tooltip)
+	end
 end
