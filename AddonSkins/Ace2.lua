28da6418969fe2bc6d7f1d27f86b08c0efa6ca82
Jncl <jncl@mac.com>
2009-03-23 13:45:38 +0000

    Updated Ace2 skin

--- AddonSkins/Ace2.lua
+++ AddonSkins/Ace2.lua
@@ -1,13 +1,15 @@
 
 function Skinner:Ace2()
 
-	-- Skin the AceAddon Donation Frame
-	self:SecureHook(LibStub("AceAddon-2.0").prototype, "OpenDonationFrame", function()
-		self:keepFontStrings(AceAddon20FrameScrollFrame)
-		self:skinScrollBar(AceAddon20FrameScrollFrame)
-		self:applySkin(AceAddon20Frame)
-		self:Unhook(LibStub("AceAddon-2.0").prototype, "OpenDonationFrame")
-	end)
+	if LibStub("AceAddon-2.0").prototype.OpenDonationFrame then
+		-- Skin the AceAddon Donation Frame
+		self:SecureHook(LibStub("AceAddon-2.0").prototype, "OpenDonationFrame", function()
+			self:keepFontStrings(AceAddon20FrameScrollFrame)
+			self:skinScrollBar(AceAddon20FrameScrollFrame)
+			self:applySkin(AceAddon20Frame)
+			self:Unhook(LibStub("AceAddon-2.0").prototype, "OpenDonationFrame")
+		end)
+	end
 	
 	-- Skin the AceAddon About Frame
 	self:SecureHook(LibStub("AceAddon-2.0").prototype, "PrintAddonInfo", function()
