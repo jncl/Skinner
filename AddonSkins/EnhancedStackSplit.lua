86f82536a0511ef559be94ba28b473bae644b281
Jncl <jncl@mac.com>
2009-05-12 17:51:50 +0100

    Updated EnhancedStackSplit skin

--- AddonSkins/EnhancedStackSplit.lua
+++ AddonSkins/EnhancedStackSplit.lua
@@ -2,21 +2,13 @@
 function Skinner:EnhancedStackSplit()
 	if not self.db.profile.StackSplit then return end
 
-	StackSplitFrame:SetHeight(StackSplitFrame:GetHeight() + 50)
+	-- resize the StackSplitFrame skin Frame
+	self.skinFrame[StackSplitFrame]:SetPoint("BOTTOMRIGHT", StackSplitFrame, "BOTTOMRIGHT", 0, -24)
+
 	self:keepFontStrings(EnhancedStackSplitTopTextureFrame)
 	self:keepFontStrings(EnhancedStackSplitBottomTextureFrame)
-	self:moveObject(EnhancedStackSplitBottomTextureFrame, "-", 7, "+", 23)
 	self:keepFontStrings(EnhancedStackSplitAutoTextureFrame)
-	self:moveObject(StackSplitText, nil, nil, "+", 16)
-	self:moveObject(StackSplitLeftButton, "+", 5, "+", 16)
-	self:moveObject(StackSplitRightButton, "-", 5, "+", 16)
-	self:moveObject(EnhancedStackSplitAuto1Button, nil, nil, "+", 24)
-
-	self:SecureHook("OpenStackSplitFrame", function(...)
---		self:Debug("OSSF")
-		self:moveObject(StackSplitOkayButton, nil, nil, "+", 24)
-		self:moveObject(StackSplitCancelButton, nil, nil, "+", 24)
-	end)
+	-- hide the StackSplit amount when in Auto Mode
 	self:SecureHook("EnhancedStackSplit_ModeSettings", function(mode)
 		if mode == 3 then StackSplitText:Hide()
 		else StackSplitText:Show() end
