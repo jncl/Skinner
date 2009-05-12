d72f05680a1fce70225633b2f523b4458e012f4d
Jncl <jncl@mac.com>
2009-05-12 17:50:47 +0100

    Updated AuctionFilterPlus skin

--- AddonSkins/AuctionFilterPlus.lua
+++ AddonSkins/AuctionFilterPlus.lua
@@ -1,16 +1,6 @@
 
 function Skinner:AuctionFilterPlus()
 
-	self:SecureHook("AuctionFilterPlus_OnEvent", function()
-		self:moveObject(afp_FlyoutButton, "-", 8, "+", 8)
-		self:Unhook("AuctionFilterPlus_OnEvent")
-	end)
-
-	self:keepFontStrings(afp_FlyoutFrame)
-	self:keepFontStrings(BrowseBuyoutSort)
-	self:applySkin(BrowseBuyoutSort)
-	self:keepFontStrings(BrowseNameSort)
-	self:applySkin(BrowseNameSort)
-	self:applySkin(afp_FlyoutFrame)
+	self:addSkinFrame{obj=afp_FlyoutFrame, kfs=true}
 
 end
