34650f7e0258fa4c015420ded5317c14e6107428
Jncl <jncl@mac.com>
2009-05-12 17:52:48 +0100

    Updated epgp skin

--- AddonSkins/epgp.lua
+++ AddonSkins/epgp.lua
@@ -6,50 +6,37 @@ function Skinner:epgp()
 
 	local function skinEPGPUI()
 	
-		Skinner:keepFontStrings(EPGPFrame)
-		EPGPFrame:SetWidth(EPGPFrame:GetWidth() * Skinner.FxMult)
-		EPGPFrame:SetHeight(EPGPFrame:GetHeight() * Skinner.FyMult)
-		Skinner:moveObject(Skinner:getRegion(EPGPFrame, 6), nil, nil, "+", 10) -- title text
-		Skinner:moveObject(Skinner:getChild(EPGPFrame, 1), "+", 33, "+", 9) -- close button
 		Skinner:skinSlider(EPGPScrollFrameScrollBar)
 		EPGPScrollFrameScrollBarBorder:SetAlpha(0)
+		
 		local sf = Skinner:getChild(EPGPFrame, 6) -- standings frame
-		Skinner:moveObject(Skinner:getChild(sf, 1), "-", 10, "-", 8) -- mass EP award button
-		Skinner:moveObject(Skinner:getChild(sf, 2), "-", 10, "-", 8) -- log button
 		local tf = Skinner:getChild(sf, 4) -- table frame
 		-- tabs
 		for _, v in pairs(tf.headers) do
 			Skinner:keepRegions(v, {5, 6}) -- N.B. regions 5 & 6 are highlight/text
-			v:SetHeight(v:GetHeight() - 5)
-			if i == 1 then Skinner:moveObject(v, nil, nil, "+", 20) end
-			Skinner:applySkin(v)
+			Skinner:applySkin{obj=v}
 		end
-		Skinner:applySkin(EPGPFrame)
+		Skinner:addSkinFrame{obj=EPGPFrame, kfs=true, x1=10, y1=-12, x2=-33, y2=71}
 		
 		-- Side Frame
-		Skinner:moveObject(EPGPSideFrame, "+", 32, nil, nil)
-		EPGPSideFrame:SetFrameLevel(EPGPSideFrame:GetFrameLevel() + 5)
-		Skinner:keepFontStrings(EPGPSideFrame)
-		Skinner:keepFontStrings(EPGPSideFrameGPControlDropDown)
-		Skinner:skinEditBox(EPGPSideFrameGPControlEditBox, {9})
-		Skinner:keepFontStrings(EPGPSideFrameEPControlDropDown)
-		Skinner:skinEditBox(EPGPSideFrameEPControlOtherEditBox, {9})
-		Skinner:skinEditBox(EPGPSideFrameEPControlEditBox, {9})
-		Skinner:applySkin(EPGPSideFrame, true)
+		Skinner:skinDropDown{obj=EPGPSideFrameGPControlDropDown}
+		Skinner:skinDropDown{obj=EPGPSideFrameEPControlDropDown}
+		Skinner:skinEditBox{obj=EPGPSideFrameGPControlEditBox, regs={9}}
+		Skinner:skinEditBox{obj=EPGPSideFrameEPControlOtherEditBox, regs={9}}
+		Skinner:skinEditBox{obj=EPGPSideFrameEPControlEditBox, regs={9}}
+		Skinner:moveObject{obj=self:getRegion(EPGPSideFrame, 2), y=-6}
+		Skinner:addSkinFrame{obj=EPGPSideFrame, kfs=true, x1=3, y1=-6, x2=-5, y2=6}
 		-- Side Frame2
-		Skinner:moveObject(EPGPSideFrame2, "+", 32, nil, nil)
-		EPGPSideFrame2:SetFrameLevel(EPGPSideFrame2:GetFrameLevel() + 5)
-		Skinner:keepFontStrings(EPGPSideFrame2)
-		Skinner:keepFontStrings(EPGPSideFrame2EPControlDropDown)
-		Skinner:skinEditBox(EPGPSideFrame2EPControlOtherEditBox, {9})
-		Skinner:skinEditBox(EPGPSideFrame2EPControlEditBox, {9})
-		Skinner:applySkin(EPGPSideFrame2)
+		Skinner:skinDropDown{obj=EPGPSideFrame2EPControlDropDown}
+		Skinner:skinEditBox{obj=EPGPSideFrame2EPControlOtherEditBox, regs={9}}
+		Skinner:skinEditBox{obj=EPGPSideFrame2EPControlEditBox, regs={9}}
+		Skinner:addSkinFrame{obj=EPGPSideFrame2, kfs=true, x1=3, y1=-6, x2=-5, y2=6}
 		-- Log Frame
-		Skinner:moveObject(EPGPLogFrame, "+", 36, nil, nil)
-		EPGPLogFrame:SetFrameLevel(EPGPLogFrame:GetFrameLevel() + 5)
-		Skinner:keepFontStrings(EPGPLogFrame)
-		Skinner:applySkin(EPGPLogRecordScrollFrame:GetParent()) -- log info frame
-		Skinner:applySkin(EPGPLogFrame)
+		Skinner:addSkinFrame{obj=EPGPLogRecordScrollFrame:GetParent()}
+		Skinner:addSkinFrame{obj=EPGPLogFrame, kfs=true, x1=3, y1=-6, x2=-5, y2=2}
+		-- ExportImport Frame
+		Skinner:skinScrollBar{obj=EPGPExportScrollFrame}
+		Skinner:addSkinFrame{obj=EPGPExportImportFrame, kfs=true}
 		
 	end
 	
