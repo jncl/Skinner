6e3fd842092186fe58ba02a48ca203e1e1f76ff8
Jncl <jncl@mac.com>
2009-05-12 17:52:21 +0100

    Updated InventoryOnPar skin

--- AddonSkins/InventoryOnPar.lua
+++ AddonSkins/InventoryOnPar.lua
@@ -1,12 +1,8 @@
 
 function Skinner:InventoryOnPar()
 
--->>--	Button on Character Frame
-	self:moveObject(IOP_CPframe, "+", 35, "-", 70)
-
 -->>--	UI Frame
-	self:keepFontStrings(InventoryOnParUIFrameTableScrollFrame)
-	self:skinScrollBar(InventoryOnParUIFrameTableScrollFrame)
+	self:skinScrollBar{obj=InventoryOnParUIFrameTableScrollFrame}
 	self:keepFontStrings(InventoryOnParUIFrameTableHeaderPlayerInfo)
 	self:keepFontStrings(InventoryOnParUIFrameTableHeaderScore)
 	self:keepFontStrings(InventoryOnParUIFrameTableHeaderPlayerName)
@@ -15,33 +11,19 @@ function Skinner:InventoryOnPar()
 	self:keepFontStrings(InventoryOnParUIFrameTableHeaderPlayerGuild)
 	self:keepFontStrings(InventoryOnParUIFrameTableHeaderLastSeen)
 	self:keepFontStrings(InventoryOnParUIFrameTableHeaderIOPScore)
-	self:keepFontStrings(InventoryOnParUIFrameTable)
-	self:applySkin(InventoryOnParUIFrameTable)
-	self:keepFontStrings(InventoryOnParUIFrame)
-	self:applySkin(InventoryOnParUIFrame)
+	self:addSkinFrame{obj=InventoryOnParUIFrameTable, kfs=true}
+	self:moveObject{obj=InventoryOnParUIFrameTitleText, y=-6}
+	self:addSkinFrame{obj=InventoryOnParUIFrame, kfs=true, x1=10, x2=-10, y2=10}
 
 -->>--	PaperDoll Frame
-	IOP_PaperDollFrame:SetWidth(IOP_PaperDollFrame:GetWidth() * self.FxMult)
-	IOP_PaperDollFrame:SetHeight(IOP_PaperDollFrame:GetHeight() * self.FyMult)
-	self:moveObject(IOP_PaperDoll_ScoreText, nil, nil, "-", 20)
-	self:moveObject(IOP_PaperDollFrameCloseButton, "+", 28, "+", 8)
-	self:moveObject(IOP_PaperDoll_ItemButtonHeadSlot, "+", 10, "-", 10)
-	self:moveObject(IOP_PaperDoll_ItemScoreHeadSlot, "+", 10, "-", 10)
-	self:moveObject(IOP_PaperDoll_ItemButtonHandsSlot, "+", 10, "-", 10)
-	self:moveObject(IOP_PaperDoll_ItemScoreHandsSlot, "+", 10, "-", 10)
-	self:moveObject(IOP_PaperDoll_ItemButtonMainHandSlot, "+", 10, "-", 10)
-	self:moveObject(IOP_PaperDoll_ItemScoreMainHandSlot, "+", 10, "-", 10)
-	self:keepFontStrings(IOP_PaperDollFrame)
-	self:applySkin(IOP_PaperDollFrame)
+	self:addSkinFrame{obj=IOP_PaperDollFrame, kfs=true, x1=10, y1=-12, x2=-33, y2=12}
 
 -->>--	Option Frame
-	self:moveObject(InventoryOnParOptionFrameTitleText, nil, nil, "-", 6)
-	self:keepFontStrings(InventoryOnParOptionDropDownUpdateMinutes)
-	self:skinEditBox(InventoryOnParOptionMinLevel, {9})
-	self:skinEditBox(InventoryOnParOptionMaxLevel, {9})
-	self:skinEditBox(InventoryOnParOptionDateFormat, {9})
-	self:moveObject(InventoryOnParOptionFrameButtonClose, nil, nil, "-", 6)
-	self:keepFontStrings(InventoryOnParOptionFrame)
-	self:applySkin(InventoryOnParOptionFrame)
+	self:skinDropDown{obj=InventoryOnParOptionDropDownUpdateMinutes}
+	self:skinEditBox{obj=InventoryOnParOptionMinLevel, regs={9}}
+	self:skinEditBox{obj=InventoryOnParOptionMaxLevel, regs={9}}
+	self:skinEditBox{obj=InventoryOnParOptionDateFormat, regs={9}}
+	self:moveObject{obj=InventoryOnParOptionFrameTitleText, y=-6}
+	self:addSkinFrame{obj=InventoryOnParOptionFrame, kfs=true}
 
 end
