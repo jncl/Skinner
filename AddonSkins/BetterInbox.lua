7b1769065ceaf058a42d15c7489835f9b35a8c9e
Jncl <jncl@mac.com>
2009-05-12 17:51:13 +0100

    Updated BetterInbox skin

--- AddonSkins/BetterInbox.lua
+++ AddonSkins/BetterInbox.lua
@@ -10,36 +10,9 @@ function Skinner:BetterInbox(LoD)
 
 	local function skinBIb()
 
-		-- move heading text
-		bib.summary.numitems:ClearAllPoints()
-		bib.summary.numitems:SetPoint( "TOPLEFT", InboxFrame, "TOPLEFT", 90, -23)
-		bib.summary.numitemsText:ClearAllPoints()
-		bib.summary.numitemsText:SetPoint( "LEFT", bib.summary.numitems, "RIGHT", 5, 0)
-		bib.summary.moneyText:ClearAllPoints()
-		bib.summary.moneyText:SetPoint( "TOPLEFT", InboxFrame, "TOPLEFT", 111, -36)
-		bib.summary.money:ClearAllPoints()
-		bib.summary.money:SetPoint( "LEFT", bib.summary.moneyText, "RIGHT", 5, 0)
-		bib.summary.soonText:ClearAllPoints()
-		bib.summary.soonText:SetPoint( "TOPLEFT", InboxFrame, "TOPLEFT", 115, -49)
-		bib.summary.soon:ClearAllPoints()
-		bib.summary.soon:SetPoint( "LEFT", bib.summary.soonText, "RIGHT", 5, 0)
-		bib.summary.codText:ClearAllPoints()
-		bib.summary.codText:SetPoint( "TOPLEFT", InboxFrame, "TOPLEFT", 130, -62)
-		bib.summary.cod:ClearAllPoints()
-		bib.summary.cod:SetPoint( "LEFT", bib.summary.codText, "RIGHT", 5, 0)
-
-		Skinner:skinScrollBar(bib.scrollframe)
 		bib.scrollframe.t1:SetAlpha(0)
 		bib.scrollframe.t2:SetAlpha(0)
-		Skinner:moveObject(bib.scrollframe, "-", 5, "+", 20)
-		bib.scrollframe:SetHeight(bib.scrollframe:GetHeight() + 10)
-		Skinner:moveObject(bib.scrollframe.entries[1], "-", 5, "+", 20)
-		if not Skinner.skinned[bib.scrollframe.dropdown] then
-			Skinner:skinDropDown(bib.scrollframe.dropdown.dropdown)
-			Skinner:applySkin(bib.scrollframe.dropdown.pullout.frame)
-		end
-		Skinner:moveObject(bib.scrollframe.dropdown.frame, "-", 10, "-", 2)
-		Skinner:moveObject(BetterInboxCancelButton, "-", 7, "-", 5)
+		Skinner:skinScrollBar{obj=bib.scrollframe}
 
 	end
 
