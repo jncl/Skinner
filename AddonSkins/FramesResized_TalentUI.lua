f3293e65bc20fcd11f13f7a09caf2d8add3f8548
Jncl <jncl@mac.com>
2009-05-13 19:22:14 +0100

    Updated FramesResized_TalentUI

--- AddonSkins/FramesResized_TalentUI.lua
+++ AddonSkins/FramesResized_TalentUI.lua
@@ -1,14 +1,7 @@
-﻿function Skinner:FramesResized_TalentUI()
+﻿
+function Skinner:FramesResized_TalentUI()
 
-	PlayerTalentFrame:SetHeight(PlayerTalentFrame:GetHeight() + 26)
-
-	PlayerTalentFrameBackgroundBottomLeft:SetHeight(384)
-	PlayerTalentFrameBackgroundBottomRight:SetHeight(384)
-	
-	self:removeRegions(PlayerTalentFrame_MidTextures)
+	self:removeRegions(TalentFrame_MidTextures)
 	self:removeRegions(PlayerTalentFrameScrollFrame_MidTextures)
-
-	self:moveObject(PlayerTalentFrameTalentPointsText, "-", 10, "+", 107)
-	self:moveObject(PlayerTalentFrameCancelButton, "-", 18, "+", 65)
 
 end
