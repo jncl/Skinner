e92fd2e17435ef48601a9bdacc38dc292e0cadc7
Jncl <jncl@mac.com>
2009-05-22 17:41:30 +0100

    Updated LightHeaded skin

--- AddonSkins/LightHeaded.lua
+++ AddonSkins/LightHeaded.lua
@@ -1,6 +1,7 @@
 
 function Skinner:LightHeaded()
-
+	if not self.db.profile.QuestLog.skin then return end
+	
 	self:skinEditBox{obj=LightHeadedSearchBox, regs={9}}
 	self:skinScrollBar{obj=LightHeadedScrollFrame}
 	self:addSkinFrame{obj=LightHeadedFrame, kfs=true, x1=2, y1=1, x2=2, y2=-2}
