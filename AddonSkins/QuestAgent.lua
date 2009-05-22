5488567f359a5519ccdf26bece144d7a1cb2ba2c
Jncl <jncl@mac.com>
2009-05-22 17:40:56 +0100

    Updated QuestAgent skin

--- AddonSkins/QuestAgent.lua
+++ AddonSkins/QuestAgent.lua
@@ -1,46 +1,37 @@
 
 function Skinner:QuestAgent()
-
-	self:SecureHook(QuestAgent, "ShowQuest", function(this, q, name)
-		self:Debug("QuestAgent_ShowQuest")
+	if not self.db.profile.QuestLog.skin then return end
+	
+	local function chgTextCols()
+		
 		for i = 1, 10 do
 			local r, g, b, a = _G["QuestAgent_QuestLogObjective"..i]:GetTextColor()
-			_G["QuestAgent_QuestLogObjective"..i]:SetTextColor(self.BTr - r, self.BTg - g, self.BTb)
+			_G["QuestAgent_QuestLogObjective"..i]:SetTextColor(Skinner.BTr - r, Skinner.BTg - g, Skinner.BTb)
 		end
-		local r, g, b, a = QuestAgent_QuestLogRequiredMoneyText:GetTextColor()
-		QuestAgent_QuestLogRequiredMoneyText:SetTextColor(self.BTr - r, self.BTg - g, self.BTb)
-		QuestAgent_QuestLogRewardTitleText:SetTextColor(self.HTr, self.HTg, self.HTb)
+		QuestAgent_QuestLogRewardTitleText:SetTextColor(Skinner.HTr, Skinner.HTg, Skinner.HTb)
+		QuestAgent_QuestLogRequiredMoneyText:SetTextColor(Skinner.BTr, Skinner.BTg, Skinner.BTb)
+			
+	end
+	
+	self:SecureHook(QuestAgent, "ShowQuest", function(this, q, name)
+		chgTextCols()
 		QuestAgent_QuestLogItemChooseText:SetTextColor(self.BTr, self.BTg, self.BTb)
 		QuestAgent_QuestLogItemReceiveText:SetTextColor(self.BTr, self.BTg, self.BTb)
 		QuestAgent_QuestLogSpellLearnText:SetTextColor(self.BTr, self.BTg, self.BTb)
 	end)
 	
-	self:keepFontStrings(QuestAgent_QuestLogFrame)
-	QuestAgent_QuestLogFrame:SetWidth(QuestAgent_QuestLogFrame:GetWidth() * self.FxMult)
-	QuestAgent_QuestLogFrame:SetHeight(QuestAgent_QuestLogFrame:GetHeight() * self.FyMult)
-	self:moveObject(QuestAgent_QuestLogTitleText, nil, nil, "+", 10)
-	self:removeRegions(QuestAgent_QuestLogDetailScrollFrame)
-	self:skinScrollBar(QuestAgent_QuestLogDetailScrollFrame)
-	self:moveObject(QuestAgent_QuestLogDetailScrollFrame, "-", 10, "+", 40)
-	self:moveObject(QuestAgent_QuestLogFrameCloseButton, "+", 30, "+", 8)
-	self:moveObject(QuestAgent_QuestFrameExitButton, "+", 33, "-", 44)
-	
 	QuestAgent_QuestLogQuestTitle:SetTextColor(self.HTr, self.HTg, self.HTb)
 	QuestAgent_QuestLogObjectivesText:SetTextColor(self.BTr, self.BTg, self.BTb)
 	QuestAgent_QuestLogTimerText:SetTextColor(self.BTr, self.BTg, self.BTb)
-	for i = 1, 10 do
-   		local r, g, b, a = _G["QuestAgent_QuestLogObjective"..i]:GetTextColor()
-   		_G["QuestAgent_QuestLogObjective"..i]:SetTextColor(self.BTr - r, self.BTg - g, self.BTb)
-   	end
-	QuestAgent_QuestLogRequiredMoneyText:SetTextColor(self.BTr, self.BTg, self.BTb)
 	QuestAgent_QuestLogSuggestedGroupNum:SetTextColor(self.HTr, self.HTg, self.HTb)
 	QuestAgent_QuestLogDescriptionTitle:SetTextColor(self.HTr, self.HTg, self.HTb)
 	QuestAgent_QuestLogQuestDescription:SetTextColor(self.BTr, self.BTg, self.BTb)
 	QuestAgent_QuestLogPlayerTitleText:SetTextColor(self.BTr, self.BTg, self.BTb)
-	QuestAgent_QuestLogRewardTitleText:SetTextColor(self.HTr, self.HTg, self.HTb)
+	chgTextCols()
 	
-	self:applySkin(QuestAgent_QuestLogFrame)
-
-	self:applySkin(QuestAgentUpdateDialog)
-
+	self:skinScrollBar{obj=QuestAgent_QuestLogDetailScrollFrame}
+	self:addSkinFrame{obj=QuestAgent_QuestLogFrame, kfs=true, x1=10, y1=-11, x2=-33, y2=48}
+	
+	self:addSkinFrame{obj=QuestAgentUpdateDialog}
+	
 end
