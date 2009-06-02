431e48abba56072d5e5ca21d2650fbd703832d77
Jncl <jncl@mac.com>
2009-06-02 11:18:26 +0100

    Updated Tablet skin

--- AddonSkins/Tablet.lua
+++ AddonSkins/Tablet.lua
@@ -4,12 +4,11 @@ function Skinner:Tablet()
 
 	local function skinTablet()
 
-		if not Skinner.db.profile.TrackerFrame then
+		if not Skinner.db.profile.TrackerFrame.skin then
 			-- Check for the QuestsFu_Tracker
 			local qftinfo = LibStub("Tablet-2.0", true).registry['QuestsFu_Tracker']
 			if qftinfo and qftinfo.tooltip then
 				local tt = qftinfo.tooltip
---				Skinner:Debug("ST: [%s, %s]", tt, tt:GetName())
 				tt:SetBackdrop(nil)
 				Skinner.skinned[tt] = true
 				tt.noFH = true
@@ -19,7 +18,6 @@ function Skinner:Tablet()
 		if Tablet20Frame then
 			local frame = Tablet20Frame
 			if not Skinner.skinned[frame] then
---				Skinner:Debug("Tablet20Frame")
 				Skinner:applySkin(frame)
 				-- hook these to stop the Backdrop colours from being changed
 				Skinner:RawHook(frame, "SetBackdropColor", function() end, true)
@@ -31,7 +29,6 @@ function Skinner:Tablet()
 		while _G["Tablet20DetachedFrame"..i] do
 			local frame = _G["Tablet20DetachedFrame"..i]
 			if not Skinner.skinned[frame] then
---				Skinner:Debug("DetachedFrame: [%s, %s]", frame:GetName(), frame:GetParent():GetName() )
 				Skinner:applySkin(frame)
 				-- hook these to stop the Backdrop colours from being changed
 				Skinner:RawHook(frame, "SetBackdropColor", function() end, true)
@@ -45,11 +42,9 @@ function Skinner:Tablet()
 	end
 
 	self:SecureHook(LibStub("Tablet-2.0", true), "Open", function(tablet, parent)
---		self:Debug("TabletOpen: [%s, %s]", tablet, parent)
 		skinTablet()
 	end)
 	self:SecureHook(LibStub("Tablet-2.0", true), "Detach", function(tablet, parent)
---		self:Debug("TabletDetach: [%s, %s]", tablet, parent)
 		skinTablet()
 	end)
 
