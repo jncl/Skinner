bf04d06bd9311ed796e639cc4ee2a58dcd4cb404
Jncl <jncl@mac.com>
2009-06-03 11:38:58 +0100

    Updated TheCollector skin

--- AddonSkins/TheCollector.lua
+++ AddonSkins/TheCollector.lua
@@ -1,27 +1,14 @@
 
 function Skinner:TheCollector()
 
-	self:keepFontStrings(TheCollectorFrame)
-	self:keepFontStrings(TheCollectorFrameHeaderFrame)
-	self:moveObject(TheCollectorFrameHeaderFrameText, nil, nil, "+", 7)
 	self:getChild(TheCollectorFrame, 2):Hide() -- button in TLH corner
-	self:skinFFToggleTabs("TheCollectorFrameTab", 2)
-	self:moveObject(TheCollectorFrameTab1, nil, nil, "-", 10)
-	self:keepFontStrings(TheCollectorFrameScrollFrame)
-	self:skinScrollBar(TheCollectorFrameScrollFrame)
-	self:moveObject(TheCollectorFrameScrollFrame, nil, nil, "+", 10)
+	self:skinFFToggleTabs("TheCollectorFrameTab", 3)
+	self:skinScrollBar{obj=TheCollectorFrameScrollFrame}
 	self:getChild(TheCollectorFrameStatusBar, 1):SetAlpha(0) -- border texture
 	self:glazeStatusBar(TheCollectorFrameStatusBar, 0)
-	self:moveObject(TheCollectorFrameStatusBar, nil, nil, "+", 10)
-	self:moveObject(self:getChild(TheCollectorFrame, 4), nil, nil, "+", 10) -- close button in BRH corner
-	self:applySkin(TheCollectorFrame)
+	self:keepFontStrings(TheCollectorFrameHeaderFrame)
+	self:addSkinFrame{obj=TheCollectorFrame, kfs=true, x1=4, y1=-5, x2=-5, y2=-10}
 	
---[[
-	-- Model Frame
-	self:applySkin(TheCollectorModelFrame)
-	self:makeMFRotatable(TheCollectorModel)
-	TheCollectorModelRotateLeftButton:Hide()
-	TheCollectorModelRotateRightButton:Hide()
---]]
+	self:moveObject{obj=TheCollectorPetToggleButton, y=-2}
 	
 end
