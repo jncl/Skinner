d8708a4497f66915150bba08d58de409434c834b
Jncl <jncl@mac.com>
2009-06-29 12:30:57 +0100

    Updated Bagnon skin

--- AddonSkins/Bagnon.lua
+++ AddonSkins/Bagnon.lua
@@ -2,37 +2,40 @@
 -- and here http://wow.curse.com/downloads/details/2090/
 -- and also for the Bagnon Addon formerly known as vBagnon, found here: http://wow-en.curse-gaming.com/files/details/2090/vbagnon/ or here http://wowui.incgamers.com/ui.php?id=3197
 
+-- Updated 26.06.09
+-- vBagnon no longer is supported
+-- Now supports the newest version found on Curse
+
 function Skinner:Bagnon(LoD)
 	if not self.db.profile.ContainerFrames or self.initialized.Bagnon then return end
 	self.initialized.Bagnon = true
 
---	if Addon is LoD then it's the original one
+	--	if Addon is LoD then it's the original one
 	if LoD then
 		self:applySkin(Bagnon)
 		-- hook these to stop the Backdrop colours from being changed
 		self:RawHook(Bagnon, "SetBackdropColor", function() end, true)
 		self:RawHook(Bagnon, "SetBackdropBorderColor", function() end, true)
-
---	otherwise it's the renamed vBagnon one
+		
+	-- it's the newest version from Curse	
 	else
-		self:SecureHook(Bagnon, "CreateInventory", function()
-			self:applySkin(Bagnon.bags)
-			-- hook these to stop the Backdrop colours from being changed
-			self:RawHook(Bagnon.bags, "SetBackdropColor", function() end, true)
-			self:RawHook(Bagnon.bags, "SetBackdropBorderColor", function() end, true)
+		local Bagnon = LibStub('AceAddon-3.0'):GetAddon('Bagnon')
+		-- skin the bag frame
+		self:RawHook(Bagnon.Frame, "New", function(this, frameID)
+			self:Debug("Bagnon.Frame_New: [%s, %s]", this, frameID)
+			local frame = self.hooks[Bagnon.Frame].New(this, frameID)
+			self:applySkin(frame)
+			self:RawHook(frame, "SetBackdropColor", function() end, true)
+			self:RawHook(frame, "SetBackdropBorderColor", function() end, true)
+			return frame
+		end)
+		-- skin the Search EditBox
+		self:RawHook(Bagnon.SearchFrame, "New", function(this, ...)
+			local eb = self.hooks[Bagnon.SearchFrame].New(this, ...)
+			self:skinEditBox{obj=eb, regs={9}}
+			return eb
 		end)
 
-		self:SecureHook(Bagnon, "CreateBank", function()
-			self:applySkin(Bagnon.bank)
-			-- hook these to stop the Backdrop colours from being changed
-			self:RawHook(Bagnon.bank, "SetBackdropColor", function() end, true)
-			self:RawHook(Bagnon.bank, "SetBackdropBorderColor", function() end, true)
-		end)
-		-- skin the search edit box
-		self:SecureHook(BagnonSpot, "Show", function(this, anchor)
-			self:skinEditBox(this.frame, {9})
-			self:Unhook(BagnonSpot, "Show")
-		end)
 	end
 
 end
