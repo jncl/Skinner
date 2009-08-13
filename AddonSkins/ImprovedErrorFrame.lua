27842bc4946a0850006e6e9ac52751870be5397d
Jncl <jncl@mac.com>
2009-08-13 13:50:29 +0100

    Updated ImprovedErrorFrame skin

--- AddonSkins/ImprovedErrorFrame.lua
+++ AddonSkins/ImprovedErrorFrame.lua
@@ -4,7 +4,4 @@ function Skinner:ImprovedErrorFrame()
 	self:applySkin(ImprovedErrorFrameCloseButton)
 	self:applySkin(ImprovedErrorFrameFrame)
 
-	self:removeRegions(ScriptErrorsScrollFrameOne)
-	self:skinScrollBar(ScriptErrorsScrollFrameOne)
-
 end
