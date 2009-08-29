7fc22abbc4d01fcaff3a2210a801f6fc7b865327
Jncl <jncl@mac.com>
2009-08-29 13:37:33 +0100

    Updated Recount skin

--- AddonSkins/Recount.lua
+++ AddonSkins/Recount.lua
@@ -3,83 +3,31 @@ function Skinner:Recount()
 
 	-- Hook this to get window objects and skin them
 	self:SecureHook(Recount, "AddWindow", function(this, window)
---		self:Debug("R_AW: [%s, %s, %s, %s]", this, window, window.Title:GetText(), window.tracking)
-		if window.Title:GetText() == "Report Data - Main" then
-			self:skinEditBox(window.Whisper, {9})
-			self:moveObject(window.Whisper, "-", 6, "-", 6, window)
-		elseif window.Title:GetText() == "Reset Recount?" then
-			window:SetHeight(window:GetHeight() + 4)
+		local tText = window.Title:GetText()
+		local x1, x2, y2 = -4, 4, -4
+		if tText:find("Report Data - ") then
+			self:skinEditBox{obj=window.Whisper, regs={9}, noHeight=true}
+			window.Whisper:SetHeight(window.Whisper:GetHeight() + 6)
+			x1, x2, y2 = 0, 0, 0
+		elseif tText == "Config Recount" then
+			x1, x2, y2 = -2, 3, -2
 		end
-		self:moveObject(window.Title, "+", 10, "+", 8, window)
-		if window.CloseButton then
-			self:moveObject(window.CloseButton, nil, nil, "+", 8, window)
-		end
-		if window.DragBottomRight then
-			self:moveObject(window.DragBottomRight, "+", 5, nil, nil, window)
-		end
-		if window.DragBottomLeft then
-			self:moveObject(window.DragBottomLeft, "-", 5, nil, nil, window)
-		end
-		-- handle Realtime Frames
-		if window.tracking then
-			window:SetBackdrop(nil)
-			self:moveObject(window.Graph, nil, nil, "+", 6, window)
-			self:addSkinFrame(window)
-		else
-			self:applySkin(window)
-		end
+		self:addSkinFrame{obj=window, kfs=true, x1=x1, y1=-10, x2=x2, y2=y2}
 	end)
-	-- hook these to manage the Scroll Bar display
-	self:SecureHook(Recount, "HideScrollbarElements", function(this, name)
---		self:Debug("Recount-HSbE: [%s, %s]", this, name)
-		_G[name.."ScrollBar"]:SetBackdrop(nil)
-	end)
-	self:SecureHook(Recount, "ShowScrollbarElements", function(this, name)
---		self:Debug("Recount-SSbE: [%s, %s]", this, name)
-		self:keepFontStrings(_G[name])
-		self:skinScrollBar(_G[name])
-	end)
+
+-->>-- Main Window
+	self:addSkinFrame{obj=Recount.MainWindow, kfs=true, x1=-2, y1=-8, x2=2}
+-->>-- Detail Window
+	self:addSkinFrame{obj=Recount_DetailWindow, kfs=true, x1=-2, y1=-8, x2=3, y2=-2}
+-->>-- Graph Window
+	self:addSkinFrame{obj=Recount_GraphWindow, kfs=true, hdr=true, x1=-2, y1=-8, x2=2, y2=-2}
 
 -->>-- skin Realtime frames already created
 	for i = 1, UIParent:GetNumChildren() do
 		local obj = select(i, UIParent:GetChildren())
 		if obj:GetName() == nil and obj:IsObjectType("Frame") and obj.Graph then
---			self:Debug("Recount_RTW: [%s, %s, %s]", obj, obj.Title:GetText(), obj.id)
-			obj:SetBackdrop(nil)
-			self:moveObject(obj.Title, "+", 10, "+", 8, obj)
-			self:moveObject(obj.CloseButton, nil, nil, "+", 8, obj)
-			self:moveObject(obj.DragBottomRight, "+", 5, nil, nil, obj)
-			self:moveObject(obj.DragBottomLeft, "-", 5, nil, nil, obj)
-			self:moveObject(obj.Graph, nil, nil, "+", 6, obj)
-			self:addSkinFrame(obj)
+			self:addSkinFrame{obj=obj, kfs=true, x1=-4, y1=-10, x2=4, y2=-4}
 		end
 	end
-
--->>-- Main Window
-	Recount.MainWindow:SetBackdrop(nil)
-	self:moveObject(Recount.MainWindow.Title, nil, nil, "+", 6)
-	self:moveObject(Recount.MainWindow.RightButton, "+", 6, "+", 6)
-	self:moveObject(Recount.MainWindow.CloseButton, "+", 6, "+", 8)
-	self:moveObject(Recount.MainWindow.DragBottomRight, "+", 5, nil, nil)
-	self:moveObject(Recount.MainWindow.DragBottomLeft, "-", 5, nil, nil)
-	if Recount.db.profile.MainWindow.ShowScrollbar then
-		self:keepFontStrings(Recount.MainWindow.ScrollBar)
-		self:skinScrollBar(Recount.MainWindow.ScrollBar)
-	end
--- 	Create a frame to mirror the MainWindow and skin that, make it wider so the bars fit on it
-	self:addSkinFrame(Recount.MainWindow)
-
--->>-- Detail Window
-	self:moveObject(Recount_DetailWindow.Title, nil, nil, "+", 8, Recount_DetailWindow)
-	self:moveObject(Recount_DetailWindow.CloseButton, nil, nil, "+", 8, Recount_DetailWindow)
-	self:moveObject(Recount_DetailWindow.LeftButton, nil, nil, "+", 8, Recount_DetailWindow)
-	self:applySkin(Recount_DetailWindow)
--->>-- Graph Window
-	self:moveObject(Recount_GraphWindow.Title, nil, nil, "+", 8, Recount_DetailWindow)
-	self:keepFontStrings(Recount_GraphWindow_ScrollBar1)
-	self:skinScrollBar(Recount_GraphWindow_ScrollBar1)
-	self:keepFontStrings(Recount_GraphWindow_ScrollBar2)
-	self:skinScrollBar(Recount_GraphWindow_ScrollBar2)
-	self:applySkin(Recount_GraphWindow, true)
 
 end
