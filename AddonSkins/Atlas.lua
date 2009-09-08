d36355e874bb87d04847a3c7fc85a62db806b6ae
Jncl <jncl@mac.com>
2009-09-09 00:50:25 +0100

    Updated Atlas skin

--- AddonSkins/Atlas.lua
+++ AddonSkins/Atlas.lua
@@ -1,12 +1,26 @@
 
 function Skinner:Atlas()
 
+	-- this is used to add a texture to the dropdown
+	local function textureDD(dd, opt)
+
+		self:keepFontStrings(dd)
+		if self.db.profile.TexturedDD then
+			dd.ddTex = dd:CreateTexture(nil, "BORDER")
+			dd.ddTex:SetTexture(Skinner.itTex)
+			dd.ddTex:ClearAllPoints()
+			dd.ddTex:SetPoint("TOPLEFT", dd, "TOPLEFT", 20, opt and -5 or -4)
+			dd.ddTex:SetPoint("BOTTOMRIGHT", dd, "BOTTOMRIGHT", opt and -24 or -18, opt and 8 or 10)
+		end
+
+	end
+
 -->>--	AtlasFrame
 	self:moveObject(self:getRegion(AtlasFrame, 6), nil, nil, "+", 8)
 	self:moveObject(self:getRegion(AtlasFrame, 7), nil, nil, "+", 8)
 	self:moveObject(AtlasFrameCloseButton, "-", 4, "+", 8)
-	self:skinDropDown(AtlasFrameDropDownType)
-	self:skinDropDown(AtlasFrameDropDown)
+	textureDD(AtlasFrameDropDownType)
+	textureDD(AtlasFrameDropDown)
 	self:removeRegions(AtlasFrame, {1,2,3,4,5}) -- N.B. other regions are text or Map Textures
 	self:skinEditBox(AtlasSearchEditBox, {9})
 	self:keepFontStrings(AtlasScrollBar)
@@ -17,7 +31,7 @@ function Skinner:Atlas()
 
 -->>--	AtlasOptionsFrame
 	self:keepRegions(AtlasOptionsFrame, {11})
-	self:skinDropDown(AtlasOptionsFrameDropDownCats)
+	textureDD(AtlasOptionsFrameDropDownCats, true)
 	self:applySkin(AtlasOptionsFrame, true)
 
 end
