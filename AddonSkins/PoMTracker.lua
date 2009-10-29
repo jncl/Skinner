2421c127b79bcd01f0ab0bdc6ab7f5f3bac7d4e0
Jncl <jncl@mac.com>
2009-10-29 00:40:00 +0000

    Skinned AnnouncIt, MacroBrokerGUI & PoMTracker

---
+++ AddonSkins/PoMTracker.lua
@@ -0,0 +1,13 @@
+
+function Skinner:PoMTracker()
+
+	self:skinButton{obj=PoMOptionFrame_CloseButton, cb=true}
+	self:addSkinFrame{obj=PoMOptionFrame, y1=-7, x2=-7}
+	
+	self:addSkinFrame{obj=pomtracker2}
+	
+	self:glazeStatusBar(pomtrackerstatusBar, 0)
+	
+	self:skinButton{obj=pomtracker3_Button1, x1=-2, y1=1, x2=1, y2=-1}
+
+end
