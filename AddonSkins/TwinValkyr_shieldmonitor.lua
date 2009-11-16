dbb1f5ea068bc8d68bb99e491d436d834cce3a6b
Jncl <jncl@mac.com>
2009-11-16 14:26:03 +0000

    Updated TwinValkyr_shieldmonitor skin

--- AddonSkins/TwinValkyr_shieldmonitor.lua
+++ AddonSkins/TwinValkyr_shieldmonitor.lua
@@ -4,12 +4,12 @@ function Skinner:TwinValkyr_shieldmonitor()
 	self:addSkinFrame{obj=TWcolishieldmini}
 	-- Options frame
 	self:addSkinFrame{obj=TwinValmenu, x=5, y1=-5, x2=-5, y2=4}
-	self:skinButton{obj=TwinValmenu_Button1, x1=-1, y1=0, x2=1, y2=1}
+	self:skinButton{obj=TwinValmenu_Button1, x1=-1, y1=0, x2=1, y2=1, ty=0}
 	self:skinButton{obj=TwinValmenu_Button2}
 	self:skinButton{obj=TwinValmenu_Button3}
-	self:skinEditBox{obj=TwinValmenu_width1}
-	self:moveObject{obj=TwinValmenu_width1, y=10}
-	self:skinEditBox{obj=TwinValmenu_heigh1}
-	self:moveObject{obj=TwinValmenu_heigh1, y=10}
+	self:skinEditBox{obj=TwinValmenu_width1, noHeight=true}
+	self:adjWidth{obj=TwinValmenu_width1, adj=3}
+	self:skinEditBox{obj=TwinValmenu_heigh1, noHeight=true}
+	self:adjWidth{obj=TwinValmenu_heigh1 , adj=3}
 	
 end
