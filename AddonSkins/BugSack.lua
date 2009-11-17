54aad6b7ccf51c4cde9d42af4bd10d04eb7a83db
jncl <jncl@mac.com>
2009-11-17 16:59:00 +0000

    Updated BugSack skin

--- AddonSkins/BugSack.lua
+++ AddonSkins/BugSack.lua
@@ -1,8 +1,19 @@
 
 function Skinner:BugSack()
 
-	self:skinScrollBar{obj=BugSackFrameScroll}
-	self:skinAllButtons(BugSackFrame)
-	self:addSkinFrame{obj=BugSackFrame, kfs=true, y2=8}
+	-- handle new version
+	local bs = LibStub("AceAddon-3.0"):GetAddon("BugSack", true)
+	if bs and bs.UpdateSack then
+		self:SecureHook(bs, "UpdateSack", function()
+			self:skinScrollBar{obj=BugSackFrameScroll2}
+			self:skinAllButtons(BugSackFrame2)
+			self:addSkinFrame{obj=BugSackFrame2}
+			self:Unhook(bs, "UpdateSack")
+		end)
+	else
+		self:skinScrollBar{obj=BugSackFrameScroll}
+		self:skinAllButtons(BugSackFrame)
+		self:addSkinFrame{obj=BugSackFrame, kfs=true, y2=8}
+	end
 
 end
