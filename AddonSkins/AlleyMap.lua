5bc7dcfb534924e7b86e8933a900ce7a8fc661e7
Jncl <jncl@mac.com>
2010-02-03 22:40:00 +0000

    Skinned AlleyMap & BindPad

---
+++ AddonSkins/AlleyMap.lua
@@ -0,0 +1,46 @@
+
+function Skinner:AlleyMap()
+
+	local function sizeUp()
+	
+		self.skinFrame[WorldMapFrame]:SetParent(WorldMapFrame)
+		self.skinFrame[WorldMapFrame]:SetFrameStrata("LOW")
+		self.skinFrame[WorldMapFrame]:ClearAllPoints()
+		self.skinFrame[WorldMapFrame]:SetPoint("TOPLEFT", WorldMapFrame, "TOPLEFT", 255, -95)
+		self.skinFrame[WorldMapFrame]:SetPoint("BOTTOMRIGHT", WorldMapFrame, "BOTTOMRIGHT", -255, 95)
+		
+	end
+	local function sizeDown()
+
+		local kids = {WorldMapDetailFrame:GetChildren()}
+		for _, child in pairs(kids) do
+			child:SetAlpha(0)
+		end
+		kids = nil
+	
+		self.skinFrame[WorldMapFrame]:SetParent(WorldMapDetailFrame) -- handle frame movement
+		self.skinFrame[WorldMapFrame]:SetFrameStrata("LOW")
+		self.skinFrame[WorldMapFrame]:ClearAllPoints()
+		self.skinFrame[WorldMapFrame]:SetPoint("TOPLEFT", WorldMapDetailFrame, "TOPLEFT", -5, 4)
+		self.skinFrame[WorldMapFrame]:SetPoint("BOTTOMRIGHT", WorldMapDetailFrame, "BOTTOMRIGHT", 4, -4)
+		
+	end
+	self:SecureHook("WorldMap_ToggleSizeUp", function()
+		sizeUp()
+		self:moveObject{obj=WorldMapFrameCloseButton:GetFontString(), y=-1}
+	end)
+	self:SecureHook("WorldMap_ToggleSizeDown", function()
+		sizeDown()
+		self:moveObject{obj=WorldMapFrameCloseButton:GetFontString(), y=1}
+	end)
+	
+	self:addSkinFrame{obj=WorldMapFrame, ft="u", kfs=true}
+	-- handle big/small map sizes
+	if WorldMapFrame.sizedDown then
+		sizeDown()
+		self:moveObject{obj=WorldMapFrameCloseButton:GetFontString(), x=-1}
+	else
+		sizeUp()
+	end
+
+end
