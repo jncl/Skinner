9df6dcc42a339db53e2dc09cd7f0f1ccdbc03536
Jncl <jncl@mac.com>
2010-10-14 23:50:46 +0100

    Updated TimeManager skin

--- UIElements2.lua
+++ UIElements2.lua
@@ -65,7 +65,7 @@ function Skinner:TimeManager() -- LoD
 -->>--	Time Manager Clock Button
 	self:removeRegions(TimeManagerClockButton, {1})
 	if not self.db.profile.MinimalMMBtns then
-		self:addSkinFrame{obj=TimeManagerClockButton, ft=ftype, x1=7, y1=-3, x2=-7, y2=3}
+			self:addSkinFrame{obj=TimeManagerClockButton, ft=ftype, x1=10, y1=-3, x2=-5, y2=5}
 	end
 
 -->>--	Stopwatch Frame
