d5d767cf572d697663f79f983a99e9f41540b487
Jncl <jncl@mac.com>
2010-11-20 21:06:34 +0000

    Moved Bagnon_Forever code into new skin file

--- AddonSkins/Bagnon.lua
+++ AddonSkins/Bagnon.lua
@@ -49,16 +49,6 @@ function Skinner:Banknon()
 
 end
 
-function Skinner:Bagnon_Forever()
-	if not self.db.profile.ContainerFrames then return end
-
-	self:SecureHook(BagnonDB, "ToggleDropdown", function(this)
-		self:keepFontStrings(BagnonDBCharSelect)
-		self:Unhook(BagnonDB, "ToggleDropdown")
-	end)
-
-end
-
 function Skinner:Bagnon_Options()
 
 	self:applySkin(BagnonRightClickMenu)
