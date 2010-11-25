f36236a5deb461ba7f32d9bc4a7ab84e3981e4ff
Jncl <jncl@mac.com>
2010-11-25 20:57:03 +0000

    Updated AtlasLoot & BugSack skins

--- AddonSkins/AtlasLoot.lua
+++ AddonSkins/AtlasLoot.lua
@@ -1,20 +1,23 @@
-if not Skinner:isAddonEnabled("AtlasLoot") then return end
+local aName, aObj = ...
+if not aObj:isAddonEnabled("AtlasLoot") then return end
 
-function Skinner:AtlasLoot()
+function aObj:AtlasLoot()
 
 -->>-- Default Frame
-	local df  = LibStub("AceAddon-3.0"):GetAddon("AtlasLoot"):GetModule("DefaultFrame")
+	local df  = AtlasLoot:GetModule("DefaultFrame")
 	self:skinScrollBar{obj=df.Frame.ScrollFrame}
 	self:skinDropDown{obj=df.Frame.ModuleSelect}
 	self:skinDropDown{obj=df.Frame.InstanceSelect}
 	self:addSkinFrame{obj=df.Frame, kfs=true, y1=-10, x2=2}
 
 -->>-- AtlasLootPanel Frame
-	self:skinEditBox{obj=AtlasLootSearch_Box, regs={9}, y=-6}
-	self:addSkinFrame{obj=AtlasLootPanel, kfs=true, ofs=-2, y2=-6}
+	self:skinEditBox{obj=AtlasLoot.SearchBox, regs={9}, y=-6}
+	self:addSkinFrame{obj=AtlasLoot.AtlasLootPanel, kfs=true, ofs=-2, y2=-6}
 
 -->>-- AtlasLootInfo2 Frame
-	self:skinButton{obj=AtlasLoot_AtlasInfoFrame_ToggleALButton}
+	AtlasLoot.AtlasInfoFrame.Version:SetWidth(180)
+	AtlasLoot.AtlasInfoFrame.Info:SetWidth(180)
+	self:skinButton{obj=AtlasLoot.AtlasInfoFrame.Button}
 
 -->>-- AtlasLootItems Frame
 	for i = 1, #AtlasLoot.ItemFrame.ItemButtons do
@@ -24,9 +27,9 @@ function Skinner:AtlasLoot()
 
 -->>-- Tooltip
 	if self.db.profile.Tooltips.skin then
-		if self.db.profile.Tooltips.style == 3 then AtlasLootTooltip:SetBackdrop(self.backdrop) end
-		self:SecureHookScript(AtlasLootTooltip, "OnShow", function(this)
-			self:skinTooltip(AtlasLootTooltip)
+		if self.db.profile.Tooltips.style == 3 then AtlasLootTooltipTEMP:SetBackdrop(self.backdrop) end
+		self:SecureHookScript(AtlasLootTooltipTEMP, "OnShow", function(this)
+			self:skinTooltip(this)
 		end)
 	end
 
--- AddonSkins/BugSack.lua
+++ AddonSkins/BugSack.lua
@@ -1,36 +1,33 @@
-if not Skinner:isAddonEnabled("BugSack") then return end
+local aName, aObj = ...
+if not aObj:isAddonEnabled("BugSack") then return end
 
-function Skinner:BugSack()
+function aObj:BugSack()
 
-	-- handle new version
-	if BugSack and BugSack.OpenSack then
-		self:SecureHook(BugSack, "OpenSack", function(this)
-			self:skinScrollBar{obj=BugSackFrameScroll}
-			self:addSkinFrame{obj=BugSackFrame, kfs=true, y1=-2, x2=-1, y2=2}
-			-- tabs
-			local tabs = {BugSackTabAll, BugSackTabSession, BugSackTabLast}
-			for _, tabObj in pairs(tabs) do
-				self:keepRegions(tabObj, {7, 8}) -- N.B. region 7 is text, 8 is highlight
-				self:addSkinFrame{obj=tabObj, ft=ftype, noBdr=self.isTT, x1=6, y1=0, x2=-6, y2=2}
-				local tabSF = self.skinFrame[tabObj]
-				if self.isTT then
-					if tabObj == BugSackTabAll then self:setActiveTab(tabSF)
-					else self:setInactiveTab(tabSF) end
-					-- hook this to change the texture for the Active and Inactive tabs
-					self:SecureHookScript(tabObj, "OnClick", function(this)
-						for _, tabObj in pairs(tabs) do
-							local tabSF = self.skinFrame[tabObj]
-							if tabObj == this then self:setActiveTab(tabSF)
-							else self:setInactiveTab(tabSF) end
-						end
-					end)
-				end
+	local aVer = tonumber(GetAddOnMetadata("BugSack", "X-Curse-Packaged-Version"):sub(2))
+
+	self:SecureHook(BugSack, "OpenSack", function(this)
+		self:skinScrollBar{obj=aVer > 241 and BugSackScroll or BugSackFrameScroll}
+		self:addSkinFrame{obj=BugSackFrame, kfs=true, y1=-2, x2=-1, y2=2}
+		-- tabs
+		local tabs = {BugSackTabAll, BugSackTabSession, BugSackTabLast}
+		for _, tabObj in pairs(tabs) do
+			self:keepRegions(tabObj, {7, 8}) -- N.B. region 7 is text, 8 is highlight
+			self:addSkinFrame{obj=tabObj, ft=ftype, noBdr=self.isTT, x1=6, y1=0, x2=-6, y2=2}
+			local tabSF = self.skinFrame[tabObj]
+			if self.isTT then
+				if tabObj == BugSackTabAll then self:setActiveTab(tabSF)
+				else self:setInactiveTab(tabSF) end
+				-- hook this to change the texture for the Active and Inactive tabs
+				self:SecureHookScript(tabObj, "OnClick", function(this)
+					for _, tabObj in pairs(tabs) do
+						local tabSF = self.skinFrame[tabObj]
+						if tabObj == this then self:setActiveTab(tabSF)
+						else self:setInactiveTab(tabSF) end
+					end
+				end)
 			end
-			self:Unhook(BugSack, "OpenSack")
-		end)
-	else
-		self:skinScrollBar{obj=BugSackFrameScroll}
-		self:addSkinFrame{obj=BugSackFrame, kfs=true, y2=8}
-	end
+		end
+		self:Unhook(BugSack, "OpenSack")
+	end)
 
 end
