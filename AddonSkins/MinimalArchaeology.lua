8a119743d9f3bef24c239a772c927a0139374f9a
Jncl <jncl@mac.com>
2010-12-31 22:12:35 +0000

    Updated MinimalArchaeology skin

--- AddonSkins/MinimalArchaeology.lua
+++ AddonSkins/MinimalArchaeology.lua
@@ -3,22 +3,27 @@ if not aObj:isAddonEnabled("MinimalArchaeology") then return end
 
 function aObj:MinimalArchaeology()
 
-	self:glazeStatusBar(MinimalArchaeologyFrame.skillBar, 0,  nil)
-	self:glazeStatusBar(MinimalArchaeologyFrame.statusBar1, 0,  nil)
-	self:glazeStatusBar(MinimalArchaeologyFrame.statusBar2, 0,  nil)
-	self:glazeStatusBar(MinimalArchaeologyFrame.statusBar3, 0,  nil)
-	self:glazeStatusBar(MinimalArchaeologyFrame.statusBar4, 0,  nil)
-	self:glazeStatusBar(MinimalArchaeologyFrame.statusBar5, 0,  nil)
-	self:glazeStatusBar(MinimalArchaeologyFrame.statusBar6, 0,  nil)
-	self:glazeStatusBar(MinimalArchaeologyFrame.statusBar7, 0,  nil)
-	self:glazeStatusBar(MinimalArchaeologyFrame.statusBar8, 0,  nil)
-	self:glazeStatusBar(MinimalArchaeologyFrame.statusBar9, 0,  nil)
-	self:glazeStatusBar(MinimalArchaeologyFrame.statusBar1, 0,  nil)
-	self:addSkinFrame{obj=MinimalArchaeologyFrame}
+	MinimalArchaeologyFrameSkillBarBorder:Hide()
+	MinimalArchaeologyFrameSkillBarBackground:SetAllPoints(MinimalArchaeologyFrame.skillBar)
+	self:glazeStatusBar(MinimalArchaeologyFrame.skillBar, 0,  MinimalArchaeologyFrameSkillBarBackground)
+
+	for i = 1, 10 do
+		local objName = "MinimalArchaeologyFrameArtifactBar"..i
+		local obj = _G[objName]
+		_G[objName.."BarBG"]:Hide()
+		obj:GetStatusBarTexture():SetTexCoord(0, 1, 0, 1)
+		obj.progress:GetStatusBarTexture():SetTexCoord(0, 1, 0, 1)
+		self:glazeStatusBar(obj, 0,  nil)
+		self:glazeStatusBar(obj.progress, 0,  nil)
+		self:skinButton{obj=obj.solveButton}
+	end
+
+	self:addSkinFrame{obj=MinimalArchaeologyFrame, nb=true, x1=8, y1=4, x2=-8}
 
 	-- option panels
 	self:addSkinFrame{obj=MinimalArchaeologyOptionPanel.hideArtifact}
 	self:addSkinFrame{obj=MinimalArchaeologyOptionPanel.useKeystones}
 	self:addSkinFrame{obj=MinimalArchaeologyOptionPanel.miscOptions}
-	
+	self:addSkinFrame{obj=MinimalArchaeologyOptionPanel.frameScale}
+
 end
