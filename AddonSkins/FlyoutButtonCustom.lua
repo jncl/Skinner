3459f37ec92bdef1b83187a4ac20385b21bdbc72
Jncl <jncl@mac.com>
2011-08-09 01:16:35 +0100

    Bugfix for GuildRank dropdown placement

--- AddonSkins/FlyoutButtonCustom.lua
+++ AddonSkins/FlyoutButtonCustom.lua
@@ -4,18 +4,19 @@ if not aObj:isAddonEnabled("FlyoutButtonCustom") then return end
 function aObj:FlyoutButtonCustom()
 
 	-- turn off borders and keep them off
-	FbcShowBorders = false
+	FbcShowBorders = nil
 	FlyoutButtonCustom_Settings = FlyoutButtonCustom_Settings or {}
-	FlyoutButtonCustom_Settings["ShowBorders"] = nil
+	FlyoutButtonCustom_Settings["ShowBorders"] = false
 	local mt = {__newindex = function(t, k, v)
 		if k == "ShowBorders" then
-			FbcShowBorders = false
+			FbcShowBorders = nil
+			rawset(t, k, false)
 			return
 		else rawset(t, k, v) end
 	end}
 	setmetatable(FlyoutButtonCustom_Settings, mt)
 	
 	-- Settings frame
-	self:ScheduleTimer(function() self:addSkinFrame{obj=FBCSettingsDialog, kfs=true, y1=2, x2=2} end, FBC_STARTUP_DELAY)
+	self:ScheduleTimer(function() aObj:addSkinFrame{obj=FBCSettingsDialog, kfs=true, y1=2, x2=2} end, 5)
 
 end
--- AddonSkins/xMerchant.lua
+++ AddonSkins/xMerchant.lua
@@ -1,8 +1,9 @@
-if not Skinner:isAddonEnabled("xMerchant") then return end
+local aName, aObj = ...
+if not aObj:isAddonEnabled("xMerchant") then return end
 
-function Skinner:xMerchant()
+function aObj:xMerchant()
 
-	local frame = NuuhMerchantFrame
+	local frame = NuuhMerchantFrame or xMerchantFrame
 	self:skinEditBox{obj=frame.search, regs={9}}
 	self:skinScrollBar{obj=frame.scrollframe}
 	frame.top:Hide()
