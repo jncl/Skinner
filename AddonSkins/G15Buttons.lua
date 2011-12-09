03910231c805803327ff2bbea7a47583c70e04f1
Jncl <jncl@mac.com>
2011-12-09 13:45:50 +0000

    Skinned G15Buttons & TrinketBar

---
+++ AddonSkins/G15Buttons.lua
@@ -0,0 +1,33 @@
+local aName, aObj = ...
+if not aObj:isAddonEnabled("G15Buttons") then return end
+
+function aObj:G15Buttons()
+
+	if self.modBtnBs then
+		local function skinBtns()
+
+			for i = 1, objG15.LastGiveId do
+				local btn = _G[objG15.UIObjectPrefix..objG15.UIButtonPrefix..i]
+				if btn
+				and not btn.sknrBdr
+				then
+					self:addButtonBorder{obj=btn, abt=true}
+				end
+			end
+
+		end
+		-- hook this to skin new buttons
+		self:SecureHook(objG15, "CreateButton", function(parent)
+			skinBtns()
+		end)
+		-- skin existing buttons
+		skinBtns()
+	end
+
+	-- Options frame
+	self:skinDropDown{obj=G15Buttons_OptionsForm_LBGroups}
+	self:skinEditBox{obj=G15Buttons_OptionsForm_edtSpacePerRow, regs={9}}
+	self:skinEditBox{obj=G15Buttons_OptionsForm_edtSpacePerColumn, regs={9}}
+	self:addSkinFrame{obj=G15Buttons_OptionsForm, hdr=true}
+
+end
---
+++ AddonSkins/TrinketBar.lua
@@ -0,0 +1,44 @@
+local aName, aObj = ...
+if not aObj:isAddonEnabled("TrinketBar") then return end
+
+function aObj:TrinketBar()
+
+	self:addSkinFrame{obj=K_TBanchor}
+
+	if self.modBtnBs then
+		local function skinPopupBtns()
+
+			for i = 1, 20 do
+				local btn = _G["TrinketBarPopupButton"..i]
+				if btn
+				and not btn.sknrBdr
+				then
+					aObj:addButtonBorder{obj=btn, abt=true}
+				end
+			end
+
+		end
+		local spbTmr
+		self:addButtonBorder{obj=K_TB_TopTrinket, abt=true, sec=true}
+		self:SecureHookScript(K_TB_TopTrinket, "OnEnter", function(this)
+			if not spbTmr then
+				spbTmr = self:ScheduleRepeatingTimer(skinPopupBtns, 0.1)
+			end
+		end)
+		self:SecureHookScript(K_TB_TopTrinket, "OnLeave", function(this)
+			self:CancelTimer(spbTmr, true)
+			spbTmr = nil
+		end)
+		self:addButtonBorder{obj=K_TB_BottomTrinket, abt=true, sec=true}
+		self:SecureHookScript(K_TB_BottomTrinket, "OnEnter", function(this)
+			if not spbTmr then
+				spbTmr = self:ScheduleRepeatingTimer(skinPopupBtns, 0.1)
+			end
+		end)
+		self:SecureHookScript(K_TB_BottomTrinket, "OnLeave", function(this)
+			self:CancelTimer(spbTmr, true)
+			spbTmr = nil
+		end)
+	end
+
+end
