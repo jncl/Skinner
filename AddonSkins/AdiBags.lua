d169a02084ad884383ddb84962006fbc8e5e5941
Jncl <jncl@mac.com>
2012-05-29 17:49:54 +0100

    Updated AdiBags skin

--- AddonSkins/AdiBags.lua
+++ AddonSkins/AdiBags.lua
@@ -5,8 +5,17 @@ function aObj:AdiBags()
 
 	local aBag = LibStub("AceAddon-3.0"):GetAddon("AdiBags", true)
 	
-	-- hook this for equipped bag panel creation
+	-- hook this for bag creation
+	aBag:HookBagFrameCreation(aObj, function(bag)
+		aObj:ScheduleTimer(function(bag)
+			local frame = bag:GetFrame()
+			aObj:addSkinFrame{obj=frame}
+			aObj:skinEditBox{obj=_G[frame:GetName().."SearchBox"], regs={9}, mi=true}
+		end, 0.2, bag) -- wait for 2/10th second for frame to be created fully
+	end)
+
 	if self.modBtnBs then
+		-- hook this for equipped bag panel creation
 		self:RawHook(aBag, "CreateBagSlotPanel", function(this, ...)
 			local bPanel = self.hooks[this].CreateBagSlotPanel(this, ...)
 			self:addSkinFrame{obj=bPanel}
@@ -15,15 +24,17 @@ function aObj:AdiBags()
 			end
 			return bPanel
 		end, true)
+		-- don't conceal the quality border
+		local function updBtn(evt, btn)
+			if not btn.sknrBdr then aObj:addButtonBorder{obj=btn} end
+			if btn.IconQuestTexture:GetBlendMode() == "ADD" then
+				btn.sknrBdr:Hide()
+			else
+				btn.sknrBdr:Show()
+			end
+		end
+		-- register for button updates
+		aBag:RegisterMessage("AdiBags_UpdateButton", updBtn)
 	end
-	-- hook this for bag creation
-	aBag:HookBagFrameCreation(aObj, function(bag)
-		aObj:ScheduleTimer(function(bag)
-			local frame = bag:GetFrame()
-			aObj:Debug("OnBagFrameCreated: [%s, %s, %s]", bag, bag.bagName, frame)
-			aObj:addSkinFrame{obj=frame}
-			aObj:skinEditBox{obj=_G[frame:GetName().."SearchBox"], regs={9}, mi=true}
-		end, 0.2, bag) -- wait for 2/10th second for frame to be created fully
-	end)
 
 end
