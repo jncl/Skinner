4c80b5aa61ba1631c85fae637bfc4f8d1fc8024b
Jncl <jncl@mac.com>
2012-12-02 19:29:55 +0000

    BugFix for ChatTemporaryWindow tabs

--- UIFrames.lua
+++ UIFrames.lua
@@ -627,6 +627,19 @@ local function skinChatTab(tab)
 	aObj:rmRegionsTex(tab, {1, 2, 3, 4, 5, 6})
 	aObj:addSkinFrame{obj=tab, ft=ftype, noBdr=aObj.isTT, x1=2, y1=-9, x2=-2, y2=-4}
 	tab.sf:SetAlpha(tab:GetAlpha())
+	-- hook this to fix tab gradient texture overlaying text & highlight
+	aObj:SecureHook(tab, "SetParent", function(this, parent)
+		if parent == _G.GeneralDockManager.scrollFrame.child then
+			this.sf:SetParent(_G.GeneralDockManager)
+		else
+			this.sf:SetParent(this)
+			this.sf:SetFrameLevel(1) -- reset frame level so that the texture is behind text etc
+		end
+	end)
+	-- hook this to manage alpha changes when chat frame fades in and out
+	aObj:SecureHook(tab, "SetAlpha", function(this, alpha)
+		this.sf:SetAlpha(alpha)
+	end)
 
 end
 function aObj:ChatTabs()
@@ -636,19 +649,6 @@ function aObj:ChatTabs()
 	for i = 1, _G.NUM_CHAT_WINDOWS do
 		local tab = _G["ChatFrame" .. i .. "Tab"]
 		skinChatTab(tab)
-		-- hook this to fix tab gradient texture overlaying text & highlight
-		self:SecureHook(tab, "SetParent", function(this, parent)
-			if parent == _G.GeneralDockManager.scrollFrame.child then
-				this.sf:SetParent(_G.GeneralDockManager)
-			else
-				this.sf:SetParent(this)
-				this.sf:SetFrameLevel(1) -- reset frame level so that the texture is behind text etc
-			end
-		end)
-		-- hook this to manage alpha changes when chat frame fades in and out
-		self:SecureHook(tab, "SetAlpha", function(this, alpha)
-			this.sf:SetAlpha(alpha)
-		end)
 	end
 
 	-- hook this to hide/show the skin frame
