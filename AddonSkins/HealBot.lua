d2ad82521560c5e8d0502fba649929410d9b0226
Jncl <jncl@mac.com>
2012-12-19 11:59:58 +0000

    Updated HealBot & PetBattleTeams skins

--- AddonSkins/HealBot.lua
+++ AddonSkins/HealBot.lua
@@ -1,213 +1,144 @@
--- many thanks to acirac/diacono for the updated skin
-if not Skinner:isAddonEnabled("HealBot") then return end
+-- many thanks to acirac/diacono for their work on previous skins
+local aName, aObj = ...
+if not aObj:isAddonEnabled("HealBot") then return end
 
-function Skinner:HealBot() -- version 5.0.5.5
+function aObj:HealBot() -- v 5.1.0.2
 	if not self.db.profile.Tooltips.skin then return end
 
--->>--	Tooltips
-	if self.db.profile.Tooltips.style == 3 then
-		HealBot_ScanTooltip:SetBackdrop(self.backdrop)
-		HealBot_Tooltip:SetBackdrop(self.backdrop)
+	-- Options frame
+	self:addSkinFrame{obj=HealBot_Options_Contents}
+	self:addSkinFrame{obj=HealBot_Options_MainFrame}
+	self:addSkinFrame{obj=HealBot_Options, kfs=true, hdr=true, y1=4}
+
+	-- About
+	self:addSkinFrame{obj=HealBot_Options_Panel0_1}
+	self:skinDropDown{obj=HealBot_Options_FAQ}
+	self:addSkinFrame{obj=HealBot_Options_Panel0_2}
+	self:addSkinFrame{obj=HealBot_Options_Panel0_3}
+	-- General
+	self:addSkinFrame{obj=HealBot_Options_GenFrame}
+	self:skinDropDown{obj=HealBot_Options_hbLangs}
+	self:skinDropDown{obj=HealBot_Options_hbCommands}
+	self:skinDropDown{obj=HealBot_Options_EmergencyFClass}
+	self:addSkinFrame{obj=HealBot_Options_EmergLFrame}
+	-- Spells
+	self:skinDropDown{obj=HealBot_Options_ActionBarsCombo}
+	self:adjHeight{obj=HealBot_Options_ActionBarsCombo, adj=12}
+	self:skinDropDown{obj=HealBot_Options_CastButton}
+	self:skinDropDown{obj=HealBot_Options_ButtonCastMethod}
+	for _, v in pairs{"Click", "Shift", "Ctrl", "Alt", "CtrlShift", "AltShift", "CtrlAlt"} do
+		self:skinEditBox{obj=_G["HealBot_Options_" .. v], regs={9}, noHeight=true}
+		self:skinButton{obj=_G["HealBot_Options_" .. v .. "HelpSelectButton"]}
 	end
-	self:skinTooltip(HealBot_ScanTooltip)
-	self:skinTooltip(HealBot_Tooltip)
+	self:addSkinFrame{obj=HealBot_Options_KeysFrame}
+	self:addSkinFrame{obj=HealBot_Options_DisabledBarPanel}
+	for _, v in pairs{"HealSpells", "OtherSpells", "Macros", "Items", "Cmds"} do
+		self:skinDropDown{obj=_G["HealBot_Options_Select" .. v .. "Combo"]}
+		self:adjHeight{obj=_G["HealBot_Options_Select" .. v .. "Combo"], adj=12}
+		self:skinButton{obj=_G["HealBot_Options_" .. v .. "Select"]}
+	end
+	self:addSkinFrame{obj=HealBot_Options_SelectSpellsFrame}
+	-- Skins
+	self:skinDropDown{obj=HealBot_Options_Skins}
+	self:skinEditBox{obj=HealBot_Options_NewSkin, regs={9, 10}}
+	self:skinDropDown{obj=HealBot_Options_ShareSkin}
+		-- General
+		self:skinDropDown{obj=HealBot_Options_SkinPartyRaidDefault}
+		self:skinDropDown{obj=HealBot_Options_ActionAnchor}
+		self:skinDropDown{obj=HealBot_Options_ActionBarsAnchor}
+		self:skinDropDown{obj=HealBot_Options_ManaIndicator}
+		self:addSkinFrame{obj=HealBot_Options_GeneralSkinsFrame}
+		-- Healing
+		self:addSkinFrame{obj=HealBot_Options_HealAlertFrame}
+		self:skinDropDown{obj=HealBot_Options_EmergencyFilter}
+		self:addSkinFrame{obj=HealBot_Options_HealRaidFrame}
+		self:skinDropDown{obj=HealBot_Options_ExtraSort}
+		self:skinDropDown{obj=HealBot_Options_ExtraSubSort}
+		self:addSkinFrame{obj=HealBot_Options_HealSortFrame}
+		self:addSkinFrame{obj=HealBot_Options_HealHideFrame}
+		self:addSkinFrame{obj=HealBot_Options_HealingSkinsFrame}
+		-- Headers
+		self:skinDropDown{obj=HealBot_Options_HeadFontOutline}
+		self:addSkinFrame{obj=HealBot_Options_HeadersSkinsFrame}
+		-- Bars
+		self:skinDropDown{obj=HealBot_Options_BarHealthColour}
+		self:skinDropDown{obj=HealBot_Options_BarHealthBackColour}
+		self:skinDropDown{obj=HealBot_Options_BarIncHealColour}
+		self:addSkinFrame{obj=HealBot_Options_BarsSkinsFrame}
+		-- Icons
+		self:skinDropDown{obj=HealBot_Options_FilterHoTctl}
+		self:skinDropDown{obj=HealBot_Options_Class_HoTctlName}
+		self:skinDropDown{obj=HealBot_Options_Class_HoTctlAction}
+		self:addSkinFrame{obj=HealBot_Options_IconsSkinsFrame}
+		-- Aggro
+		self:skinDropDown{obj=HealBot_Options_BarHealthNumFormatAggro}
+		self:skinDropDown{obj=HealBot_Options_AggroIndAlertLevel}
+		self:skinDropDown{obj=HealBot_Options_AggroAlertLevel}
+		self:addSkinFrame{obj=HealBot_Options_AggroSkinsFrame}
+		-- Protection
+		self:skinEditBox{obj=HealBot_Options_CrashProtEditBox, regs={9, 10}}
+		self:addSkinFrame{obj=HealBot_Options_CrashProtPanel}
+		self:addSkinFrame{obj=HealBot_Options_CombatProtPanel}
+		self:addSkinFrame{obj=HealBot_Options_ProtSkinsFrame}
+		-- Chat
+		self:skinEditBox{obj=HealBot_Options_NotifyChan, regs={9}}
+		self:skinEditBox{obj=HealBot_Options_NotifyOtherMsg, regs={9, 10}}
+		self:addSkinFrame{obj=HealBot_Options_ChatSkinsFrame}
+		-- Bar text
+		self:skinDropDown{obj=HealBot_Options_BarHealthIncHeal}
+		self:skinDropDown{obj=HealBot_Options_BarHealthType}
+		self:skinDropDown{obj=HealBot_Options_BarHealthNumFormat1}
+		self:skinDropDown{obj=HealBot_Options_BarHealthNumFormat2}
+		self:skinDropDown{obj=HealBot_Options_FontOutline}
+		self:addSkinFrame{obj=HealBot_Options_TextSkinsFrame}
+		-- Icon text
+		self:addSkinFrame{obj=HealBot_Options_IconTextSkinsFrame}
 
--- Tabs
+	-- Cure
+	self:skinDropDown{obj=HealBot_Options_CDCTxt1}
+	self:skinDropDown{obj=HealBot_Options_CDCGroups1}
+	self:skinDropDown{obj=HealBot_Options_CDCTxt2}
+	self:skinDropDown{obj=HealBot_Options_CDCGroups2}
+	self:skinDropDown{obj=HealBot_Options_CDCTxt3}
+	self:skinDropDown{obj=HealBot_Options_CDCGroups3}
+	self:skinDropDown{obj=HealBot_Options_CDCPriority1}
+	self:skinDropDown{obj=HealBot_Options_CDCPriority2}
+	self:skinDropDown{obj=HealBot_Options_CDCPriority3}
+	self:skinDropDown{obj=HealBot_Options_CDCPriority4}
+	self:addSkinFrame{obj=HealBot_Options_CureDispelCleanse}
+	self:skinDropDown{obj=HealBot_Options_CDebuffCat}
+	self:skinDropDown{obj=HealBot_Options_CDebuffTxt1}
+	self:skinDropDown{obj=HealBot_Options_CDCCastBy}
+	self:skinEditBox{obj=HealBot_Options_NewCDebuff, regs={9, 10}}
+	self:skinDropDown{obj=HealBot_Options_CDCPriorityC}
+	self:addSkinFrame{obj=HealBot_Options_CustomCureFrame}
+	self:skinDropDown{obj=HealBot_Options_CDCWarnRange1}
+	self:skinDropDown{obj=HealBot_Options_CDCWarnRange2}
+	self:skinDropDown{obj=HealBot_Options_CDCWarnRange3}
+	self:skinDropDown{obj=HealBot_Options_CDCWarnRange4}
+	self:addSkinFrame{obj=HealBot_Options_WarningCureFrame}
+	-- Buffs
+	for i = 1, 8 do
+		self:skinDropDown{obj=_G["HealBot_Options_BuffTxt".. i]}
+		self:skinDropDown{obj=_G["HealBot_Options_BuffGroups" .. i]}
+	end
+	self:addSkinFrame{obj=HealBot_Options_BuffsPanel}
+	-- Tips
+	self:skinDropDown{obj=HealBot_Options_TooltipPos}
+	-- Mouse Wheel
+	for _, key in pairs{"", "Shift", "Ctrl", "Alt"} do
+		for _, action in pairs{"Up", "Down"} do
+			self:skinDropDown{obj=_G["HealBot_Options_MouseWheel" .. key .. action]}
+		end
+	end
+	-- Test
+	self:skinButton{obj=HealBot_Options_TestBarsButton}
+	-- Usage
 
-	self:keepFontStrings(HealBot_Options_Contents)
-	self:applySkin(HealBot_Options_Contents, nil)
-	self:keepFontStrings(HealBot_Options_MainFrame)
-	self:applySkin(HealBot_Options_MainFrame, nil)
-	
-	
-	self:keepFontStrings(HealBot_Options_FAQ)
-	self:keepFontStrings(HealBot_Options_Panel0_1)
-	self:applySkin(HealBot_Options_Panel0_1, nil)
-	self:keepFontStrings(HealBot_Options_Panel0_2)
-	self:applySkin(HealBot_Options_Panel0_2, nil)
-	self:keepFontStrings(HealBot_Options_Panel0_3)
-	self:applySkin(HealBot_Options_Panel0_3, nil)
-	
-	self:keepFontStrings(HealBot_Options_SelectHealSpellsCombo)
-	self:keepFontStrings(HealBot_Options_SelectOtherSpellsCombo)
-	self:keepFontStrings(HealBot_Options_SelectMacrosCombo)
-	self:keepFontStrings(HealBot_Options_SelectItemsCombo)
-	self:keepFontStrings(HealBot_Options_SelectCmdsCombo)
-	
-	self:keepFontStrings(HealBot_Options_hbCommands)
-	self:keepFontStrings(HealBot_Options_EmergencyFilter)
-	self:keepFontStrings(HealBot_Options_ManaIndicator)
-	self:keepFontStrings(HealBot_Options_BarHealthNumFormatAggro)
-	self:keepFontStrings(HealBot_Options_AggroIndAlertLevel)	
-	self:keepFontStrings(HealBot_Options_AggroAlertLevel)	
-	self:keepFontStrings(HealBot_Options_CastButton)
-	self:keepFontStrings(HealBot_Options_ButtonCastMethod)
-	self:keepFontStrings(HealBot_Options_ExtraSort)
-	self:keepFontStrings(HealBot_Options_ExtraSubSort)
-	self:keepFontStrings(HealBot_Options_FilterHoTctl)
-	self:keepFontStrings(HealBot_Options_Class_HoTctlName)
-	self:keepFontStrings(HealBot_Options_Class_HoTctlAction)
-	
-	self:keepFontStrings(HealBot_Options_BarHealthIncHeal)
-	self:keepFontStrings(HealBot_Options_BarHealthType)
-	self:keepFontStrings(HealBot_Options_BarHealthNumFormat1)
-	self:keepFontStrings(HealBot_Options_BarHealthNumFormat2)
-	self:keepFontStrings(HealBot_Options_HeadFontOutline)
-	self:keepFontStrings(HealBot_Options_FontOutline)
-	
-	self:keepFontStrings(HealBot_Options_EmergencyFClass)
-	self:keepFontStrings(HealBot_Options_EmergLFrame)
-	self:applySkin(HealBot_Options_EmergLFrame, nil)	
-	
-	self:keepFontStrings(HealBot_Options_BuffTxt1)
-	self:keepFontStrings(HealBot_Options_BuffTxt2)
-	self:keepFontStrings(HealBot_Options_BuffTxt3)
-	self:keepFontStrings(HealBot_Options_BuffTxt4)
-	self:keepFontStrings(HealBot_Options_BuffTxt5)
-	self:keepFontStrings(HealBot_Options_BuffTxt6)
-	self:keepFontStrings(HealBot_Options_BuffTxt7)
-	self:keepFontStrings(HealBot_Options_BuffTxt8)
-	
-	self:keepFontStrings(HealBot_Options_BuffGroups1)
-	self:keepFontStrings(HealBot_Options_BuffGroups2)
-	self:keepFontStrings(HealBot_Options_BuffGroups3)
-	self:keepFontStrings(HealBot_Options_BuffGroups4)
-	self:keepFontStrings(HealBot_Options_BuffGroups5)
-	self:keepFontStrings(HealBot_Options_BuffGroups6)
-	self:keepFontStrings(HealBot_Options_BuffGroups7)
-	self:keepFontStrings(HealBot_Options_BuffGroups8)
-	
-	self:keepFontStrings(HealBot_Options_CDCTxt1)
-	self:keepFontStrings(HealBot_Options_CDCTxt2)
-	self:keepFontStrings(HealBot_Options_CDCTxt3)
-	
-	self:keepFontStrings(HealBot_Options_CDCGroups1)
-	self:keepFontStrings(HealBot_Options_CDCGroups2)
-	self:keepFontStrings(HealBot_Options_CDCGroups3)
-	
-	self:keepFontStrings(HealBot_Options_CDCPriority1)
-	self:keepFontStrings(HealBot_Options_CDCPriority2)
-	self:keepFontStrings(HealBot_Options_CDCPriority3)
-	self:keepFontStrings(HealBot_Options_CDCPriority4)
-	
-	self:keepFontStrings(HealBot_Options_CDebuffCat)
-	self:keepFontStrings(HealBot_Options_CDCPriorityC)
-	
-	self:keepFontStrings(HealBot_Options_CDCWarnRange1)
-	self:keepFontStrings(HealBot_Options_CDCWarnRange2)
-	self:keepFontStrings(HealBot_Options_CDCWarnRange3)
-	self:keepFontStrings(HealBot_Options_CDCWarnRange4)
-	
-	self:keepFontStrings(HealBot_Options_BarHealthColour)
-	self:keepFontStrings(HealBot_Options_BarHealthBackColour)
-	self:keepFontStrings(HealBot_Options_BarIncHealColour)
-	
-	self:keepFontStrings(HealBot_Options_BuffsPanel)
-	self:applySkin(HealBot_Options_BuffsPanel, nil)	
-	self:keepFontStrings(HealBot_Options_ComboClassButton)
-	self:keepFontStrings(HealBot_Options_ActionBarsCombo)
-	self:keepFontStrings(HealBot_Options_MouseWheelUp)
-	self:keepFontStrings(HealBot_Options_MouseWheelDown)	
-	self:keepFontStrings(HealBot_Options_MouseWheelShiftUp)
-	self:keepFontStrings(HealBot_Options_MouseWheelShiftDown)
-	self:keepFontStrings(HealBot_Options_MouseWheelCtrlUp)
-	self:keepFontStrings(HealBot_Options_MouseWheelCtrlDown)
-	self:keepFontStrings(HealBot_Options_MouseWheelAltUp)
-	self:keepFontStrings(HealBot_Options_MouseWheelAltDown)	
-	self:keepFontStrings(HealBot_Options_SkinPartyRaidDefault)
-	self:keepFontStrings(HealBot_Options_KeysFrame)
-	self:applySkin(HealBot_Options_KeysFrame, nil)
-	self:keepFontStrings(HealBot_Options_DisabledBarPanel)
-	self:applySkin(HealBot_Options_DisabledBarPanel, nil)
-	self:keepFontStrings(HealBot_Options_SelectSpellsFrame)
-	self:applySkin(HealBot_Options_SelectSpellsFrame, nil)
-	self:keepFontStrings(HealBot_Options_HealAlertFrame)
-	self:applySkin(HealBot_Options_HealAlertFrame, nil)
-	self:keepFontStrings(HealBot_Options_HealRaidFrame)
-	self:applySkin(HealBot_Options_HealRaidFrame, nil)	
-	self:keepFontStrings(HealBot_Options_HealSortFrame)
-	self:applySkin(HealBot_Options_HealSortFrame, nil)
-	self:keepFontStrings(HealBot_Options_HealHideFrame)
-	self:applySkin(HealBot_Options_HealHideFrame, nil)	
-	self:keepFontStrings(HealBot_Options_AggroSkinsFrame)
-	self:applySkin(HealBot_Options_AggroSkinsFrame, nil)
-	self:keepFontStrings(HealBot_Options_IconTextSkinsFrame)
-	self:applySkin(HealBot_Options_IconTextSkinsFrame, nil)	
-	
-	self:keepFontStrings(HealBot_Options_TooltipPos)
-	--[[self:keepFontStrings(HealBot_Options_TooltipsPanel)
-	self:applySkin(HealBot_Options_TooltipsPanel, nil)--]]
-	self:keepFontStrings(HealBot_Options_GeneralSkinsFrame)
-	self:applySkin(HealBot_Options_GeneralSkinsFrame, nil)
-	self:keepFontStrings(HealBot_Options_HealingSkinsFrame)
-	self:applySkin(HealBot_Options_HealingSkinsFrame, nil)			
-	self:keepFontStrings(HealBot_Options_ChatSkinsFrame)
-	self:applySkin(HealBot_Options_ChatSkinsFrame, nil)	
-	self:keepFontStrings(HealBot_Options_HeadersSkinsFrame)
-	self:applySkin(HealBot_Options_HeadersSkinsFrame, nil)	
-	self:keepFontStrings(HealBot_Options_BarsSkinsFrame)
-	self:applySkin(HealBot_Options_BarsSkinsFrame, nil)	
-	self:keepFontStrings(HealBot_Options_TextSkinsFrame)
-	self:applySkin(HealBot_Options_TextSkinsFrame, nil)	
-	self:keepFontStrings(HealBot_Options_IconsSkinsFrame)
-	self:applySkin(HealBot_Options_IconsSkinsFrame, nil)
-	
-	self:keepFontStrings(HealBot_Options_CustomCureFrame)
-	self:applySkin(HealBot_Options_CustomCureFrame, nil)
-	self:keepFontStrings(HealBot_Options_WarningCureFrame)
-	self:applySkin(HealBot_Options_WarningCureFrame, nil)
-	
-	self:keepFontStrings(HealBot_Options_CDebuffTxt1)
-	self:keepFontStrings(HealBot_Options_CDCCastBy)
-	self:keepFontStrings(HealBot_Options_CureDispelCleanse)
-	self:applySkin(HealBot_Options_CureDispelCleanse, nil)	
-	self:keepFontStrings(HealBot_Options_Skins)
-	self:keepFontStrings(HealBot_Options_ShareSkin)
-	self:keepFontStrings(HealBot_Options_ActionAnchor)
-	self:keepFontStrings(HealBot_Options_ActionBarsAnchor)
-	self:keepFontStrings(HealBot_Options_GenFrame)
-	self:applySkin(HealBot_Options_GenFrame, nil)
-	self:keepFontStrings(HealBot_Options_ProtSkinsFrame)
-	self:applySkin(HealBot_Options_ProtSkinsFrame, nil)
-	self:keepFontStrings(HealBot_Options_CrashProtPanel)
-	self:applySkin(HealBot_Options_CrashProtPanel, nil)
-	self:keepFontStrings(HealBot_Options_CombatProtPanel)
-	self:applySkin(HealBot_Options_CombatProtPanel, nil)	
-	--[[self:keepFontStrings(HealBot_Options_MouseWheelPanel)
-	self:applySkin(HealBot_Options_MouseWheelPanel, nil)	
-	self:keepFontStrings(HealBot_Options_TestButtonsPanel)
-	self:applySkin(HealBot_Options_TestButtonsPanel, nil)	--]]
-	
-	
-	self:moveObject(HealBot_Options_ActionBarsCombo, nil, nil, "+", 5)
-	self:moveObject(HealBot_Options_CastButton, nil, nil, "+", 5)
-	self:moveObject(HealBot_ComboButtons_ButtonText, nil, nil, "+", 12)
-	self:moveObject(HealBot_ComboButtons_Button1, nil, nil, "+", 12)
-	self:moveObject(HealBot_AutoTarget_ButtonText, nil, nil, "+", 15)
-	self:moveObject(HealBot_AutoTrinket_ButtonText, nil, nil, "+", 15)
-	self:moveObject(HealBot_Options_Click, nil, nil, "+", 15)
-	self:moveObject(HealBot_Options_MouseWheelModKey, nil, nil, "-", 1)
-	self:moveObject(HealBot_Options_NewCDebuff, nil, nil, "+", 4)
-	self:moveObject(HealBot_Options_NewSkin, nil, nil, "+", 4)
-	self:moveObject(HealBot_Options_NewCDebuffBtn, "-", 5, nil, nil)
-	self:moveObject(HealBot_Options_NewSkinb, "-", 5, nil, nil)
-	self:moveObject(HealBot_Options_NotifyHealMsg, nil, nil, "+", 2)
-	self:moveObject(HealBot_Options_NotifyOtherMsg, nil, nil, "+", 3)
-			
-	self:skinEditBox(HealBot_Options_NotifyChan, {9})
-	self:skinEditBox(HealBot_Options_NotifyOtherMsg, {9,10})
-	self:skinEditBox(HealBot_Options_NotifyHealMsg, {9,10})
-	self:skinEditBox(HealBot_Options_Click, {9})
-	self:skinEditBox(HealBot_Options_Shift, {9})
-	self:skinEditBox(HealBot_Options_Ctrl, {9})
-	self:skinEditBox(HealBot_Options_Alt, {9})
-	self:skinEditBox(HealBot_Options_CtrlShift, {9})
-	self:skinEditBox(HealBot_Options_AltShift, {9})
-	self:skinEditBox(HealBot_Options_CtrlAlt, {9})
-	self:skinEditBox(HealBot_Options_NewCDebuff, {9,10})
-	self:skinEditBox(HealBot_Options_NewSkin, {9,10})
-	self:skinEditBox(HealBot_Options_CrashProtEditBox, {9})
-	
-	self:keepFontStrings(HealBot_Options)
-	self:applySkin(HealBot_Options, true)
+	-- tooltips
+	if self.db.profile.Tooltips.skin then
+		self:add2Table(self.ttList, "HealBot_Tooltip")
+		self:add2Table(self.ttList, "HealBot_ScanTooltip")
+	end
+
 end
--- AddonSkins/PetBattleTeams.lua
+++ AddonSkins/PetBattleTeams.lua
@@ -6,7 +6,7 @@ function aObj:PetBattleTeams()
 	local function skinTeamFrame(frame)
 
 		for i = 1, 3 do
-			local btn = frame[i]
+			local btn = frame.unitFrames[i]
 			aObj:addButtonBorder{obj=btn, relTo=btn.Icon, reParent={btn.ActualHealthBar, frame.selectedTexture, frame.lockedTexture}}
 			btn.healthBarWidth = 34
 			btn.ActualHealthBar:SetWidth(34)
@@ -19,19 +19,24 @@ function aObj:PetBattleTeams()
 	end
 
 	-- skin frame
-	self:skinScrollBar{obj=PetBattleTeamsUI.scrollBar}
-	PetBattleTeamsUI.mainFrame.menu.overlay:SetTexture(nil)
-	self:addButtonBorder{obj=PetBattleTeamsUI.mainFrame.menu, ofs=-2, x1=3}
-	self:addButtonBorder{obj=PetBattleTeamsUI.mainFrame.addTeamButton}
-	self:addSkinFrame{obj=PetBattleTeamsUI.mainFrame, ofs=-3, y1=-2, y2=-1}
-	-- Team Roster buttons
-	for i = 1, #PetBattleTeamsUI.mainFrame.petUnitFrames do
-		skinTeamFrame(PetBattleTeamsUI.mainFrame.petUnitFrames[i])
-	end
+	local pbtuimf = PetBattleTeams:GetModule("GUI").mainFrame
+	self:skinScrollBar{obj=pbtuimf.rosterFrame.scrollFrame}
+	self:addButtonBorder{obj=pbtuimf.addTeamButton}
+	self:addButtonBorder{obj=pbtuimf.reviveButton}
+	self:addButtonBorder{obj=pbtuimf.bandageButton}
+	self:addSkinFrame{obj=pbtuimf, y1=2, y2=-5}
+	-- Selected Team
+	skinTeamFrame(pbtuimf.selectedTeam)
+	-- Team Roster, wait for them to be created
+	self:ScheduleTimer(function()
+		for i = 1, #pbtuimf.rosterFrame.scrollChild.teamFrames do
+			skinTeamFrame(pbtuimf.rosterFrame.scrollChild.teamFrames[i])
+		end
+	end, 0.2)
 
 	-- tooltip
 	if self.db.profile.Tooltips.skin then
-		local tt = PetBattleTeamsUI.tooltip
+		local tt = PetBattleTeams:GetModule("Tooltip").tooltip
 		tt:DisableDrawLayer("BACKGROUND")
 		tt.ActualHealthBar:SetTexture(self.sbTexture)
 		tt.XPBar:SetTexture(self.sbTexture)
