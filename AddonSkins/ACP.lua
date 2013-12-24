344b34aceaccb635a2a5c20441559912aa3964f5
Jncl <jncl@mac.com>
2013-12-24 12:57:19 +0000

    Updated ACP skin

--- AddonSkins/ACP.lua
+++ AddonSkins/ACP.lua
@@ -1,23 +1,31 @@
 local aName, aObj = ...
 if not aObj:isAddonEnabled("ACP") then return end
+local _G = _G
 
 function aObj:ACP()
 
-	ACP_AddonList:SetScale(0.75) -- shrink frame
+	_G.ACP_AddonList:SetScale(0.75) -- shrink frame
 
-	self:skinDropDown{obj=ACP_AddonListSortDropDown, x2=110}
-	self:skinScrollBar{obj=ACP_AddonList_ScrollFrame}
-	self:addSkinFrame{obj=ACP_AddonList, kfs=true, hdr=true, x1=12, x2=-40, y2=12}
+	self:skinDropDown{obj=_G.ACP_AddonListSortDropDown, x2=110}
+	self:skinScrollBar{obj=_G.ACP_AddonList_ScrollFrame}
+	self:addSkinFrame{obj=_G.ACP_AddonList, kfs=true, hdr=true, x1=12, x2=-40, y2=12}
 	-- skin the buttons
-	self:skinButton{obj=GameMenuButtonAddOns}
-	self:skinButton{obj=ACP_AddonListCloseButton, cb=true}
-	self:skinButton{obj=ACP_AddonListDisableAll}
-	self:skinButton{obj=ACP_AddonListEnableAll}
-	self:skinButton{obj=ACP_AddonListSetButton}
-	self:skinButton{obj=ACP_AddonList_ReloadUI}
-	self:skinButton{obj=ACP_AddonListBottomClose}
+	self:skinButton{obj=_G.ACP_AddonListCloseButton, cb=true}
+	self:skinButton{obj=_G.ACP_AddonListDisableAll}
+	self:skinButton{obj=_G.ACP_AddonListEnableAll}
+	self:skinButton{obj=_G.ACP_AddonListSetButton}
+	self:skinButton{obj=_G.ACP_AddonList_ReloadUI}
+	self:skinButton{obj=_G.ACP_AddonListBottomClose}
 	for i = 1, 20 do
 		self:skinButton{obj=_G["ACP_AddonListEntry"..i.."LoadNow"]}
 	end
+
+	-- skin GameMenuFrame button and adjust GameMenuFrame height
+	self:skinButton{obj=_G.GameMenuButtonAddOns}
+	self:SecureHook("GameMenuFrame_UpdateVisibleButtons", function(this)
+		if self:getInt(_G.GameMenuFrame:GetHeight()) < 335 then
+			self:adjHeight{obj=_G.GameMenuFrame, adj=25}
+		end
+	end)
 
 end
