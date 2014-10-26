3820ec406f180569c73a69a060456933bc1952e1
Jncl <jncl@mac.com>
2014-10-26 13:50:05 +0000

    Skinned Bugger

---
+++ AddonSkins/Bugger.lua
@@ -0,0 +1,32 @@
+local aName, aObj = ...
+if not aObj:isAddonEnabled("Bugger") then return end
+local _G = _G
+
+function aObj:Bugger()
+
+	local function chgTabTex(tab)
+		for i = 1, _G.ScriptErrorsFrame.numTabs do
+			local tabObj = _G["ScriptErrorsFrameTab" .. i]
+			if tab == tabObj then
+				aObj:setActiveTab(tabObj.sf)
+			else
+				aObj:setInactiveTab(tabObj.sf)
+			end
+			
+		end
+	end
+	self:SecureHook(_G.Bugger, "SetupFrame", function(this)
+		_G.ScriptErrorsFrame.numTabs = 3
+		_G.ScriptErrorsFrame.selectedTab = 3
+		self:skinTabs{obj=_G.ScriptErrorsFrame, ignore=true, lod=true, x1=16, y1=3, x2=-14, y2=4}
+		if aObj.isTT then
+			for i = 1, _G.ScriptErrorsFrame.numTabs do
+				self:SecureHookScript(_G["ScriptErrorsFrameTab" .. i], "OnClick", function(this)
+					chgTabTex(this)	
+				end)
+			end
+		end
+		self:Unhook(_G.Bugger, "SetupFrame")
+	end)
+
+end
