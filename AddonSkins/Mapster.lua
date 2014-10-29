fffadce1b99c546429ba3a1ac9b6e35dfee1260b
Jncl <jncl@mac.com>
2014-10-29 13:43:42 +0000

    Updated Mapster & Pawn skins

--- AddonSkins/Mapster.lua
+++ AddonSkins/Mapster.lua
@@ -7,27 +7,6 @@ function aObj:Mapster()
 	local Mapster = _G.LibStub("AceAddon-3.0"):GetAddon("Mapster", true)
 	if not Mapster then return end
 
-	local function sizeDown()
-
-		_G.WorldMapFrame.sf:ClearAllPoints()
-		_G.WorldMapFrame.sf:SetPoint("TOPLEFT", _G.WorldMapFrame, "TOPLEFT", 12, -12)
-		_G.WorldMapFrame.sf:SetPoint("BOTTOMRIGHT", _G.WorldMapFrame, "BOTTOMRIGHT", -20, -15)
-
-	end
-	local function sizeUp()
-
-		_G.WorldMapFrame.sf:ClearAllPoints()
-		_G.WorldMapFrame.sf:SetPoint("TOPLEFT", _G.WorldMapFrame, "TOPLEFT", 0, 1)
-		_G.WorldMapFrame.sf:SetPoint("BOTTOMRIGHT", _G.WorldMapFrame, "BOTTOMRIGHT", 1, 0)
-
-	end
-	-- hook these to handle map size changes
-	self:SecureHook(Mapster, "SizeDown", function()
-		sizeDown()
-	end)
-	self:SecureHook(Mapster, "SizeUp", function()
-		sizeUp()
-	end)
 	self:SecureHook(Mapster, "UpdateBorderVisibility", function()
 		if Mapster.bordersVisible then
 			_G.WorldMapFrame.sf:Show()
@@ -35,14 +14,7 @@ function aObj:Mapster()
 			_G.WorldMapFrame.sf:Hide()
 		end
 	end)
-	self:addSkinFrame{obj=_G.WorldMapFrame, ft="u", kfs=true}
-	-- handle big/small map sizes
-	if _G.WORLDMAP_SETTINGS.size == _G.WORLDMAP_WINDOWED_SIZE then
-		sizeDown()
-	else
-		sizeUp()
-	end
-	-- on WorldMap frame
+	self:addSkinFrame{obj=_G.WorldMapFrame, ft="u", kfs=true, ofs=2}
 	self:skinButton{obj=Mapster.optionsButton}
 
 end
--- AddonSkins/Pawn.lua
+++ AddonSkins/Pawn.lua
@@ -1,68 +1,61 @@
 local aName, aObj = ...
 if not aObj:isAddonEnabled("Pawn") then return end
+local _G = _G
 
 function aObj:Pawn()
 
 	local aVer = GetAddOnMetadata("Pawn", "Version")
 
 	-- don't skin IOF Pawn button
-	self.ignoreIOF[PawnInterfaceOptionsFrame] = true
+	self.ignoreIOF[_G.PawnInterfaceOptionsFrame] = true
 	-- remove button textures from behind buttons
-	PawnUI_InventoryPawnButton:DisableDrawLayer("BACKGROUND")
-	PawnInterfaceOptionsFrame_PawnButton:DisableDrawLayer("BACKGROUND")
+	_G.PawnUI_InventoryPawnButton:DisableDrawLayer("BACKGROUND")
+	_G.PawnInterfaceOptionsFrame_PawnButton:DisableDrawLayer("BACKGROUND")
 
 	-- skin the UI
-	self:addSkinFrame{obj=PawnUIFrame}
+	self:addSkinFrame{obj=_G.PawnUIFrame}
 
 -->>-- Scale Selector Frame
-	self:keepFontStrings(PawnUIScaleSelector)
-	self:skinScrollBar{obj=PawnUIScaleSelectorScrollFrame}
+	self:keepFontStrings(_G.PawnUIScaleSelector)
+	self:skinScrollBar{obj=_G.PawnUIScaleSelectorScrollFrame}
 -->>-- Scales Tab
 	if self.modBtnBs then
-		self:addButtonBorder{obj=PawnUIFrame_ScaleColorSwatch, ofs=-4}
+		self:addButtonBorder{obj=_G.PawnUIFrame_ScaleColorSwatch, ofs=-4}
 	end
 -->>-- Values Tab
-	self:skinEditBox{obj=PawnUIFrame_StatValueBox, regs={9}}
-	self:addSkinFrame{obj=self:getChild(PawnUIValuesTabPage, 1)}
-	self:skinScrollBar{obj=PawnUIFrame_StatsList}
+	self:skinEditBox{obj=_G.PawnUIFrame_StatValueBox, regs={9}}
+	self:addSkinFrame{obj=self:getChild(_G.PawnUIValuesTabPage, 1)}
+	self:skinScrollBar{obj=_G.PawnUIFrame_StatsList}
 -->>-- Compare Tab
 	if self.modBtnBs then
-		self:addButtonBorder{obj=PawnUICompareItemIcon1}
-		self:addButtonBorder{obj=PawnUICompareItemIcon2}
-		self:addButtonBorder{obj=PawnUIFrame_ClearItemsButton}
-		self:addButtonBorder{obj=PawnUICompareItemShortcut1}
-		self:addButtonBorder{obj=PawnUICompareItemShortcut2}
-		self:addButtonBorder{obj=PawnUICompareItemShortcut3}
-		self:addButtonBorder{obj=PawnUICompareItemShortcut4}
+		self:addButtonBorder{obj=_G.PawnUICompareItemIcon1}
+		self:addButtonBorder{obj=_G.PawnUICompareItemIcon2}
+		self:addButtonBorder{obj=_G.PawnUIFrame_ClearItemsButton}
+		self:addButtonBorder{obj=_G.PawnUICompareItemShortcut1}
+		self:addButtonBorder{obj=_G.PawnUICompareItemShortcut2}
+		self:addButtonBorder{obj=_G.PawnUICompareItemShortcut3}
+		self:addButtonBorder{obj=_G.PawnUICompareItemShortcut4}
 	end
-	self:skinScrollBar{obj=PawnUICompareScrollFrame}
+	self:skinScrollBar{obj=_G.PawnUICompareScrollFrame}
 -->>-- Gems Tab
-	self:keepFontStrings(PawnUIGemsTabPage)
-	self:skinEditBox{obj=PawnUIFrame_GemQualityLevelBox, regs={9}}
-	self:skinScrollBar{obj=PawnUIGemScrollFrame}
--->>-- Options Tab
-	self:skinEditBox(PawnUIFrame_DigitsBox, {9})
+	self:keepFontStrings(_G.PawnUIGemsTabPage)
+	self:skinEditBox{obj=_G.PawnUIFrame_GemQualityLevelBox, regs={9}}
+	self:skinScrollBar{obj=_G.PawnUIGemScrollFrame}
 
 -->>-- Dialog Frame
-	self:skinEditBox(PawnUIStringDialog_TextBox, {9})
-	self:addSkinFrame{obj=PawnUIStringDialog}
+	self:skinEditBox(_G.PawnUIStringDialog_TextBox, {9})
+	self:addSkinFrame{obj=_G.PawnUIStringDialog}
 
 -->>-- Tabs
-	self:skinTabs{obj=PawnUIFrame, lod=true, x1=6, y1=0, x2=-6, y2=2}
+	self:skinTabs{obj=_G.PawnUIFrame, lod=true, x1=6, y1=0, x2=-6, y2=2}
 
 -->>-- Tooltips
-	PawnCommon.ColorTooltipBorder = false -- disable tooltip border color change
+	_G.PawnCommon.ColorTooltipBorder = false -- disable tooltip border color change
 	if self.db.profile.Tooltips.skin then
 		self:SecureHook("PawnUI_OnSocketUpdate", function()
-			if PawnSocketingTooltip then
+			if _G.PawnSocketingTooltip then
 				self:add2Table(self.ttList, "PawnSocketingTooltip")
 				self:Unhook("PawnUI_OnSocketUpdate")
-			end
-		end)
-		self:SecureHook("PawnUI_OnReforgingUpdate", function()
-			if PawnReforgingTooltip then
-				self:add2Table(self.ttList, "PawnReforgingTooltip")
-				self:Unhook("PawnUI_OnReforgingUpdate")
 			end
 		end)
 	end
