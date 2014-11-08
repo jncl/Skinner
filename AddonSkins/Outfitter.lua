96f3fbafd722e48dd51e110a8ccea19bac42f18f
Jncl <jncl@mac.com>
2014-11-08 23:25:40 +0000

    Updated Outfitter & tekKonfig skins

--- AddonSkins/Outfitter.lua
+++ AddonSkins/Outfitter.lua
@@ -1,21 +1,23 @@
 local aName, aObj = ...
 if not aObj:isAddonEnabled("Outfitter") then return end
+local _G = _G
 
 function aObj:Outfitter()
 
 	-- disable Frame Level & Strata being changed
-	Outfitter.SetFrameLevel = function() end
-	Outfitter.SetFrameStrata = function() end
+	_G.Outfitter.SetFrameLevel = function() end
+	_G.Outfitter.SetFrameStrata = function() end
 
 	 -- wait until Outfitter has been initialized
-	if not Outfitter.Initialized then
+	if not _G.Outfitter.Initialized then
 		self:ScheduleTimer("checkAndRunAddOn", 0.1, "Outfitter")
 		return
 	end
 
 	-- minimap button texture background
 	if self.db.profile.MinimapButtons.skin then
-		self:removeRegions(OutfitterMinimapButton, {1})
+		self:removeRegions(_G.OutfitterMinimapButton, {1})
+		_G.OutfitterMinimapButton.CurrentOutfitTexture:SetDrawLayer("ARTWORK")
 	end
 
 	local function skinOutfitBars(this, ...)
@@ -24,21 +26,29 @@ function aObj:Outfitter()
 		if not this.Bars then return end
 
 		for i = 1, #this["Bars"] do
-			local oBar = _G["OutfitterOutfitBar"..i]
-			if not aObj.skinFrame[oBar] then
+			local oBar = _G["OutfitterOutfitBar" .. i]
+			if not oBar.sf then
 				aObj:addSkinFrame{obj=oBar, kfs=true, x1=-2, y1=3, x2=0, y2=1}
 			end
 		end
 		for i = 1, 2 do
-			local dBar = this["DragBar"..i]
-			if dBar and not aObj.skinFrame[dBar] then
+			local dBar = this["DragBar" .. i]
+			if dBar
+			and not dBar.sf
+			then
 				aObj:addSkinFrame{obj=dBar, kfs=true, x1=-2, y1=3, x2=0, y2=1}
-				if not dBar.Vertical then dBar:SetWidth(20)
-				else dBar:SetHeight(20) end
+				if not dBar.Vertical then
+					dBar:SetWidth(20)
+				else
+					dBar:SetHeight(20)
+				end
 				-- hook this to handle an Orientation change
 				aObj:SecureHook(dBar, "SetVerticalOrientation", function(this, pVertical)
-					if not this.Vertical then this:SetWidth(20)
-					else this:SetHeight(20) end
+					if not this.Vertical then
+						this:SetWidth(20)
+					else
+						this:SetHeight(20)
+					end
 				end)
 			end
 		end
@@ -47,7 +57,10 @@ function aObj:Outfitter()
 
 	local function skinDropDown(obj)
 
-		if not aObj.db.profile.TexturedDD then aObj:keepFontStrings(obj) return end
+		if not aObj.db.profile.TexturedDD then
+			aObj:keepFontStrings(obj)
+			return
+		end
 		-- dropdown
 		aObj:moveObject{obj=obj.LeftTexture, x=2, y=-22}
 		obj.LeftTexture:SetAlpha(0)
@@ -66,31 +79,35 @@ function aObj:Outfitter()
 	local function skinMultiStats(obj)
 
 		for i = 1, #obj.ConfigLines do
-			if not aObj.skinned[obj.ConfigLines[i]] then
+			if not obj.ConfigLines[i].sknd then
 				skinDropDown(obj.ConfigLines[i].StatMenu)
 				skinDropDown(obj.ConfigLines[i].OpMenu)
-				if obj.ConfigLines[i].DeleteButton then aObj:skinButton{obj=obj.ConfigLines[i].DeleteButton} end
+				if obj.ConfigLines[i].DeleteButton then
+					aObj:skinButton{obj=obj.ConfigLines[i].DeleteButton}
+				end
 			end
 		end
 
 	end
 
  	-- hook these to handle the Outfit Bars
-	self:SecureHook(Outfitter.OutfitBar, "UpdateBar", function(this, ...)
+	self:SecureHook(_G.Outfitter.OutfitBar, "UpdateBar", function(this, ...)
 		skinOutfitBars(this, ...)
 	end)
-	self:SecureHook(Outfitter.OutfitBar, "DragBar_OnClick", function(this)
-		if OutfitBarSettingsDialog then
-			self:Unhook(Outfitter.OutfitBar, "DragBar_OnClick")
-			self:applySkin(OutfitBarSettingsDialog)
+	self:SecureHook(_G.Outfitter.OutfitBar, "DragBar_OnClick", function(this)
+		if _G.OutfitBarSettingsDialog then
+			self:applySkin(_G.OutfitBarSettingsDialog)
+			self:Unhook(_G.Outfitter.OutfitBar, "DragBar_OnClick")
 		end
 	end)
 
 	-- hook this to skin additional Shopping tooltips
-	self:SecureHook(Outfitter._ExtendedCompareTooltip, "AddShoppingLink", function(this, ...)
+	self:SecureHook(_G.Outfitter._ExtendedCompareTooltip, "AddShoppingLink", function(this, ...)
 		for i = 1, #this.Tooltips do
 			if self.db.profile.Tooltips.skin then
-				if self.db.profile.Tooltips.style == 3 and not self.skinned[this.Tooltips[i]] then
+				if self.db.profile.Tooltips.style == 3
+				and not self.skinned[this.Tooltips[i]]
+				then
 					this.Tooltips[i]:SetBackdrop(self.Backdrop[1])
 				end
 				self:skinTooltip(this.Tooltips[i])
@@ -99,31 +116,31 @@ function aObj:Outfitter()
 	end)
 
 -->>--	Outfitter Frame
-	self:SecureHook(OutfitterFrame, "Show", function(this, ...)
-		self:getChild(OutfitterFrame, 8):SetAlpha(0) -- hide band on the left
-		self:addSkinFrame{obj=OutfitterFrame, kfs=true--[=[, bg=true--]=], x1=-2, y1=2, x2=2, y2=-7}
-		self:Unhook(OutfitterFrame, "Show")
+	self:SecureHook(_G.OutfitterFrame, "Show", function(this, ...)
+		self:getChild(_G.OutfitterFrame, 8):SetAlpha(0) -- hide band on the left
+		self:addSkinFrame{obj=_G.OutfitterFrame, kfs=true, x1=-2, y1=2, x2=2, y2=-7}
+		self:Unhook(_G.OutfitterFrame, "Show")
 	end)
 
 -->>--	Main Frame
-	self:keepRegions(OutfitterMainFrame, {2, 3}) -- N.B. region 2 is text, 3 is background texture
-	self:removeRegions(OutfitterMainFrameScrollbarTrench)
-	self:skinScrollBar{obj=OutfitterMainFrameScrollFrame}
+	self:keepRegions(_G.OutfitterMainFrame, {2, 3}) -- N.B. region 2 is text, 3 is background texture
+	self:removeRegions(_G.OutfitterMainFrameScrollbarTrench)
+	self:skinScrollBar{obj=_G.OutfitterMainFrameScrollFrame}
 	-- m/p buttons
-	for i = 0, Outfitter.cMaxDisplayedItems - 1 do
-		local iBtn = "OutfitterItem"..i
-		self:skinButton{obj=_G[iBtn.."CategoryExpand"], mp=true} -- treat as a texture
-		self:SecureHook(_G[iBtn.."CategoryExpand"], "SetNormalTexture", function(this, nTex)
+	for i = 0, _G.Outfitter.cMaxDisplayedItems - 1 do
+		local iBtn = "OutfitterItem" .. i
+		self:skinButton{obj=_G[iBtn .. "CategoryExpand"], mp=true} -- treat as a texture
+		self:SecureHook(_G[iBtn .. "CategoryExpand"], "SetNormalTexture", function(this, nTex)
 			self:checkTex{obj=this, nTex=nTex}
 		end)
 	end
 
 -->>--	Outfitter Tabs
-	self:skinTabs{obj=OutfitterFrame, lod=true, x1=6, y1=0, x2=-6, y2=2}
+	self:skinTabs{obj=_G.OutfitterFrame, lod=true, x1=6, y1=0, x2=-6, y2=2}
 
 -->>--	New Outfit Panel
 
-	self:SecureHook(Outfitter, "CreateNewOutfit", function(this)
+	self:SecureHook(_G.Outfitter, "CreateNewOutfit", function(this)
 		local frame = this.NameOutfitDialog
 		self:skinEditBox{obj=frame.Name, regs={9, 15, 16}}
 		self:moveObject{obj=frame.Title, y=-6}
@@ -140,17 +157,17 @@ function aObj:Outfitter()
 		self:skinButton{obj=frame.CancelButton}
 		self:skinButton{obj=frame.DoneButton}
 		self:addSkinFrame{obj=frame, kfs=true, nb=true}
-		self:Unhook(Outfitter, "CreateNewOutfit")
+		self:Unhook(_G.Outfitter, "CreateNewOutfit")
 	end)
-	self:SecureHook(Outfitter, "BeginCombiProgress", function(this, ...)
+	self:SecureHook(_G.Outfitter, "BeginCombiProgress", function(this, ...)
 		local cpd = this.CombiProgressDialog
 		self:glazeStatusBar(cpd.ProgressBar, 0,  nil)
 		self:skinButton{obj=cpd.CancelButton}
 		self:addSkinFrame{obj=cpd.ContentFrame}
-		self:Unhook(Outfitter, "BeginCombiProgress")
+		self:Unhook(_G.Outfitter, "BeginCombiProgress")
 	end)
 -->>--	Rebuild Outfit Panel
-	self:SecureHook(Outfitter, "OpenRebuildOutfitDialog", function(this, ...)
+	self:SecureHook(_G.Outfitter, "OpenRebuildOutfitDialog", function(this, ...)
 		local frame = this.RebuildOutfitDialog
 		self:moveObject{obj=frame.Title, y=-6}
 		self:addSkinFrame{obj=frame.StatsSection}
@@ -163,29 +180,29 @@ function aObj:Outfitter()
 		self:skinButton{obj=frame.CancelButton}
 		self:skinButton{obj=frame.DoneButton}
 		self:addSkinFrame{obj=frame, kfs=true, nb=true}
-		self:Unhook(Outfitter, "OpenRebuildOutfitDialog")
+		self:Unhook(_G.Outfitter, "OpenRebuildOutfitDialog")
 	end)
 
 -->>--	ChooseIcon Dialog
-	self:getChild(OutfitterChooseIconDialog, 1):SetBackdrop(nil) -- remove textures from anonymous frame
-	self:skinDropDown{obj=OutfitterChooseIconDialogIconSetMenu}
-	self:skinEditBox{obj=OutfitterChooseIconDialogFilterEditBox, regs={6}}
-	self:skinScrollBar{obj=OutfitterChooseIconDialogScrollFrame}
-	self:addSkinFrame{obj=OutfitterChooseIconDialog, x1=12, y1=-12, x2=-16, y2=16}
+	self:getChild(_G.OutfitterChooseIconDialog, 1):SetBackdrop(nil) -- remove textures from anonymous frame
+	self:skinDropDown{obj=_G.OutfitterChooseIconDialogIconSetMenu}
+	self:skinEditBox{obj=_G.OutfitterChooseIconDialogFilterEditBox, regs={6}}
+	self:skinScrollBar{obj=_G.OutfitterChooseIconDialogScrollFrame}
+	self:addSkinFrame{obj=_G.OutfitterChooseIconDialog, x1=12, y1=-12, x2=-16, y2=16}
 
 -->>--	EditScript Dialog
-	if OutfitterEditScriptDialog then
-		self:skinDropDown{obj=OutfitterEditScriptDialogPresetScript}
-		self:keepFontStrings(OutfitterEditScriptDialogSourceScript)
-		self:skinScrollBar{obj=OutfitterEditScriptDialogSourceScript, noRR=true}
-		self:addSkinFrame{obj=OutfitterEditScriptDialog, kfs=true, x2=1, y2=-5}
+	if _G.OutfitterEditScriptDialog then
+		skinDropDown(_G.OutfitterEditScriptDialogPresetScript)
+		self:keepFontStrings(_G.OutfitterEditScriptDialogSourceScript)
+		self:skinScrollBar{obj=_G.OutfitterEditScriptDialogSourceScript, noRR=true}
+		self:addSkinFrame{obj=_G.OutfitterEditScriptDialog, kfs=true, x2=1, y2=-5}
 		-- Tabs
-		self:skinTabs{obj=OutfitterEditScriptDialog, lod=true, x1=6, y1=0, x2=-6, y2=2}
+		self:skinTabs{obj=_G.OutfitterEditScriptDialog, lod=true, x1=6, y1=0, x2=-6, y2=2}
 	end
 	-- hook to skin script generated objects
-	self:SecureHook(OutfitterEditScriptDialog, "ConstructSettingsFields", function(this, pSettings)
-		for k, v in pairs(this.FrameCache) do
-			for l, w in pairs(v) do
+	self:SecureHook(_G.OutfitterEditScriptDialog, "ConstructSettingsFields", function(this, pSettings)
+		for k, v in _G.pairs(this.FrameCache) do
+			for l, w in _G.pairs(v) do
 				if k == "ScrollableEditBox" then
 					-- self:skinScrollBar{obj=v}
 					-- self:addSkinFrame{obj=self:getChild(v, 1)}
@@ -200,10 +217,11 @@ function aObj:Outfitter()
 	end)
 
 -->>-- Outfit Bars
-	self:ScheduleTimer(skinOutfitBars, 1, Outfitter.OutfitBar) -- wait for a second before skinning the Outfit Bars
+	self:ScheduleTimer(skinOutfitBars, 1, _G.Outfitter.OutfitBar) -- wait for a second before skinning the Outfit Bars
 
 -->>-- Character panel buttons
-	self:skinButton{obj=OutfitterEnableAll}
-	self:skinButton{obj=OutfitterEnableNone}
+	self:skinButton{obj=_G.OutfitterEnableAll}
+	self:skinButton{obj=_G.OutfitterEnableNone}
+	self:addButtonBorder{obj=_G.OutfitterButton, x1=9, y1=-2, x2=-10, y2=2}
 
 end
--- AddonSkins/tekKonfig.lua
+++ AddonSkins/tekKonfig.lua
@@ -1,4 +1,5 @@
 local aName, aObj = ...
+local _G = _G
 -- This is a Framework
 
 local allHooked
@@ -7,55 +8,12 @@ function aObj:tekKonfig()
 
 	if allHooked then return end
 
-	local tKDd, ver = tKDd, ver or LibStub("tekKonfig-Dropdown", true)
-	if tKDd and not self:IsHooked(tKDd, "new") then
-		self:RawHook(tKDd, "new", function(parent, label, ...)
---			self:Debug("tKDd:[%s, %s]", parent, label)
-			local frame, text, container = self.hooks[tKDd].new(parent, label, ...)
-			if not self.db.profile.TexturedDD then self:keepFontStrings(frame)
-			else
-				local leftTex, midTex, rightTex
-				if ver == 2 then
-					leftTex, midTex, rightTex = frame:GetRegions()
-				else
-					leftTex, rightTex, midTex = frame:GetRegions()
-				end
-				leftTex:SetAlpha(0)
-				midTex:SetTexture(self.itTex)
-				midTex:SetHeight(18)
-				midTex:SetTexCoord(0, 1, 0, 1)
-				rightTex:SetAlpha(0)
-			end
-			return frame, text, container
-		end, true)
-	end
-
-	local tKG = tKG or LibStub("tekKonfig-Group", true)
-	if tKG and not self:IsHooked(tKG, "new") then
-		self:RawHook(tKG, "new", function(parent, label, ...)
---			self:Debug("tKG:[%s, %s]", parent, label)
-			local box = self.hooks[tKG].new(parent, label, ...)
-			self:applySkin(box)
-			return box
-		end, true)
-	end
-
-	local tKS = tKS or LibStub("tekKonfig-Scroll", true)
-	if tKS and not self:IsHooked(tKS, "new") then
-		self:RawHook(tKS, "new", function(parent, offset, step)
---			self:Debug("tKS:[%s, %s, %s]", parent, offset, step)
-			local frame, up, down, border = self.hooks[tKS].new(parent, offset, step)
-			self:skinSlider{obj=frame, size=3}
-			border:SetBackdrop(nil)
-			return frame, up, down, border
-		end, true)
-	end
-
-	local tKAP = tKAP or LibStub("tekKonfig-AboutPanel", true)
+	local tKAP = tKAP or _G.LibStub:GetLibrary("tekKonfig-AboutPanel", true)
 	if tKAP and not self:IsHooked(tKAP, "OpenEditbox") then
 		self:SecureHook(tKAP, "OpenEditbox", function(this)
 --			self:Debug("tKAP:[%s, %s]", this, tKAP.editbox)
-			if not self.skinned[tKAP.editbox] then
+			if not tKAP.editbox.sknd then
+				tKAP.editbox.sknd = true
 				tKAP.editbox:SetHeight(24)
 				local l, r, t, b = tKAP.editbox:GetTextInsets()
 				tKAP.editbox:SetTextInsets(l + 5, r + 5, t, b)
@@ -66,15 +24,54 @@ function aObj:tekKonfig()
 		end)
 	end
 
-	local tkB = tkB or LibStub("tekKonfig-Button", true)
-	if tkB and not self:IsHooked(tkB, "new") then
-		self:RawHook(tkB, "new", function(...)
-			local btn = self.hooks[tkB].new(...)
+	local tKB = tKB or _G.LibStub:GetLibrary("tekKonfig-Button", true)
+	if tKB and not self:IsHooked(tKB, "new") then
+		self:RawHook(tKB, "new", function(parent, ...)
+			local btn = self.hooks[tKB].new(parent, ...)
 			self:skinButton{obj=btn}
 			return btn
 		end)
 	end
 
-	if tKDd and tKG and tKS and tKAP and tkB then allHooked = true end
+	-- tekKonfig-Checkbox
+
+	local tKDd = tKDd or _G.LibStub:GetLibrary("tekKonfig-Dropdown", true)
+	if tKDd and not self:IsHooked(tKDd, "new") then
+		self:RawHook(tKDd, "new", function(parent, label, ...)
+			-- self:Debug("tKDd:[%s, %s, %s]", parent, label, ...)
+			local frame, text, container, labeltext = self.hooks[tKDd].new(parent, label, ...)
+			if not self.db.profile.TexturedDD then self:keepFontStrings(frame)
+			else
+				local leftTex, rightTex, icon, midTex = frame:GetRegions()
+				leftTex:SetAlpha(0)
+				midTex:SetTexture(self.itTex)
+				midTex:SetPoint("LEFT", leftTex, "RIGHT", -5, 1)
+				midTex:SetPoint("RIGHT", rightTex, "LEFT", 5, 1)
+				midTex:SetHeight(18)
+				midTex:SetTexCoord(0, 1, 0, 1)
+				rightTex:SetAlpha(0)
+			end
+			if self.db.profile.DropDownButtons then
+				self:addSkinFrame{obj=frame, aso={ng=true}, nb=true, x1=16, y1=-1, x2=-15, y2=5}
+				self:addButtonBorder{obj=self:getChild(frame, 1), es=12, ofs=-2}
+			end
+			return frame, text, container, labeltext
+		end, true)
+	end
+
+	-- tekKonfig-FadeIn
+	-- tekKonfig-Heading
+
+	local tKS = tKS or _G.LibStub:GetLibrary("tekKonfig-Slider", true)
+	if tKS and not self:IsHooked(tKS, "new") then
+		self:RawHook(tKS, "new", function(parent, label, lowvalue, highvalue, ...)
+--			self:Debug("tKS:[%s, %s, %s, %s, %s]", parent, label, lowvalue, highvalue, ...)
+			local slider, text, container, low, high = self.hooks[tKS].new(parent, label, lowvalue, highvalue, ...)
+			self:skinSlider{obj=slider, size=3}
+			return slider, text, container, low, high
+		end, true)
+	end
+
+	if tKAP and tKB and tKDd and tKS then allHooked = true end
 
 end
