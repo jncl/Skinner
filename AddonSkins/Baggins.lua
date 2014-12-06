86000f540b83f66c5cadcdf9010bda345888bb3b
Jncl <jncl@mac.com>
2014-12-06 14:07:23 +0000

    Updated Baggins skin

--- AddonSkins/Baggins.lua
+++ AddonSkins/Baggins.lua
@@ -1,11 +1,12 @@
 local aName, aObj = ...
 if not aObj:isAddonEnabled("Baggins") then return end
+local _G = _G
 
 function aObj:Baggins()
 	if not self.db.profile.ContainerFrames then return end
 
 	-- check for Ace3 version
-	local a3Ver = LibStub("AceAddon-3.0"):GetAddon("Baggins", true)
+	local Baggins = _G.LibStub("AceAddon-3.0"):GetAddon("Baggins", true)
 
 	-- setup default colours
 	if not self.db.profile.BagginsBBC then
@@ -15,16 +16,16 @@ function aObj:Baggins()
 	local function skinBag(id, frame)
 		aObj:addSkinFrame{obj=frame, y1=-3, x2=-3}
 		if Baggins.db.profile.bags[id].isBank then
-			frame:SetBackdropColor(aObj.db.profile.BagginsBBC.r, aObj.db.profile.BagginsBBC.g, aObj.db.profile.BagginsBBC.b, aObj.db.profile.BagginsBBC.a)
+			frame.sf:SetBackdropColor(aObj.db.profile.BagginsBBC.r, aObj.db.profile.BagginsBBC.g, aObj.db.profile.BagginsBBC.b, aObj.db.profile.BagginsBBC.a)
 		end
+		frame.SetBackdrop = function() end
 		frame.SetBackdropColor = function() end
-		if a3Ver then
-			if a3Ver.db.profile.skin == "blizzard" then
-				frame.icon:SetTexture(nil)
-				frame.portrait:SetTexture(nil)
-			elseif a3Ver.db.profile.skin == "oSkin" then
-				frame.tfade:SetTexture(nil)
-			end
+		frame.SetBackdropBorderColor = function() end
+		if Baggins.db.profile.skin == "blizzard" then
+			frame.icon:SetTexture(nil)
+			frame.portrait:SetTexture(nil)
+		elseif Baggins.db.profile.skin == "oSkin" then
+			frame.tfade:SetTexture(nil)
 		end
 	end
 
@@ -34,7 +35,7 @@ function aObj:Baggins()
 	end)
 
 	-- skin existing bags
-	for k, v in pairs(Baggins.bagframes) do
+	for k, v in _G.pairs(Baggins.bagframes) do
 		skinBag(k, v)
 	end
 
