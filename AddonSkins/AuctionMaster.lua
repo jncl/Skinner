93df7ce93b492c330e022a51309fe575fbd1a395
Jncl <jncl@mac.com>
2014-12-21 17:34:00 +0000

    Updated AuctionMaster & MasterPlan skins

--- AddonSkins/AuctionMaster.lua
+++ AddonSkins/AuctionMaster.lua
@@ -2,7 +2,7 @@ local aName, aObj = ...
 if not aObj:isAddonEnabled("AuctionMaster") then return end
 local _G = _G
 
-function aObj:AuctionMaster()
+function aObj:AuctionMaster() -- version 6.0.0
 
 	local vendor = _G.vendor
 
@@ -10,13 +10,28 @@ function aObj:AuctionMaster()
 		local table = tab2 and vendor[tab1][tab2].itemTable.cols or vendor[tab1].itemTable.cols
 		for _, obj in _G.pairs(table) do
 			if obj.sortButton.sortId then
-				aObj:keepRegions(obj.sortButton, {4, 5, 6}) -- N.B. region 4 is the text, 5 is the arrow, 6 is the highlight
+				aObj:keepRegions(obj.sortButton, {4, 5, 6}) -- N.B. region 4 is text, 5 is arrow, 6 is highlight
 				aObj:addSkinFrame{obj=obj.sortButton, nb=true, bd=6}
 				aObj:adjHeight{obj=obj.sortButton, adj=1}
 			else
 				obj.sortButton.sb:Hide()
 			end
 		end
+		local obj = tab2 and vendor[tab1][tab2].itemTable or vendor[tab1].itemTable
+		if obj.upperList
+		or obj.cfg.size == 100
+		or not obj.sortFrame
+		then
+			return
+		end
+		-- find and remove background frame's texture
+		local kids = {obj.sortFrame:GetChildren()}
+		for _, child in _G.ipairs(kids) do
+			if child:IsObjectType("Frame") then
+				self:removeRegions(child, {1})
+			end
+		end
+		kids = _G.null
 	end
 
 	-- logo
@@ -27,18 +42,25 @@ function aObj:AuctionMaster()
 	self:glazeStatusBar(vendor.OwnAuctions.statusBar, 0,  nil)
 	skinSortBtns("OwnAuctions")
 	self:skinAllButtons{obj=vendor.OwnAuctions.itemTable.cmdFrame}
-	self:removeRegions(vendor.OwnAuctions.itemTable.innerFrame, {1})
+	self:removeRegions(vendor.OwnAuctions.itemTable.cmdFrame, {1}) -- background
+	self:removeRegions(vendor.OwnAuctions.itemTable.innerFrame, {1}) -- background
 
 	-- Seller Tab
 	self:keepFontStrings(vendor.Seller.frame)
-	skinSortBtns("Seller", "inventorySeller")
-	self:removeRegions(vendor.Seller.inventorySeller.itemTable.innerFrame, {1})
+	skinSortBtns("Seller")
+	self:skinAllButtons{obj=vendor.Seller.itemTable.cmdFrame}
+	self:removeRegions(vendor.Seller.itemTable.cmdFrame, {1}) -- background
+	self:removeRegions(vendor.Seller.itemTable.innerFrame, {1}) -- background
 	self:addButtonBorder{obj=vendor.Seller.itemBut}
 	self:skinEditBox{obj=vendor.Seller.stackDropDown.editBox, regs={9}}
 	self:skinEditBox{obj=vendor.Seller.countDropDown.editBox, regs={9}}
 	self:skinMoneyFrame{obj=vendor.Seller.startPriceBut, moveSEB=true, moveGEB=true}
 	self:skinMoneyFrame{obj=vendor.Seller.buyoutPriceBut, moveSEB=true, moveGEB=true}
 	self:glazeStatusBar(vendor.Seller.statusBar, 0,  nil)
+
+	-->>-- inventorySeller subframe
+	skinSortBtns("Seller", "inventorySeller")
+	self:removeRegions(vendor.Seller.inventorySeller.itemTable.innerFrame, {1})
 
 	-->>-- Item Settings frame (press edit button to display)
 	self:addSkinFrame{obj=self:getChild(vendor.ItemSettings.frame, 3), nb=true} -- type frame
@@ -75,7 +97,8 @@ function aObj:AuctionMaster()
 	self:addButtonBorder{obj=vendor.SearchTab.scan} -- texture is changed depending upon status
 	skinSortBtns("SearchTab")
 	self:skinAllButtons{obj=vendor.SearchTab.itemTable.cmdFrame}
-	self:removeRegions(vendor.SearchTab.itemTable.innerFrame, {1})
+	self:removeRegions(vendor.SearchTab.itemTable.cmdFrame, {1}) -- background
+	self:removeRegions(vendor.SearchTab.itemTable.innerFrame, {1}) -- background
 	-- saved searches
 	self:addSkinFrame{obj=vendor.SearchTab.searchList.frame}
 	self:addButtonBorder{obj=vendor.SearchTab.searchList.rename, ofs=0}
@@ -103,6 +126,8 @@ function aObj:AuctionMaster()
 	self:SecureHook(vendor.GuiTools, "GetNextId", function(this)
 		if not vendor.OwnAuctions.itemTable.cols[1].sortButton then
 			self:ScheduleTimer(skinSortBtns, 0.1, "OwnAuctions")
+		elseif not vendor.Seller.itemTable.cols[1].sortButton then
+			self:ScheduleTimer(skinSortBtns, 0.1, "Seller")
 		elseif not vendor.Seller.inventorySeller.itemTable.cols[1].sortButton then
 			self:ScheduleTimer(skinSortBtns, 0.1, "Seller", "inventorySeller")
 		elseif not vendor.SearchTab.itemTable.cols[1].sortButton then
--- AddonSkins/MasterPlan.lua
+++ AddonSkins/MasterPlan.lua
@@ -4,19 +4,88 @@ local _G = _G
 
 function aObj:MasterPlan() -- LoD
 
-	-- skin tab
-	local tab = _G.GarrisonMissionFrameTab3
-	self:keepRegions(tab, {7, 8, 9, 10})
-	self:addSkinFrame{obj=tab, noBdr=self.isTT, x1=9, y1=2, x2=-9, y2=0}
-	self:setInactiveTab(tab.sf)
-	_G.PanelTemplates_SetNumTabs(_G.GarrisonMissionFrame, 3)
+	local function skinTab()
+		local tab = _G.GarrisonMissionFrameTab3
+		self:keepRegions(tab, {7, 8, 9, 10})
+		self:addSkinFrame{obj=tab, noBdr=self.isTT, x1=9, y1=2, x2=-9, y2=0}
+		_G.PanelTemplates_SetNumTabs(_G.GarrisonMissionFrame, 3)
+		if _G.GarrisonMissionFrame.selectedTab == 3 then
+			self:setActiveTab(tab.sf)
+		else
+			self:setInactiveTab(tab.sf)
+		end
+	end
+	-- skin tab after short delay
+	self:ScheduleTimer(skinTab, 0.2)
 
-	-- skin resized Border frame
-	local bf = _G.GarrisonMissionFrameMissions.CompleteDialog.BorderFrame
-	self:addSkinFrame{obj=bf, ft="u", kfs=true}
+	-- Garrison Missions Frame - Active Missions Tab
+	local activeUI = self:getChild(_G.GarrisonMissionFrameMissions, 4)
+	local sf = self:getChild(_G.GarrisonMissionFrameMissions, 5) -- scroll frame
+	self:skinSlider{obj=self:getChild(sf, 1), adj=-4}
+	local sc = sf:GetScrollChild()
 
-	-- skin sort indicator button
-	local sib = self:getChild(_G.GarrisonMissionFrameMissions, _G.GarrisonMissionFrameMissions:GetNumChildren())
-	self:removeRegions(sib, {1})
+	local function skinMissionButtons()
+		local kids = {sc:GetChildren()}
+		for _, child in _G.ipairs(kids) do
+			if child:IsObjectType("Button") then
+				child:DisableDrawLayer("BACKGROUND")
+				child:DisableDrawLayer("OVERLAY") -- shadow background
+				child:DisableDrawLayer("BORDER")
+				self:removeRegions(child, {16, 17, 18, 19}) -- highlight corners
+				for i = 1, 3 do
+					child.followers[i].ring:SetTexture(nil)
+				end
+				for i = 1, #child.rewards do
+					self:addButtonBorder{obj=child.rewards[i], reParent={child.rewards[i].quantity}}
+				end
+			end
+		end
+		kids = _G.null
+	end
+	-- hook activeUI SetShown to skin buttons
+	self:SecureHook(activeUI, "SetShown", function(this, action)
+		if action then
+			self:ScheduleTimer(skinMissionButtons, 0.2, nil) -- wait for buttons to be created
+		end
+	end)
+	-- skin any existing buttons, first time displayed
+	skinMissionButtons()
+
+	-- ActiveUI lootframe
+	self:addSkinFrame{obj=activeUI.lootFrame, kfs=true}
+	local lootContainer = self:getChild(activeUI.lootFrame, 2)
+	local function skinLootContainer()
+		for i = 1, #lootContainer.items do
+			lootContainer.items[i].Border:SetTexture(nil)
+			self:addButtonBorder{obj=lootContainer.items[i], relTo=lootContainer.items[i].Icon, ofs=3}
+		end
+		for i = 1, #lootContainer.followers do
+			lootContainer.followers[i].PortraitRing:SetTexture(nil)
+			lootContainer.followers[i].LevelBorder:SetAlpha(0) -- texture changed
+			lootContainer.followers[i].PortraitRingCover:SetTexture(nil)
+		end
+	end
+	-- skin any existing followers & items
+	skinLootContainer()
+	-- hook this to skin new followers & items
+	self:SecureHook(activeUI.lootFrame, "Show", function(this)
+		skinLootContainer()
+	end)
+
+	-- Garrison Missions Frame - Available Missions Tab
+	local sortIndicator = self:getChild(_G.GarrisonMissionFrameMissions, 6)
+	self:removeRegions(sortIndicator, {1}) -- background
+	local roamingParty = self:getChild(_G.GarrisonMissionFrameMissions, 7)
+	for i = 1, 3 do
+		local btn = self:getChild(roamingParty, i)
+		self:removeRegions(btn, {2}) -- portrait ring
+	end
+
+	-- Garrison Missions Frame - Available Missions Tab - Mission Page
+	-- Get Suggested Groups button
+	local lfgButton = self:getChild(_G.GarrisonMissionFrame.MissionTab.MissionPage.Stage, 2)
+	self:removeRegions(lfgButton, {2}) -- ring texture
+	-- minimize button
+	self:skinButton{obj=_G.GarrisonMissionFrame.MissionTab.MissionPage.MinimizeButton, ob="-"}
 
 end
