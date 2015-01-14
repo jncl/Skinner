f0414f1805fc1bbd188bf0044d0accac8d4cec9f
Jncl <jncl@mac.com>
2015-01-14 15:51:30 +0000

    Updated Altoholic & MasterPlan skins

--- AddonSkins/Altoholic.lua
+++ AddonSkins/Altoholic.lua
@@ -2,11 +2,11 @@ local aName, aObj = ...
 if not aObj:isAddonEnabled("Altoholic") then return end
 local _G = _G
 
-local function skinMenuItms(itmName, cnt)
+local function skinMenuItms(frameName, cnt)
 
 	local itm
 	for i = 1, cnt do
-		itm = _G[itmName..i]
+		itm = frameName["MenuItem" .. i]
 		aObj:keepRegions(itm, {3, 4}) -- N.B. region 3 is the highlight, 4 is the text
 		aObj:applySkin(itm)
 	end
@@ -30,22 +30,12 @@ function aObj:Altoholic()
 	self:skinEditBox{obj=_G.AltoholicFrame_SearchEditBox, regs={9}}
 	self:addSkinFrame{obj=_G.AltoholicFrame, kfs=true, y1=-11, y2=3}
 	-- Tabs
-	for i = 1, _G.AltoholicFrame.numTabs do
-		local tabObj = _G["AltoholicFrameTab"..i]
-		self:keepRegions(tabObj, {7, 8}) -- N.B. region 7 is text, 8 is highlight
-		local tabSF = self:addSkinFrame{obj=tabObj, noBdr=self.isTT, x1=6, y1=0, x2=-6, y2=2}
-		if i == 1 then
-			if self.isTT then self:setActiveTab(tabSF) end
-		else
-			if self.isTT then self:setInactiveTab(tabSF) end
-		end
-	end
-	self.tabFrames[_G.AltoholicFrame] = true
+	self:skinTabs{obj=_G.AltoholicFrame}
 
 -->>-- Message Box
 	self:addSkinFrame{obj=_G.AltoMsgBox, kfs=true, x1=6, y1=-6, x2=-6, y2=6}
 
--->>--	Tooltip
+-->>-- Tooltip
 	if self.db.profile.Tooltips.skin then
 		if self.db.profile.Tooltips.style == 3 then _G.AltoTooltip:SetBackdrop(self.backdrop) end
 		self:SecureHookScript(_G.AltoTooltip, "OnShow", function(this)
@@ -55,17 +45,18 @@ function aObj:Altoholic()
 
 -->>-- Account Sharing option menu panel (buttons and panels)
 	-- make sure icons are visible by changing their draw layer
-	_G.AltoholicAccountSharingOptionsIconNever:SetDrawLayer("OVERLAY")
-	_G.AltoholicAccountSharingOptionsIconAsk:SetDrawLayer("OVERLAY")
-	_G.AltoholicAccountSharingOptionsIconAuto:SetDrawLayer("OVERLAY")
+	_G.AltoholicAccountSharingOptions.IconNever:SetDrawLayer("OVERLAY")
+	_G.AltoholicAccountSharingOptions.IconAsk:SetDrawLayer("OVERLAY")
+	_G.AltoholicAccountSharingOptions.IconAuto:SetDrawLayer("OVERLAY")
+
 	self:skinScrollBar{obj=_G.AltoholicFrameSharingClientsScrollFrame}
 	self:addSkinFrame{obj=_G.AltoholicFrameSharingClients}
 	self:skinScrollBar{obj=_G.AltoholicFrameSharedContentScrollFrame}
 -->>-- SharedContent option menu panel
 	self:skinButton{obj=_G.AltoholicSharedContent_ToggleAll, mp2=true}
 	self:addSkinFrame{obj=_G.AltoholicFrameSharedContent}
-	for i = 1, 10 do
-		self:skinButton{obj=_G["AltoholicFrameSharedContentEntry"..i.."Collapse"], mp2=true}
+	for i = 1, 14 do
+		self:skinButton{obj=_G["AltoholicFrameSharedContentEntry" .. i .. "Collapse"], mp2=true}
 	end
 
 -->>-- Account Sharing frame
@@ -74,8 +65,9 @@ function aObj:Altoholic()
 	self:skinEditBox{obj=_G.AltoAccountSharing_AccTargetEditBox, regs={9}}
 	self:skinScrollBar{obj=_G.AltoholicFrameAvailableContentScrollFrame}
 	for i = 1, 10 do
-		self:skinButton{obj=_G["AltoholicFrameAvailableContentEntry"..i.."Collapse"], mp2=true}
+		self:skinButton{obj=_G["AltoholicFrameAvailableContentEntry" .. i .. "Collapse"], mp2=true}
 	end
+	self:addSkinFrame{obj=_G.AltoholicFrameAvailableContent}
 	self:addSkinFrame{obj=_G.AltoAccountSharing}
 
 end
@@ -85,29 +77,31 @@ function aObj:Altoholic_Summary() -- LoD
 	if self.modBtns then
 		-- skin minus/plus buttons
 		for i = 1, 14 do
-			self:skinButton{obj=_G["AltoholicFrameSummaryEntry"..i.."Collapse"], mp2=true}
-			self:skinButton{obj=_G["AltoholicFrameBagUsageEntry"..i.."Collapse"], mp2=true}
-			self:skinButton{obj=_G["AltoholicFrameActivityEntry"..i.."Collapse"], mp2=true}
-			self:skinButton{obj=_G["AltoholicFrameSkillsEntry"..i.."Collapse"], mp2=true}
+			self:skinButton{obj=_G["AltoholicFrameSummaryEntry" .. i .. "Collapse"], mp2=true}
+			self:skinButton{obj=_G["AltoholicFrameBagUsageEntry" .. i .. "Collapse"], mp2=true}
+			self:skinButton{obj=_G["AltoholicFrameActivityEntry" .. i .. "Collapse"], mp2=true}
+			self:skinButton{obj=_G["AltoholicFrameSkillsEntry" .. i .. "Collapse"], mp2=true}
 		end
 	end
+	self:skinScrollBar{obj=_G.AltoholicFrameSummaryScrollFrame}
 
 -->>-- Summary tab
-	skinMenuItms("AltoholicTabSummaryMenuItem", 5)
-	skinSortBtns("AltoholicTabSummary_Sort", 8)
+	skinMenuItms(_G.AltoholicTabSummary, 6)
+	skinSortBtns("AltoholicTabSummary_Sort", 9)
 	self:skinButton{obj=_G.AltoholicTabSummaryToggleView, mp2=true, plus=true}
 	self:skinDropDown{obj=_G.AltoholicTabSummary_SelectLocation}
-	self:skinScrollBar{obj=_G.AltoholicFrameSummaryScrollFrame}
+	_G.UIDropDownMenu_SetButtonWidth(_G.AltoholicTabSummary_SelectLocation, 24)
+
+	if self.modBtnBs then
+		self:addButtonBorder{obj=_G.AltoholicTabSummary_OptionsDataStore}
+		self:addButtonBorder{obj=_G.AltoholicTabSummary_Options}
+		self:addButtonBorder{obj=_G.AltoholicTabSummary_RequestSharing}
+	end
+
 	self:skinScrollBar{obj=_G.AltoholicFrameBagUsageScrollFrame}
 	self:skinScrollBar{obj=_G.AltoholicFrameSkillsScrollFrame}
 	self:skinScrollBar{obj=_G.AltoholicFrameActivityScrollFrame}
 	self:skinScrollBar{obj=_G.AltoholicFrameCurrenciesScrollFrame}
-
-	if self.modBtnBs then
-		self:addButtonBorder{obj=_G.AltoholicTabSummary_RequestSharing}
-		self:addButtonBorder{obj=_G.AltoholicTabSummary_Options}
-		self:addButtonBorder{obj=_G.AltoholicTabSummary_OptionsDataStore}
-	end
 
 end
 
@@ -115,25 +109,29 @@ function aObj:Altoholic_Characters() -- LoD
 
  	-- Icons on LHS
  	-- Characters
-	self:skinDropDown{obj=_G.AltoholicTabCharacters_SelectRealm}
+	self:skinDropDown{obj=_G.AltoholicTabCharacters.SelectRealm}
+	_G.UIDropDownMenu_SetButtonWidth(_G.AltoholicTabCharacters.SelectRealm, 24)
 	skinSortBtns("AltoholicTabCharacters_Sort", 4)
 
 	-- Icons at the Top in Character View
 	if self.modBtnBs then
-		self:addButtonBorder{obj=_G.AltoholicTabCharacters_CharactersIcon}
-		self:addButtonBorder{obj=_G.AltoholicTabCharacters_BagsIcon}
-		self:addButtonBorder{obj=_G.AltoholicTabCharacters_QuestsIcon}
-		self:addButtonBorder{obj=_G.AltoholicTabCharacters_TalentsIcon}
-		self:addButtonBorder{obj=_G.AltoholicTabCharacters_AuctionIcon}
-		self:addButtonBorder{obj=_G.AltoholicTabCharacters_MailIcon}
-		self:addButtonBorder{obj=_G.AltoholicTabCharacters_SpellbookIcon}
-		self:addButtonBorder{obj=_G.AltoholicTabCharacters_ProfessionsIcon}
+		self:addButtonBorder{obj=_G.AltoholicTabCharacters.MenuIcons.CharactersIcon}
+		self:addButtonBorder{obj=_G.AltoholicTabCharacters.MenuIcons.BagsIcon}
+		self:addButtonBorder{obj=_G.AltoholicTabCharacters.MenuIcons.QuestsIcon}
+		self:addButtonBorder{obj=_G.AltoholicTabCharacters.MenuIcons.TalentsIcon}
+		self:addButtonBorder{obj=_G.AltoholicTabCharacters.MenuIcons.AuctionIcon}
+		self:addButtonBorder{obj=_G.AltoholicTabCharacters.MenuIcons.MailIcon}
+		self:addButtonBorder{obj=_G.AltoholicTabCharacters.MenuIcons.SpellbookIcon}
+		self:addButtonBorder{obj=_G.AltoholicTabCharacters.MenuIcons.ProfessionsIcon}
 	end
 	-- Characters
 	-- Containers
 	self:skinScrollBar{obj=_G.AltoholicFrameContainersScrollFrame}
 	-- Quests
 	self:skinScrollBar{obj=_G.AltoholicFrameQuestsScrollFrame}
+	for i = 1, 14 do
+		self:skinButton{obj=_G["AltoholicFrameQuestsEntry" .. i .. "Collapse"], mp2=true}
+	end
 	-- Talents/Glyphs
 	self:skinDropDown{obj=_G.AltoholicFrameTalents_SelectMember}
 	-- AuctionsHouse
@@ -143,25 +141,22 @@ function aObj:Altoholic_Characters() -- LoD
 	-- SpellBook/Mounts/Companions/Glyphs
 	local btn, btnName
 	for i = 1, 12 do
-		btnName = "AltoholicFrameSpellbook_SpellIcon"..i
+		btnName = "AltoholicFrameSpellbook_SpellIcon" .. i
 		btn = _G[btnName]
 		btn:DisableDrawLayer("BACKGROUND")
 		btn:DisableDrawLayer("BORDER")
-		_G[btnName.."SlotFrame"]:SetAlpha(0)
+		_G[btnName .. "SlotFrame"]:SetAlpha(0)
 		btn.UnlearnedFrame:SetAlpha(0)
 		btn.TrainFrame:SetAlpha(0)
 		btn.RequiredLevelString:SetTextColor(self.BTr, self.BTg, self.BTb)
 		btn.SeeTrainerString:SetTextColor(self.BTr, self.BTg, self.BTb)
 	end
 	local function clrTxt()
-
 		for i = 1, 12 do
-			btnName = "AltoholicFrameSpellbook_SpellIcon"..i
-			btn = _G[btnName]
-			btn.SpellName:SetTextColor(self.HTr, self.HTg, self.HTb)
-			btn.SpellSubName:SetTextColor(self.BTr, self.BTg, self.BTb)
+			btn = "AltoholicFrameSpellbook_SpellIcon" .. i
+			btn.SpellName:SetTextColor(aObj.HTr, aObj.HTg, aObj.HTb)
+			btn.SpellSubName:SetTextColor(aObj.BTr, aObj.BTg, aObj.BTb)
 		end
-
 	end
 	self:makeMFRotatable(_G.AltoholicFramePetsNormal_ModelFrame)
 	-- hook this to colour Spell text
@@ -174,53 +169,60 @@ function aObj:Altoholic_Characters() -- LoD
 	end)
 	-- Professions
 	self:skinButton{obj=_G.AltoholicFrameRecipesInfo_ToggleAll, mp2=true}
+	for i = 1, 14 do
+		self:skinButton{obj=_G["AltoholicFrameRecipesEntry" .. i .. "Collapse"], mp2=true}
+	end
 	self:skinScrollBar{obj=_G.AltoholicFrameRecipesScrollFrame}
 
 end
 
 function aObj:Altoholic_Search() --LoD
 
+	skinMenuItms(_G.AltoholicTabSearch, 15)
 	self:skinScrollBar{obj=_G.AltoholicSearchMenuScrollFrame}
-	self:skinEditBox{obj=_G.AltoholicTabSearch_MinLevel, regs={9}}
-	self:skinEditBox{obj=_G.AltoholicTabSearch_MaxLevel, regs={9}}
-	self:skinDropDown{obj=_G.AltoholicTabSearch_SelectRarity}
-	self:skinDropDown{obj=_G.AltoholicTabSearch_SelectSlot}
-	self:skinDropDown{obj=_G.AltoholicTabSearch_SelectLocation}
+	self:skinEditBox{obj=_G.AltoholicTabSearch.MinLevel, regs={9}}
+	self:skinEditBox{obj=_G.AltoholicTabSearch.MaxLevel, regs={9}}
+	self:skinDropDown{obj=_G.AltoholicTabSearch.SelectRarity}
+	_G.UIDropDownMenu_SetButtonWidth(_G.AltoholicTabSearch.SelectRarity, 24)
+	self:skinDropDown{obj=_G.AltoholicTabSearch.SelectSlot}
+	_G.UIDropDownMenu_SetButtonWidth(_G.AltoholicTabSearch.SelectSlot, 24)
+	self:skinDropDown{obj=_G.AltoholicTabSearch.SelectLocation}
+	_G.UIDropDownMenu_SetButtonWidth(_G.AltoholicTabSearch.SelectLocation, 24)
 	self:skinScrollBar{obj=_G.AltoholicFrameSearchScrollFrame}
-	skinMenuItms("AltoholicTabSearchMenuItem", 15)
 	skinSortBtns("AltoholicTabSearch_Sort", 8)
 
 end
 
 function aObj:Altoholic_Guild() -- LoD
 
+	skinMenuItms(_G.AltoholicTabGuild, 2)
+	skinSortBtns("AltoholicTabGuild_Sort", 5)
+	for i = 1, 14 do
+		self:skinButton{obj=_G.AltoholicTabGuild.Members["Entry" .. i].Collapse, mp2=true}
+	end
 	-- Icons at the Top
 	if self.modBtnBs then
-		self:addButtonBorder{obj=_G.AltoholicFrameGuildBank_GuildIcon}
-		self:addButtonBorder{obj=_G.AltoholicFrameGuildBank_TabsIcon}
-		self:addButtonBorder{obj=_G.AltoholicFrameGuildBank_UpdateIcon}
-		self:addButtonBorder{obj=_G.AltoholicFrameGuildBank_RarityIcon}
+		self:addButtonBorder{obj=_G.AltoholicTabGuild.Bank.MenuIcons.GuildIcon}
+		self:addButtonBorder{obj=_G.AltoholicTabGuild.Bank.MenuIcons.TabsIcon}
+		self:addButtonBorder{obj=_G.AltoholicTabGuild.Bank.MenuIcons.UpdateIcon}
+		self:addButtonBorder{obj=_G.AltoholicTabGuild.Bank.MenuIcons.RarityIcon}
 	end
-	skinMenuItms("AltoholicTabGuildMenuItem", 2)
-	for i = 1, 14 do
-		self:skinButton{obj=_G["AltoholicFrameGuildMembersEntry"..i.."Collapse"], mp2=true}
-	end
-	skinSortBtns("AltoholicTabGuild_Sort", 5)
 
 end
 
 function aObj:Altoholic_Achievements() -- LoD
 
-	self:skinScrollBar{obj=_G.AltoholicAchievementsMenuScrollFrame}
+	self:skinDropDown{obj=_G.AltoholicTabAchievements.SelectRealm}
+	_G.UIDropDownMenu_SetButtonWidth(_G.AltoholicTabAchievements.SelectRealm, 24)
+	skinMenuItms(_G.AltoholicTabAchievements, 15)
+	self:skinScrollBar{obj=_G.AltoholicTabAchievements.ScrollFrame}
 	self:skinScrollBar{obj=_G.AltoholicFrameAchievementsScrollFrame}
-	self:skinDropDown{obj=_G.AltoholicTabAchievements_SelectRealm}
-	skinMenuItms("AltoholicTabAchievementsMenuItem", 15)
 
 end
 
 function aObj:Altoholic_Agenda() -- LoD
 
-	skinMenuItms("AltoholicTabAgendaMenuItem", 5)
+	skinMenuItms(_G.AltoholicTabAgenda, 5)
 
 end
 
@@ -229,7 +231,16 @@ function aObj:Altoholic_Grids() -- LoD
 	self:skinDropDown{obj=_G.AltoholicFrameGridsRightClickMenu}
 	self:skinScrollBar{obj=_G.AltoholicFrameGridsScrollFrame}
 	-- TabGrids
-	self:skinDropDown{obj=_G.AltoholicTabGrids_SelectView}
-	self:skinDropDown{obj=_G.AltoholicTabGrids_SelectRealm}
+	self:skinDropDown{obj=_G.AltoholicTabGrids.SelectRealm}
+	_G.UIDropDownMenu_SetButtonWidth(_G.AltoholicTabGrids.SelectRealm, 24)
+
+	-- hook this to resize button
+	self:RawHook("UIDropDownMenu_SetButtonWidth", function(frame, size)
+		if frame == _G.AltoholicTabGrids.SelectView then
+			size = 24
+		end
+		return self.hooks.UIDropDownMenu_SetButtonWidth(frame, size)
+	end, true)
+	self:skinDropDown{obj=_G.AltoholicTabGrids.SelectView}
 
 end
--- AddonSkins/MasterPlan.lua
+++ AddonSkins/MasterPlan.lua
@@ -4,26 +4,30 @@ local _G = _G
 
 function aObj:MasterPlan() -- LoD
 
-	local function skinTab()
-		local tab = _G.GarrisonMissionFrameTab3
-		self:keepRegions(tab, {7, 8, 9, 10})
-		self:addSkinFrame{obj=tab, noBdr=self.isTT, x1=9, y1=2, x2=-9, y2=0}
-		_G.PanelTemplates_SetNumTabs(_G.GarrisonMissionFrame, 3)
-		if _G.GarrisonMissionFrame.selectedTab == 3 then
-			self:setActiveTab(tab.sf)
+	local function skinTab(tab, id)
+		aObj:rmRegionsTex(tab, {1, 2, 3 ,4 ,5 ,6})
+		aObj:addSkinFrame{obj=tab, noBdr=self.isTT, x1=9, y1=2, x2=-9, y2=0}
+		if _G.GarrisonMissionFrame.selectedTab == id then
+			aObj:setActiveTab(tab.sf)
 		else
-			self:setInactiveTab(tab.sf)
+			aObj:setInactiveTab(tab.sf)
 		end
 	end
-	-- skin tab after short delay
-	self:ScheduleTimer(skinTab, 0.2)
+	-- skin extra tabs
+	skinTab(_G.GarrisonMissionFrameTab3, 3)
+	skinTab(_G.GarrisonMissionFrameTab4, 4)
+	_G.PanelTemplates_SetNumTabs(_G.GarrisonMissionFrame, 4)
 
 	-- Garrison Missions Frame - Active Missions Tab
-	local activeUI = self:getChild(_G.GarrisonMissionFrameMissions, 4)
-	local availUI = self:getChild(_G.GarrisonMissionFrameMissions, 5)
-	local sf = self:getChild(_G.GarrisonMissionFrameMissions, 6) -- scroll frame
+	local missionList = _G.MasterPlanMissionList
+	local mlc = missionList:GetNumChildren()
+	local activeUI = self:getChild(missionList, mlc - 3)
+	local availUI = self:getChild(missionList, mlc - 2)
+	local interestUI = self:getChild(missionList, mlc - 1)
+	local sf = self:getChild(missionList, mlc) -- scroll frame
 	local sc = sf:GetScrollChild()
 	local bar = self:getChild(sf, 1)
+	self:skinButton{obj=activeUI.CompleteAll}
 	self:skinSlider{obj=bar, adj=-4}
 
 	local function skinMissionButtons()
@@ -32,17 +36,17 @@ function aObj:MasterPlan() -- LoD
 			if child:IsObjectType("Button") then
 				child:DisableDrawLayer("BACKGROUND")
 				child:DisableDrawLayer("BORDER")
-				-- self:getChild(child, 1):DisableDrawLayer("OVERLAY") -- shadow background, don't remove this
+				-- aObj:getChild(child, 1):DisableDrawLayer("OVERLAY") -- shadow background, don't remove this as it identifies missions that are unable to be slected
 				-- re-align the top & bottom highlight
-				local t = self:getRegion(child, 12)
+				local t = aObj:getRegion(child, 12)
 				t:ClearAllPoints()
 				t:SetPoint("topleft", 0, 4)
 				t:SetPoint("topright", 0, 4)
-				t = self:getRegion(child, 13)
+				t = aObj:getRegion(child, 13)
 				t:ClearAllPoints()
 				t:SetPoint("bottomleft", 0, -4)
 				t:SetPoint("bottomright", 0, -4)
-				self:removeRegions(child, {14, 15, 16, 17, 23}) -- highlight corners & rare indicator
+				aObj:removeRegions(child, {14, 15, 16, 17, 23}) -- highlight corners & rare indicator
 				if child.followers then -- Active mission button has these
 					for i = 1, #child.followers do
 						child.followers[i].ring:SetTexture(nil)
@@ -54,7 +58,16 @@ function aObj:MasterPlan() -- LoD
 					end
 				end
 				for i = 1, #child.rewards do
-					self:addButtonBorder{obj=child.rewards[i], reParent={child.rewards[i].quantity}}
+					aObj:addButtonBorder{obj=child.rewards[i], reParent={child.rewards[i].quantity}}
+				end
+				 -- Missions of interest have these unused follower buttons
+				local grandchild = aObj:getChild(child, child:GetNumChildren())
+				if grandchild:IsObjectType("Button")
+				and grandchild:GetNumChildren() == 21
+				then
+					for i = 1, grandchild:GetNumChildren() do
+						grandchild[i].ring:SetTexture(nil)
+					end
 				end
 			end
 		end
@@ -73,11 +86,12 @@ function aObj:MasterPlan() -- LoD
 	local function skinLootContainer()
 		for i = 1, #lootContainer.items do
 			lootContainer.items[i].Border:SetTexture(nil)
-			self:addButtonBorder{obj=lootContainer.items[i], relTo=lootContainer.items[i].Icon, ofs=3}
+			aObj:addButtonBorder{obj=lootContainer.items[i], relTo=lootContainer.items[i].Icon, ofs=3}
 		end
 		for i = 1, #lootContainer.followers do
 			lootContainer.followers[i].PortraitRing:SetTexture(nil)
-			lootContainer.followers[i].LevelBorder:SetAlpha(0) -- texture changed
+			lootContainer.followers[i].LevelBorder:SetTexture(nil)
+			-- lootContainer.followers[i].LevelBorder:SetAlpha(0) -- texture changed
 			lootContainer.followers[i].PortraitRingCover:SetTexture(nil)
 		end
 	end
@@ -89,8 +103,7 @@ function aObj:MasterPlan() -- LoD
 	end)
 
 	-- Garrison Missions Frame - Available Missions Tab
-	local sortIndicator = self:getChild(availUI, 1)
-	self:removeRegions(sortIndicator, {1}) -- background
+	self:removeRegions(self:getChild(availUI, 1), {1}) -- sort indicator background
 	local roamingParty = self:getChild(availUI, 2)
 	for i = 1, 3 do
 		local btn = self:getChild(roamingParty, i)
@@ -99,8 +112,7 @@ function aObj:MasterPlan() -- LoD
 
 	-- Garrison Missions Frame - Available Missions Tab - Mission Page
 	-- Get Suggested Groups button
-	local lfgButton = self:getChild(_G.GarrisonMissionFrame.MissionTab.MissionPage.Stage, 2)
-	self:removeRegions(lfgButton, {2}) -- ring texture
+	self:removeRegions(self:getChild(_G.GarrisonMissionFrame.MissionTab.MissionPage.Stage, 2), {2}) -- ring texture
 	-- minimize button
 	self:skinButton{obj=_G.GarrisonMissionFrame.MissionTab.MissionPage.MinimizeButton, ob="-"}
 
