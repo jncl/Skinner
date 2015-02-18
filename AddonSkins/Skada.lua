dffd2701b4bece1aaec9a53512e0e9a209629c91
Jncl <jncl@mac.com>
2015-02-18 12:31:32 +0000

    Updated Skada skin

--- AddonSkins/Skada.lua
+++ AddonSkins/Skada.lua
@@ -1,11 +1,12 @@
 local aName, aObj = ...
 if not aObj:isAddonEnabled("Skada") then return end
+local _G = _G
 
 function aObj:Skada()
 
 	local function changeSettings(db)
 
-		db.barcolor = CopyTable(aObj.db.profile.StatusBar)
+		db.barcolor = _G.CopyTable(aObj.db.profile.StatusBar)
 		db.bartexture = db.bartexture == "Empty" and db.bartexture or db.barcolor.texture -- change if not "Empty" texture
 		db.barcolor.texture = nil -- remove texture element
 		-- background settings
@@ -22,13 +23,13 @@ function aObj:Skada()
 		if not aObj.skinFrame[win.bargroup]
 		then
 			-- print(win.bargroup.bgframe, win.bargroup.button)
-			aObj:addSkinFrame{obj=win.bargroup}
+			aObj:addSkinFrame{obj=win.bargroup, x1=-4, y1=18, x2=3, y2=-3}
 			win.bargroup:SetBackdrop(nil)
 			win.bargroup.SetBackdrop = function() end
 		end
 
 	end
-	local barDisplay = Skada:GetModule("BarDisplay", true)
+	local barDisplay = _G.Skada:GetModule("BarDisplay", true)
 	if barDisplay then
 		-- hook this to skin new frames
 		self:SecureHook(barDisplay, "ApplySettings", function(this, win)
@@ -37,14 +38,30 @@ function aObj:Skada()
 	end
 
 	-- change the default settings
-	changeSettings(Skada.windowdefaults)
+	changeSettings(_G.Skada.windowdefaults)
 
 	-- change existing ones
-	for _, win in pairs(Skada:GetWindows()) do
+	for _, win in _G.pairs(_G.Skada:GetWindows()) do
 		changeSettings(win.db)
 		skinFrame(win)
 		-- apply these changes
 		win.display:ApplySettings(win)
 	end
+
+	-- Temp upgrade popup
+	if _G.SkadaWarn then
+		self:addSkinFrame{obj=_G.SkadaWarn}
+	end
+
+	-- skin popup frame
+	self.RegisterCallback("Skada", "UIParent_GetChildren", function(this, child)
+		if child:IsObjectType("Frame")
+		and child:GetName() == nil
+		and self:getInt(child:GetWidth()) == 250
+		and self:getInt(child:GetHeight()) == 70
+		then
+			self:addSkinFrame{obj=child}
+		end
+	end)
 
 end
