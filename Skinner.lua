f62c2035f108be2def9e30a3d0c2c004adc8c76b
Jncl <jncl@mac.com>
2016-12-03 16:15:24 +0000

    Updated version numbers

--- Skinner.lua
+++ Skinner.lua
@@ -28,8 +28,8 @@ do
 	-- player level
 	aObj.uLvl = _G.UnitLevel("player")
 
-	local liveInfo = {"7.1.0", 22996}
-	local ptrInfo = {"7.1.5", 23038}
+	local liveInfo = {"7.1.0", 23171}
+	local ptrInfo = {"7.1.5", 23138}
 	local betaInfo = {"7.0.0", 99999}
 	local buildInfo, portal = {_G.GetBuildInfo()}, _G.GetCVar("portal") or nil
 --@alpha@
