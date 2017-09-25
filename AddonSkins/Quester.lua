3713d86b2948ca5bcd1953a443f090fa62ab261d
Jncl <jncl@mac.com>
2017-09-25 18:44:29 +0100

    Skinned Quester

---
+++ AddonSkins/Quester.lua
@@ -0,0 +1,33 @@
+local aName, aObj = ...
+if not aObj:isAddonEnabled("Quester") then return end
+local _G = _G
+
+aObj.addonsToSkin.Quester = function(self) -- v 7.3.0.0
+
+	local Quester = _G.LibStub("AceAddon-3.0"):GetAddon("Quester", true)
+
+	-- hook these to handle colour changes
+	local function chgTxtClr(text)
+		if text:match("^|c(.*)%[") then
+			text = _G.gsub(text, "|r", "") .. "|r"
+		elseif text:match("^%[") then
+			text = _G.format(_G.NORMAL_QUEST_DISPLAY, text)
+		end
+		return text
+	end
+	local btn
+	for i = 1, _G.NUMGOSSIPBUTTONS do
+		btn = _G["GossipTitleButton" .. i]
+		self:RawHook(btn, "SetText", function(this, text)
+			self.hooks[this].SetText(this, chgTxtClr(text))
+		end, true)
+	end
+	for i = 1, _G.MAX_NUM_QUESTS do
+		btn = _G["QuestTitleButton" .. i]
+		self:RawHook(btn, "SetText", function(this, text)
+			self.hooks[this].SetText(this, chgTxtClr(text))
+		end, true)
+	end
+	btn = nil
+
+end
