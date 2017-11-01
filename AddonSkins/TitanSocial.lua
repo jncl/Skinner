1f4e2f0bfaf87445ce29f1911cea3174a7819a24
Jncl <jncl@mac.com>
2017-11-01 22:34:00 +0000

    Skinned TitanSocial

---
+++ AddonSkins/TitanSocial.lua
@@ -0,0 +1,21 @@
+local aName, aObj = ...
+if not aObj:isAddonEnabled("TitanSocial") then return end
+local _G = _G
+
+aObj.addonsToSkin.TitanSocial = function(self) -- v 7.3.0
+
+	-- find the tooltip frame and skin it
+	self.RegisterCallback("TitanSocial", "UIParent_GetChildren", function(this, child)
+		if child.lines
+		and child.columns
+		and child.scrollframe
+		and child.scrollchild
+		then
+			self.ttHook[child] = true
+			self:add2Table(self.ttList, child)
+			self.UnregisterCallback("TitanSocial", "UIParent_GetChildren")
+		end
+	end)
+	self:scanUIParentsChildren()
+
+end
