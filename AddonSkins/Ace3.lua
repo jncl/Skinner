d7fdab1c69201d0689ad7aa71ae475aa2dd901ea
Jncl <jncl@mac.com>
2018-03-07 22:13:59 +0000

    Bugfix for Ace3 skin

--- AddonSkins/Ace3.lua
+++ AddonSkins/Ace3.lua
@@ -356,7 +356,7 @@ aObj.libsToSkin["AceGUI-3.0"] = function(self) -- v AceGUI-3.0, 34
 
 			-- OrderHallCommander objects
 			elseif objType == "OHCGUIContainer" then
-				aObj:skinCloseButton{obj=obj.frame.close}
+				aObj:skinCloseButton{obj=obj.frame.Close}
 				aObj:addSkinFrame{obj=obj.frame, ft="a", kfs=true, nb=true}
 
 			-- ignore these types for now
