7f0ba2863ac5aed68f34c57dea8f647d3c168830
Jncl <jncl@mac.com>
2018-07-19 14:42:29 +0100

    Updated Bartender4 & HandyNotes skins

--- AddonSkins/Bartender4.lua
+++ AddonSkins/Bartender4.lua
@@ -2,41 +2,34 @@ local aName, aObj = ...
 if not aObj:isAddonEnabled("Bartender4") then return end
 local _G = _G
 
-function aObj:Bartender4() -- v 4.7.9
+aObj.addonsToSkin.Bartender4 = function(self) -- v 4.8.1
+
+	self:SecureHook(_G.Bartender4, "ShowUnlockDialog", function(this)
+		self:skinCheckButton{obj=_G.Bartender4Snapping}
+		self:skinStdButton{obj=_G.Bartender4DialogLock}
+		self:addSkinFrame{obj=this.unlock_dialog, ft="a", kfs=true, nb=true, y1=6}
+		self:Unhook(_G.Bartender4, "ShowUnlockDialog")
+	end)
 
 	local BT4ABs = _G.Bartender4:GetModule("ActionBars", true)
 	if not BT4ABs then return end
 
-	self:SecureHook(_G.Bartender4, "ShowUnlockDialog", function(this)
-		self:skinCheckButton{obj=_G.Bartender4Snapping}
-		self:skinButton{obj=_G.Bartender4DialogLock}
-		self:addSkinFrame{obj=this.unlock_dialog, kfs=true, nb=true, y1=6}
-		self:Unhook(_G.Bartender4, "ShowUnlockDialog")
-	end)
-
 	-- skin ActionBar buttons
-	for _, ab in _G.pairs(BT4ABs.actionbars) do
-		for _, btn in _G.pairs(ab.buttons) do
+	for _, bar in BT4ABs:GetAll() do
+		for _, btn in _G.ipairs(bar.buttons) do
 			self:addButtonBorder{obj=btn, abt=true, sec=true}
 		end
 	end
 
-	-- skin XPBar, ReputationWatchBar, ArtifactWatchBar, HonorWatchBar if required
-	if not self.db.profile.MainMenuBar.skin then
-		-- XPBar
-		_G.MainMenuExpBar:DisableDrawLayer("OVERLAY", -1)
-		_G.ExhaustionTick:SetAlpha(0)
-		_G.ReputationWatchBar.StatusBar:DisableDrawLayer("ARTWORK", 0)
-		_G.ArtifactWatchBar.StatusBar:DisableDrawLayer("ARTWORK", 0)
-		_G.ArtifactWatchBar.Tick:SetAlpha(0)
-		_G.HonorWatchBar.StatusBar:DisableDrawLayer("ARTWORK", 0)
-		_G.HonorWatchBar.ExhaustionTick:SetAlpha(0)
+	local BT4STB = _G.Bartender4:GetModule("StatusTrackingBar", true)
+	if not BT4STB then return end
+
+	-- skin Status bars
+	BT4STB.bar.manager.SingleBarLarge:SetTexture(nil)
+	for k, bar in ipairs(BT4STB.bar.manager.bars) do
+		self:skinStatusBar{obj=bar.StatusBar, fi=0, bgTex=bar.StatusBar.Background}
 	end
-	if self.db.profile.MainMenuBar.glazesb then
-		self:glazeStatusBar(_G.MainMenuExpBar, 0, self:getRegion(_G.MainMenuExpBar, 9), {_G.ExhaustionLevelFillBar})
-		self:glazeStatusBar(_G.ReputationWatchBar.StatusBar, 0, _G.ReputationWatchBar.StatusBar.Background)
-		self:glazeStatusBar(_G.ArtifactWatchBar.StatusBar, 0, _G.ArtifactWatchBar.StatusBar.Background, {_G.ArtifactWatchBar.StatusBar.Underlay})
-		self:glazeStatusBar(_G.HonorWatchBar.StatusBar, 0, _G.HonorWatchBar.StatusBar.Background, {_G.HonorWatchBar.StatusBar.Underlay, _G.HonorWatchBar.ExhaustionLevelFillBar})
-	end
+
+	BT4ABs, BT4STB = nil, nil
 
 end
--- AddonSkins/HandyNotes.lua
+++ AddonSkins/HandyNotes.lua
@@ -2,17 +2,20 @@ local aName, aObj = ...
 if not aObj:isAddonEnabled("HandyNotes") then return end
 local _G = _G
 
-aObj.addonsToSkin.HandyNotes = function(self) -- v
+aObj.addonsToSkin.HandyNotes = function(self) -- v 1.5.2
 
 	-- thanks to Xinhuan for the pointer in the code :-)
-	local HNEditFrame = LibStub("AceAddon-3.0"):GetAddon("HandyNotes"):GetModule("HandyNotes").HNEditFrame
+	local HNEditFrame = _G.LibStub("AceAddon-3.0"):GetAddon("HandyNotes"):GetModule("HandyNotes").HNEditFrame
 
 	HNEditFrame.titleinputframe:SetBackdrop(nil)
+	self:moveObject{obj=HNEditFrame.title, y=-6}
 	self:skinEditBox(HNEditFrame.titleinputbox, {})
 	self:addSkinFrame{obj=HNEditFrame.descframe, ft="a", nb=true}
 	self:skinSlider{obj=HNEditFrame.descscrollframe.ScrollBar, size=3}
 	self:skinDropDown{obj=HNEditFrame.icondropdown}
-	self:skinDropDown{obj=HNEditFrame.leveldropdown}
-	self:addSkinFrame{obj=HNEditFrame, kfs=true, ft="a", nb=true}
+	self:skinCheckButton{obj=HNEditFrame.continentcheckbox}
+	self:skinStdButton{obj=HNEditFrame.okbutton}
+	self:skinStdButton{obj=HNEditFrame.cancelbutton}
+	self:addSkinFrame{obj=HNEditFrame, kfs=true, ft="a", ofs=-5, y1=-4, y2=30}
 
 end
