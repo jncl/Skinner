6021e7ff7ddbc61fce6791d53e187708989f9add
Jncl <jncl@mac.com>
2018-07-24 16:29:01 +0100

    Skinned LibAboutPanel

---
+++ AddonSkins/LibAboutPanel.lua
@@ -0,0 +1,30 @@
+local aName, aObj = ...
+local _G = _G
+-- This is a Library
+
+aObj.libsToSkin["LibAboutPanel"] = function(self) -- v 1.52
+	if self.initialized.LibAboutPanel then return end
+	self.initialized.LibAboutPanel = true
+
+	local lAP = _G.LibStub("LibAboutPanel", true)
+	if lAP then
+		self:skinEditBox{obj=lAP.editbox, regs={6}} -- 6 is text
+		-- hook this to move editbox to the left
+		self:RawHook(lAP.editbox, "SetPoint", function(this, point, relTo)
+			-- aObj:Debug("lPA editbox SetPoint: [%s, %s, %s]", this, point, relTo)
+			if point == "LEFT" then
+				self.hooks[this].SetPoint(this, point, relTo, point, -4, 0)
+			else
+				self.hooks[this].SetPoint(this, point, relTo)
+			end
+		end, true)
+	end
+
+	-- this is to stop the buttons being skinned
+	self.RegisterCallback("LibAboutPanel", "IOFPanel_Before_Skinning", function(this, panel)
+		if panel.name ~= "About" then return end
+		self.iofSkinnedPanels[panel] = true
+		self.UnregisterCallback("LibAboutPanel", "IOFPanel_Before_Skinning")
+	end)
+
+end
