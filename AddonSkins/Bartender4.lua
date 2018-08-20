28598f520bc3cb074318db08cb0dfd1b8df7fd0c
Jncl <jncl@mac.com>
2018-08-20 22:10:54 +0100

    Updated Bartender4 skin

--- AddonSkins/Bartender4.lua
+++ AddonSkins/Bartender4.lua
@@ -12,34 +12,63 @@ aObj.addonsToSkin.Bartender4 = function(self) -- v 4.8.1
 	end)
 
 	local mod = _G.Bartender4:GetModule("ActionBars", true)
-	if mod
-	and mod.enabledState
-	then
-		-- skin ActionBar buttons
-		for _, bar in mod:GetAll() do
-			for _, btn in _G.ipairs(bar.buttons) do
-				self:addButtonBorder{obj=btn, abt=true, sec=true}
+	if mod then
+		local function skinActionButtons(mod)
+			-- skin ActionBar buttons
+			for _, bar in mod:GetAll() do
+				for _, btn in _G.ipairs(bar.buttons) do
+					self:addButtonBorder{obj=btn, abt=true, sec=true}
+				end
 			end
+		end
+		if mod.enabledState then
+			skinActionButtons(mod)
+		else
+			self:SecureHook(mod, "OnEnable", function(this)
+				skinActionButtons(this)
+				self:Unhook(this, "OnEnable")
+			end)
 		end
 	end
 
 	local mod = _G.Bartender4:GetModule("StatusTrackingBar", true)
-	if mod
-	and mod.enabledState
-	then
-		-- skin Status bars
-		mod.bar.manager.SingleBarLarge:SetTexture(nil)
-		for _, bar in _G.ipairs(mod.bar.manager.bars) do
-			self:skinStatusBar{obj=bar.StatusBar, fi=0, bgTex=bar.StatusBar.Background}
+	if mod then
+		local function skinStatusBars(mod)
+			-- skin Status bars
+			mod.bar.manager:DisableDrawLayer("OVERLAY") -- status bar textures
+			for _, bar in _G.ipairs(mod.bar.manager.bars) do
+				self:skinStatusBar{obj=bar.StatusBar, fi=0, bgTex=bar.StatusBar.Background}
+				if bar.ExhaustionTick then -- HonorStatusBar & ExpStatusBar
+					bar.ExhaustionTick:GetNormalTexture():SetTexture(nil)
+					bar.ExhaustionTick:GetHighlightTexture():SetTexture(nil)
+				elseif bar.Tick then -- ArtifactStatusBar
+					bar.Tick:GetNormalTexture():SetTexture(nil)
+					bar.Tick:GetHighlightTexture():SetTexture(nil)
+				end
+			end
+		end
+		if mod.enabledState then
+			skinStatusBars(mod)
+		else
+			self:SecureHook(mod, "OnEnable", function(this)
+				skinStatusBars(this)
+				self:Unhook(this, "OnEnable")
+			end)
 		end
 	end
 
 	local mod = _G.Bartender4:GetModule("BlizzardArt", true)
-	if mod
-	and mod.enabledState
-	then
-		-- disable Art
-		mod:ToggleModule(nil, false)
+	if mod then
+		if mod.enabledState then
+			-- disable Art
+			mod:ToggleModule(nil, false)
+		else
+			self:SecureHook(mod, "OnEnable", function(this)
+				-- disable Art
+				mod:ToggleModule(nil, false)
+				self:Unhook(this, "OnEnable")
+			end)
+		end
 	end
 
 	mod = nil
