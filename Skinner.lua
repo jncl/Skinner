bfad1ca3d3cf477cffb26793a0e13f74144b6612
Jncl <jncl@mac.com>
2018-08-20 22:12:43 +0100

    Updated version numbers

--- Skinner.lua
+++ Skinner.lua
@@ -36,7 +36,7 @@ do
 
 	local betaInfo = {"8.0.1", 27326}
 	local ptrInfo = {"8.0.1", 27326}
-	local liveInfo = {"8.0.1", 27356}
+	local liveInfo = {"8.0.1", 27377}
 	local buildInfo, portal = {_G.GetBuildInfo()}, _G.GetCVar("portal") or nil
 --@alpha@
 	aObj:Debug(liveInfo[1], liveInfo[2], buildInfo[1], buildInfo[2], buildInfo[3], buildInfo[4], portal)
