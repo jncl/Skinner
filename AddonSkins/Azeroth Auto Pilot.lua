8df7b394616a8005b89e3191944dcf7159d62181
Jncl <jncl@mac.com>
2018-08-20 22:10:43 +0100

    Skinned Azeroth Auto Pilot & War Campaigns Complete

---
+++ AddonSkins/Azeroth Auto Pilot.lua
@@ -0,0 +1,97 @@
+local aName, aObj = ...
+if not aObj:isAddonEnabled("Azeroth Auto Pilot") then return end
+local _G = _G
+
+aObj.addonsToSkin["Azeroth Auto Pilot"] = function(self) -- v 0.156
+
+	-- Core frames
+	self:addSkinFrame{obj=_G.AAP_AfkFrame, ft="a", kfs=true, nb=true}
+	if self.modBtns then
+		self:skinStdButton{obj=_G.AAP_ArrowFrame.Button}
+	end
+
+	-- QuestList Frames
+	for i = 1, 5 do
+		self:addSkinFrame{obj=_G.AAP.PartyList.PartyFrames[i], ft="a", kfs=true, nb=true}
+		self:addSkinFrame{obj=_G.AAP.PartyList.PartyFrames2[i], ft="a", kfs=true, nb=true}
+	end
+
+	self:addSkinFrame{obj=_G.AAP.QuestList.SugQuestFrame, ft="a", kfs=true, nb=true}
+	self:addSkinFrame{obj=_G.AAP.QuestList.SugQuestFrame2, ft="a", kfs=true, nb=true}
+	self:addSkinFrame{obj=_G.AAP.QuestList.SugQuestFrame2.TextureAFrame, ft="a", kfs=true, nb=true}
+	if self.modBtns then
+		self:skinStdButton{obj=_G.AAP.QuestList.SugQuestFrame["Button1"], seca=true}
+		self:skinStdButton{obj=_G.AAP.QuestList.SugQuestFrame["Button2"], seca=true}
+		self:skinStdButton{obj=_G.AAP.QuestList.SugQuestFrame2["Button1"], seca=true}
+		self:skinStdButton{obj=_G.AAP.QuestList.SugQuestFrame2["Button2"], seca=true}
+	end
+
+	self:skinEditBox{obj=AAP.QuestList.Greetings2EB1, regs={6}, x=-5, y=-8} -- 6 is text
+	self:skinStdButton{obj=AAP.QuestList.GreetingsHideB, seca=true}
+	self:addSkinFrame{obj=_G.AAP.QuestList.Greetings, ft="a", kfs=true, nb=true, y2=-2}
+
+	for i = 1, 50 do
+		self:addSkinFrame{obj=_G.AAP.QuestList.QuestFrames[i], ft="a", kfs=true, nb=true, x1=-2, x2=2}
+		if self.modBtns then
+			self:skinStdButton{obj=_G.AAP.QuestList.QuestFrames["FS" .. i]["Button"]}
+		end
+		if self.modBtnBs then
+			self:addButtonBorder{obj=_G.AAP.QuestList2["BF" .. i]["AAP_Button"], seca=true}
+		end
+	end
+
+	self:addSkinFrame{obj=_G.AAP.QuestList.Warcamp, ft="a", kfs=true, nb=true}
+	self:addSkinFrame{obj=_G.AAP.QuestList.Warcamp2, ft="a", kfs=true, nb=true}
+	if self.modBtns then
+		self:skinStdButton{obj=_G.AAP.QuestList.WarcampB1, seca=true}
+		self:skinStdButton{obj=_G.AAP.QuestList.WarcampB2, seca=true}
+	end
+
+	-- Options Frame
+	self:SecureHookScript(_G.AAP.OptionsFrame.MainFrame, "OnShow", function(this)
+		if self.modChkBtns then
+			self:skinCheckButton{obj=_G.AAP.OptionsFrame.CheckButton1}
+			self:skinCheckButton{obj=_G.AAP.OptionsFrame.CheckButton2}
+			self:skinCheckButton{obj=_G.AAP.OptionsFrame.CheckButton3}
+			self:skinCheckButton{obj=_G.AAP.OptionsFrame.CheckButton10}
+			self:skinCheckButton{obj=_G.AAP.OptionsFrame.CheckButton11}
+			self:skinCheckButton{obj=_G.AAP.OptionsFrame.CheckButton12}
+			self:skinCheckButton{obj=_G.AAP.OptionsFrame.CheckButton5}
+			self:skinCheckButton{obj=_G.AAP.OptionsFrame.CheckButton7}
+			self:skinCheckButton{obj=_G.AAP.OptionsFrame.CheckButton8}
+			self:skinCheckButton{obj=_G.AAP.OptionsFrame.CheckButton9}
+			self:skinCheckButton{obj=_G.AAP.OptionsFrame.CheckButton13}
+			self:skinCheckButton{obj=_G.AAP.OptionsFrame.CheckButton14}
+		end
+		self:skinSlider{obj=_G.AAP.OptionsFrame.Slider1}--, rt="artwork", wdth=-4, size=3, hgt=-10}
+		self:skinSlider{obj=_G.AAP.OptionsFrame.Slider2}--, rt="artwork", wdth=-4, size=3, hgt=-10}
+		self:skinSlider{obj=_G.AAP.OptionsFrame.Slider3}--, rt="artwork", wdth=-4, size=3, hgt=-10}
+		if self.modBtns then
+			self:skinStdButton{obj=_G.AAP.OptionsFrame["Button1"], seca=true}
+			self:skinStdButton{obj=_G.AAP.OptionsFrame["Button2"], seca=true}
+			self:skinStdButton{obj=_G.AAP.OptionsFrame["Button3"], seca=true}
+			self:skinStdButton{obj=_G.AAP.OptionsFrame["Button4"], seca=true}
+		end
+		self:skinDropDown{obj=_G.AAP_DropDownList}--, noSkin=true, x1=0, y1=0, x2=0, y2=0}
+		self:skinDropDown{obj=_G.AAP_dropDown1}--, noSkin=true, x1=0, y1=0, x2=0, y2=0}
+		self:addSkinFrame{obj=_G.AAP.OptionsFrame.MainFrame, ft="a", kfs=true, nb=true, ofs=4}
+
+		self:Unhook(this, "OnShow")
+	end)
+
+	-- Banners Frames
+	self:addSkinFrame{obj=_G.AAP.Banners.BannersFrame.Frame, ft="a", kfs=true, nb=true}
+	if self.modBtns then
+		self:skinStdButton{obj=_G.AAP.Banners.BannersFrame.B1, seca=true}
+		self:skinStdButton{obj=_G.AAP.Banners.BannersFrame.B2, seca=true}
+		self:skinStdButton{obj=_G.AAP.Banners.BannersFrame.B3, seca=true}
+	end
+	for i = 1, 4 do
+		self:addSkinFrame{obj=_G.AAP.Banners.BannersFrame["Frame" .. i], ft="a", kfs=true, nb=true}
+	end
+
+	-- BrutalStatic Frames
+	self:addSkinFrame{obj=_G.AAP.BrutallCC.BrutallFrame.Frame, ft="a", kfs=true, nb=true}
+	self:addSkinFrame{obj=_G.AAP.BrutallCC.BrutallFrame.FrameName, ft="a", kfs=true, nb=true}
+
+end
---
+++ AddonSkins/WarCampaignsComplete.lua
@@ -0,0 +1,114 @@
+local aName, aObj = ...
+if not aObj:isAddonEnabled("WarCampaignsComplete") then return end
+local _G = _G
+
+aObj.addonsToSkin.WarCampaignsComplete = function(self) -- v 1.0
+
+	local WCCUIMF = "WarCampaignsCompleteUIMainFrame"
+
+	self:addSkinFrame{obj=_G[WCCUIMF], ft="a", kfs=true, ri=true, ofs=2, x2=1}
+
+	self:skinTabs{obj=_G[WCCUIMF .. "SubFrameHeader"]}
+
+	-- Monitor Tab (1)
+	self:addButtonBorder{obj=_G[WCCUIMF .. "Tab1SubFrameRefreshButton"], ofs=-2, x1=1}
+	for _, type in pairs{"Name", "Lvl", "Buttons"} do
+		self:keepRegions(_G[WCCUIMF .. "Tab1SubFrame" .. type .. "ColumnHeaderButton"], {4, 5, 6})
+		self:addSkinFrame{obj=_G[WCCUIMF .. "Tab1SubFrame" .. type .. "ColumnHeaderButton"], ft="a", nb=true, aso={bd=5}}
+	end
+	self:skinSlider{obj=_G[WCCUIMF .. "Tab1SubFrameScrollFrame"].ScrollBar, rt="artwork"}
+	if self.modBtnBs then
+		self:SecureHookScript(_G[WCCUIMF .. "Tab1SubFrameScrollFrame"], "OnUpdate", function(this)
+			for i = 1, _G[WCCUIMF .. "Tab1SubFrameScrollFrame"].numToDisplay do
+				_G[WCCUIMF .. "Tab1SubFrame_ScrollFrameButton" .. i .. "BG"]:SetTexture(nil)
+				for j = 1, 5 do
+					local btn = WCCUIMF .. "Tab1SubFrame_ScrollFrameButton" .. i .. "Monitor" .. j
+					self:addButtonBorder{obj=_G[btn], reParent={_G[btn .. "TopRightText"], _G[btn .. "CenterText"], _G[btn .. "Indicator"]}}
+				end
+			end
+		end)
+	end
+	_G[WCCUIMF .. "Tab1SubFrameFooterBG"]:SetTexture(nil)
+	-- skin footer buttons
+	if self.modBtnBs then
+		self:addButtonBorder{obj=_G[WCCUIMF .. "Tab1SubFrameFooterMissionsReportButton"]}
+		self:addButtonBorder{obj=_G[WCCUIMF .. "Tab1SubFrameFooterAdvancementsReportButton"]}
+		self:addButtonBorder{obj=_G[WCCUIMF .. "Tab1SubFrameFooterWorkOrdersReportButton"]}
+	end
+
+	-- Characters Tab (2)
+	self:skinDropDown{obj=_G[WCCUIMF .. "Tab2SubFrameCharacterDropDownMenu"]}
+	self:skinSlider{obj=_G[WCCUIMF .. "Tab2SubFrameScrollFrame"].ScrollBar, rt="artwork"}
+	if self.modBtnBs then
+		for i = 1, _G[WCCUIMF .. "Tab2SubFrameScrollFrame"].numToDisplay do
+			local btn = WCCUIMF .. "Tab2SubFrame_ScrollFrameButton" .. i
+			self:addButtonBorder{obj=_G[btn], relTo=_G[btn .. "_IconTexture"]}
+			_G.RaiseFrameLevel(_G[btn].sbb)
+			if self.modChkBtns then
+				self:skinCheckButton{obj=_G[btn .. "_Check"]}
+			end
+		end
+	end
+	if self.modChkBtns then
+		self:skinCheckButton{obj=_G[WCCUIMF .. "Tab2SubFrameOrderAutomaticallyCheckButton"]}
+		self:skinCheckButton{obj=_G[WCCUIMF .. "Tab2SubFrameCurrentCharacterFirstCheckButton"]}
+	end
+	if self.modBtns then
+		self:skinStdButton{obj=_G[WCCUIMF .. "Tab2SubFrameOrderButton"]}
+		self:skinStdButton{obj=_G[WCCUIMF .. "Tab2SubFrameDeleteCharacterButton"]}
+		self:skinStdButton{obj=_G[WCCUIMF .. "Tab2SubFrameUncheckAllButton"]}
+		self:skinStdButton{obj=_G[WCCUIMF .. "Tab2SubFrameCheckAllButton"]}
+	end
+
+	-- Misc Tab (3)
+	if self.modChkBtns then
+		self:skinCheckButton{obj=_G[WCCUIMF .. "Tab3SubFrameShowMinimapButtonCheckButton"]}
+		self:skinCheckButton{obj=_G[WCCUIMF .. "Tab3SubFrameShowCharacterTooltipMinimapButtonCheckButton"]}
+		self:skinCheckButton{obj=_G[WCCUIMF .. "Tab3SubFrameDockMinimapButtonCheckButton"]}
+		self:skinCheckButton{obj=_G[WCCUIMF .. "Tab3SubFrameLockMinimapButtonCheckButton"]}
+		self:skinCheckButton{obj=_G[WCCUIMF .. "Tab3SubFrameLargeMinimapButtonCheckButton"]}
+		self:skinCheckButton{obj=_G[WCCUIMF .. "Tab3SubFrameShowOriginalMissionsReportMinimapButtonCheckButton"]}
+		self:skinCheckButton{obj=_G[WCCUIMF .. "Tab3SubFrameShowTroopDetailsInTooltipCheckButton"]}
+		self:skinCheckButton{obj=_G[WCCUIMF .. "Tab3SubFrameShowCharacterRealmsCheckButton"]}
+		self:skinCheckButton{obj=_G[WCCUIMF .. "Tab3SubFrameForgetDragPositionCheckButton"]}
+	end
+	self:skinDropDown{obj=_G[WCCUIMF .. "Tab3SubFrameMonitorRowsDropDownMenu"]}
+	self:skinDropDown{obj=_G[WCCUIMF .. "Tab3SubFrameMonitorColumnsDropDownMenu"]}
+	if self.modBtns then
+		self:skinStdButton{obj=_G[WCCUIMF .. "Tab3SubFrameCenterButton"]}
+		self:skinStdButton{obj=_G[WCCUIMF .. "Tab3SubFrameReloadUIButton"]}
+		self:skinStdButton{obj=_G[WCCUIMF .. "Tab3SubFrameColumnButton"]}
+		self:skinStdButton{obj=_G[WCCUIMF .. "Tab3SubFrameResetButton"]}
+	end
+
+	-- Alert Tab (4)
+	self:skinDropDown{obj=_G[WCCUIMF .. "Tab4SubFrameAlertDropDownMenu"]}
+	if self.modChkBtns then
+		-- skin check buttons
+		self:skinCheckButton{obj=_G[WCCUIMF .. "Tab4SubFrameAlertMissionsCheckButton"]}
+		self:skinCheckButton{obj=_G[WCCUIMF .. "Tab4SubFrameAlertAdvancementsCheckButton"]}
+		self:skinCheckButton{obj=_G[WCCUIMF .. "Tab4SubFrameAlertTroopsCheckButton"]}
+		self:skinCheckButton{obj=_G[WCCUIMF .. "Tab4SubFrameAlertDisableInInstancesCheckButton"]}
+	end
+
+	-- LDB Tab (5)
+	self:skinDropDown{obj=_G[WCCUIMF .. "Tab5SubFrameSourceDropDownMenu"]}
+	self:skinDropDown{obj=_G[WCCUIMF .. "Tab5SubFrameTextFormatDropDownMenu"]}
+
+	if self.modChkBtns then
+		self:skinCheckButton{obj=_G[WCCUIMF .. "Tab5SubFrameShowLabelsCheckButton"]}
+		self:skinCheckButton{obj=_G[WCCUIMF .. "Tab5SubFrameShowWhenNoneCheckButton"]}
+		self:skinCheckButton{obj=_G[WCCUIMF .. "Tab5SubFrameShowNextMissionCheckButton"]}
+		self:skinCheckButton{obj=_G[WCCUIMF .. "Tab5SubFrameShowNextMissionCharacterCheckButton"]}
+		self:skinCheckButton{obj=_G[WCCUIMF .. "Tab5SubFrameShowNextAdvancementCheckButton"]}
+		self:skinCheckButton{obj=_G[WCCUIMF .. "Tab5SubFrameShowNextAdvancementCharacterCheckButton"]}
+		self:skinCheckButton{obj=_G[WCCUIMF .. "Tab5SubFrameShowNextOrderCheckButton"]}
+		self:skinCheckButton{obj=_G[WCCUIMF .. "Tab5SubFrameShowNextOrderCharacterCheckButton"]}
+		self:skinCheckButton{obj=_G[WCCUIMF .. "Tab5SubFrameHideLibDBIconMinimapButtonCheckButton"]}
+		self:skinCheckButton{obj=_G[WCCUIMF .. "Tab5SubFrameShowCharacterTooltipLibDBIconMinimapButtonCheckButton"]}
+	end
+
+	-- minimap button
+	self.mmButs["WarCampaignsComplete"] = _G.WCCMinimapButton
+
+end
