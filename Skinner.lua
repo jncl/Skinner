b6aa06216af8532674c20251966df3a6be3ba6ed
Jncl <jncl@mac.com>
2019-02-12 12:55:35 +0000

    Updated version numbers

--- Skinner.lua
+++ Skinner.lua
@@ -33,8 +33,8 @@ do
 	aObj.blizzLoDFrames = {p = {}, n = {}, u = {}}
 
 	local betaInfo = {"9.0.0", 99999}
-	local ptrInfo = {"8.1.5", 28938}
-	local liveInfo = {"8.1.0", 29088}
+	local ptrInfo = {"8.1.5", 29281}
+	local liveInfo = {"8.1.0", 29297}
 	local buildInfo, portal = {_G.GetBuildInfo()}, _G.GetCVar("portal") or nil
 --@alpha@
 	aObj:Print(liveInfo[1] .. ", " .. liveInfo[2] .. ", " .. buildInfo[1] .. ", " .. buildInfo[2] .. ", " .. buildInfo[3] .. ", " .. buildInfo[4] .. ", " .. portal)
