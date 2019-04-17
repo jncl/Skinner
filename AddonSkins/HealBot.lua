c5a0aa4ec8efa1d29e45b31b7aabcabf463b4ded
Jncl <jncl@mac.com>
2019-04-17 09:05:16 +0100

    Updated HealBot skin

--- AddonSkins/HealBot.lua
+++ AddonSkins/HealBot.lua
@@ -3,7 +3,7 @@ local aName, aObj = ...
 if not aObj:isAddonEnabled("HealBot") then return end
 local _G = _G
 
-aObj.addonsToSkin.HealBot = function(self) -- v 8.0.1.10
+aObj.addonsToSkin.HealBot = function(self) -- v 8.1.5.2
 
 	-- Options frame
 	self:addSkinFrame{obj=_G.HealBot_Options_Contents, ft="a", kfs=true, nb=true}
@@ -97,45 +97,6 @@ aObj.addonsToSkin.HealBot = function(self) -- v 8.0.1.10
 		end
 		self:skinSlider{obj=_G.HealBot_Options_BarUpdateFreq}
 		self:addSkinFrame{obj=_G.HealBot_Options_GeneralSkinsFrame, ft="a", kfs=true, nb=true}
-		-- Healing
-		self:addSkinFrame{obj=_G.HealBot_Options_HealingSkinsFrame, ft="a", kfs=true, nb=true}
-		self:skinSlider{obj=_G.HealBot_Options_AlertLevelIC}
-		self:skinSlider{obj=_G.HealBot_Options_AlertLevelOC}
-		self:skinDropDown{obj=_G.HealBot_Options_EmergencyFilter}
-		self:addSkinFrame{obj=_G.HealBot_Options_HealRaidFrame, ft="a", kfs=true, nb=true}
-		self:skinDropDown{obj=_G.HealBot_Options_ExtraSort}
-		self:skinDropDown{obj=_G.HealBot_Options_ExtraSubSort}
-		self:addSkinFrame{obj=_G.HealBot_Options_HealSortFrame, ft="a", kfs=true, nb=true}
-		self:addSkinFrame{obj=_G.HealBot_Options_HealHideFrame, ft="a", kfs=true, nb=true}
-		if self.modBtns then
-			self:skinStdButton{obj=_G.HealBot_Options_HealingHealGroupSettings}
-			self:skinStdButton{obj=_G.HealBot_SkinsSubFrameSelectHealRaidFrameb}
-			self:skinStdButton{obj=_G.HealBot_SkinsSubFrameSelectHealSortFrameb}
-			self:skinStdButton{obj=_G.HealBot_SkinsSubFrameSelectHealHideFrameb}
-		end
-		-- Aggro
-		if self.modChkBtns then
-			self:skinCheckButton{obj=_G.HealBot_Options_AggroTrack}
-			self:skinCheckButton{obj=_G.HealBot_Options_HighlightActiveBar}
-			self:skinCheckButton{obj=_G.HealBot_Options_HighlightTargetBar}
-		end
-		self:skinDropDown{obj=_G.HealBot_Options_BarHealthNumFormatAggro}
-		self:skinDropDown{obj=_G.HealBot_Options_AggroIndAlertLevel}
-		self:skinDropDown{obj=_G.HealBot_Options_AggroAlertLevel}
-		self:skinSlider{obj=_G.HealBot_Options_AggroFlashFreq}
-		self:skinSlider{obj=_G.HealBot_Options_AggroFlashAlphaMin}
-		self:skinSlider{obj=_G.HealBot_Options_AggroFlashAlphaMax}
-		self:addSkinFrame{obj=_G.HealBot_Options_AggroSkinsFrame, ft="a", kfs=true, nb=true}
-		-- Enemy
-		if self.modChkBtns then
-			self:skinCheckButton{obj=_G.HealBot_Options_ShowEnemyIncSelf}
-			self:skinCheckButton{obj=_G.HealBot_Options_ShowEnemyIncTanks}
-			self:skinCheckButton{obj=_G.HealBot_Options_ShowEnemyIncArena}
-			self:skinCheckButton{obj=_G.HealBot_Options_ShowEnemyIncMyTargets}
-			self:skinCheckButton{obj=_G.HealBot_Options_HideEnemyOutOfCombat}
-		end
-		self:skinSlider{obj=_G.HealBot_Options_ShowEnemyNumBoss}
-		self:addSkinFrame{obj=_G.HealBot_Options_EnemySkinsFrame, ft="a", kfs=true, nb=true}
 		-- Protection
 		if self.modChkBtns then
 			self:skinCheckButton{obj=_G.HealBot_Options_CrashProt}
@@ -146,21 +107,11 @@ aObj.addonsToSkin.HealBot = function(self) -- v 8.0.1.10
 		self:addSkinFrame{obj=_G.HealBot_Options_CrashProtPanel, ft="a", kfs=true, nb=true}
 		self:addSkinFrame{obj=_G.HealBot_Options_CombatProtPanel, ft="a", kfs=true, nb=true}
 		self:addSkinFrame{obj=_G.HealBot_Options_ProtSkinsFrame, ft="a", kfs=true, nb=true}
-		-- Share
-		self:skinDropDown{obj=_G.HealBot_Options_ShareSkin}
-		if self.modBtns then
-			self:skinStdButton{obj=_G.HealBot_Options_ShareSkinb}
-			self:skinStdButton{obj=_G.HealBot_Options_LoadSkinb}
-		end
-		self:skinSlider{obj=_G.HealBot_Options_ShareExternalScrollScrollBar}
-		self:addSkinFrame{obj=_G.HealBot_Options_ShareExternalEditBoxFrame, ft="a", kfs=true, nb=true}
-		self:addSkinFrame{obj=_G.HealBot_Options_ShareSkinsFrame, ft="a", kfs=true, nb=true}
 		-- Chat
 		self:skinEditBox{obj=_G.HealBot_Options_NotifyChan, regs={6}}
 		self:skinEditBox{obj=_G.HealBot_Options_NotifyOtherMsg, regs={6, 7}}
 		self:addSkinFrame{obj=_G.HealBot_Options_ChatSkinsFrame, ft="a", kfs=true, nb=true}
-
-		-- Frames
+		-- Frames (General)
 		if self.modBtns then
 			 self:skinStdButton{obj=_G.HealBot_Options_FramesHealGroupSettings}
 		end
@@ -189,7 +140,6 @@ aObj.addonsToSkin.HealBot = function(self) -- v 8.0.1.10
 			self:skinStdButton{obj=_G.HealBot_BorderColorpickb}
 		end
 		self:addSkinFrame{obj=_G.HealBot_Options_FramesSkinsFrame, ft="a", kfs=true, nb=true}
-
 			-- Heal Groups
 			self:skinDropDown{obj=_G.HealBot_Options_HealGroups1Frame}
 			self:skinDropDown{obj=_G.HealBot_Options_HealGroups2Frame}
@@ -209,6 +159,9 @@ aObj.addonsToSkin.HealBot = function(self) -- v 8.0.1.10
 				end
 			end
 			-- Headers
+			if self.modChkBtns then
+				self:skinCheckButton{obj=_G.HealBot_Options_ShowHeaders}
+			end
 			self:skinSlider{obj=_G.HealBot_Options_HeadTextureS}
 			self:skinSlider{obj=_G.HealBot_Options_HeadHightS}
 			self:skinSlider{obj=_G.HealBot_Options_HeadWidthS}
@@ -217,66 +170,130 @@ aObj.addonsToSkin.HealBot = function(self) -- v 8.0.1.10
 			self:skinDropDown{obj=_G.HealBot_Options_HeadFontOutline}
 			self:addSkinFrame{obj=_G.HealBot_Options_HeadersSkinsFrame, ft="a", kfs=true, nb=true}
 			-- Bars
-			self:skinSlider{obj=_G.HealBot_Options_BarTextureS}
-			self:skinSlider{obj=_G.HealBot_Options_BarNumColsS}
-			self:skinSlider{obj=_G.HealBot_Options_BarHeightS}
-			self:skinSlider{obj=_G.HealBot_Options_BarWidthS}
-			self:skinSlider{obj=_G.HealBot_Options_BarBRSpaceS}
-			self:skinSlider{obj=_G.HealBot_Options_BarBCSpaceS}
-			self:skinSlider{obj=_G.HealBot_Options_AggroBarSize}
-			self:skinSlider{obj=_G.HealBot_Options_Bar2Size}
+			if self.modBtns then
+				 self:skinStdButton{obj=_G.HealBot_Options_SkinsFramesBarsGeneralb}
+				 self:skinStdButton{obj=_G.HealBot_Options_SkinsFramesBarsColoursb}
+				 self:skinStdButton{obj=_G.HealBot_Options_SkinsFramesBarsTextb}
+				 self:skinStdButton{obj=_G.HealBot_Options_SkinsFramesBarsSortb}
+				 self:skinStdButton{obj=_G.HealBot_Options_SkinsFramesBarsVisibilityb}
+				 self:skinStdButton{obj=_G.HealBot_Options_SkinsFramesBarsAggrob}
+			end
 			self:addSkinFrame{obj = _G.HealBot_Options_BarsSkinsFrame, ft="a", kfs=true, nb=true}
-			-- Bar Colours
-			self:skinDropDown{obj = _G.HealBot_Options_BarHealthColour}
-			self:skinSlider{obj=_G.HealBot_Options_BarAlpha}
-			self:skinDropDown{obj = _G.HealBot_Options_BarHealthBackColour}
-			self:skinSlider{obj=_G.HealBot_Options_BarOutlineBackGround}
-			self:skinSlider{obj=_G.HealBot_Options_BarAlphaBackGround}
-			self:skinDropDown{obj = _G.HealBot_Options_BarIncHealColour}
-			self:skinSlider{obj=_G.HealBot_Options_BarAlphaInHeal}
-			self:skinDropDown{obj = _G.HealBot_Options_AbsorbColour}
-			self:skinSlider{obj=_G.HealBot_Options_BarAlphaAbsorb}
-			self:skinSlider{obj=_G.HealBot_Options_BarAlphaEor}
-			self:skinSlider{obj=_G.HealBot_Options_BarAlphaDis}
-			self:addSkinFrame{obj = _G.HealBot_Options_BarColoursSkinsFrame, ft="a", kfs=true, nb=true}
-			-- Bar text
-			if self.modChkBtns then
-				self:skinCheckButton{obj=_G.HealBot_Options_BarTextInClassColour}
-				self:skinCheckButton{obj=_G.HealBot_Options_ShowClassOnBar}
-				self:skinCheckButton{obj=_G.HealBot_Options_ShowNameOnBar}
-				self:skinCheckButton{obj=_G.HealBot_Options_NumberTextLines}
-				self:skinCheckButton{obj=_G.HealBot_Options_ShowHealthOnBar}
-			end
-			self:skinDropDown{obj=_G.HealBot_Options_BarHealthIncHeal}
-			self:skinDropDown{obj=_G.HealBot_Options_BarHealthType}
-			self:skinDropDown{obj=_G.HealBot_Options_BarHealthNumFormat1}
-			self:skinDropDown{obj=_G.HealBot_Options_BarHealthNumFormat2}
-			self:skinSlider{obj=_G.HealBot_Options_FontName}
-			self:skinSlider{obj=_G.HealBot_Options_TextAlign}
-			self:skinSlider{obj=_G.HealBot_Options_FontHeight}
-			self:skinDropDown{obj=_G.HealBot_Options_FontOutline}
-			self:addSkinFrame{obj=_G.HealBot_Options_TextSkinsFrame, ft="a", kfs=true, nb=true}
+				-- General
+				self:skinSlider{obj=_G.HealBot_Options_BarTextureS}
+				self:skinSlider{obj=_G.HealBot_Options_BarNumColsS}
+				self:skinSlider{obj=_G.HealBot_Options_BarHeightS}
+				self:skinSlider{obj=_G.HealBot_Options_BarWidthS}
+				self:skinSlider{obj=_G.HealBot_Options_BarBRSpaceS}
+				self:skinSlider{obj=_G.HealBot_Options_BarBCSpaceS}
+				self:skinSlider{obj=_G.HealBot_Options_AggroBarSize}
+				self:skinSlider{obj=_G.HealBot_Options_Bar2Size}
+				self:addSkinFrame{obj = _G.HealBot_Options_SkinsFramesBarsGeneral, ft="a", kfs=true, nb=true, ofs=0}
+				-- Colours
+				self:skinDropDown{obj = _G.HealBot_Options_BarHealthColour}
+				self:skinSlider{obj=_G.HealBot_Options_BarAlpha}
+				self:skinDropDown{obj = _G.HealBot_Options_BarHealthBackColour}
+				self:skinSlider{obj=_G.HealBot_Options_BarOutlineBackGround}
+				self:skinSlider{obj=_G.HealBot_Options_BarAlphaBackGround}
+				self:skinDropDown{obj = _G.HealBot_Options_BarIncHealColour}
+				self:skinSlider{obj=_G.HealBot_Options_BarAlphaInHeal}
+				self:skinDropDown{obj = _G.HealBot_Options_AbsorbColour}
+				self:skinSlider{obj=_G.HealBot_Options_BarAlphaAbsorb}
+				self:skinSlider{obj=_G.HealBot_Options_BarAlphaEor}
+				self:skinSlider{obj=_G.HealBot_Options_BarAlphaDis}
+				self:addSkinFrame{obj = _G.HealBot_Options_SkinsFramesBarsColours, ft="a", kfs=true, nb=true, ofs=0}
+				-- Text
+				if self.modChkBtns then
+					self:skinCheckButton{obj=_G.HealBot_Options_BarTextInClassColour}
+					self:skinCheckButton{obj=_G.HealBot_Options_ShowClassOnBar}
+					self:skinCheckButton{obj=_G.HealBot_Options_ShowNameOnBar}
+					self:skinCheckButton{obj=_G.HealBot_Options_NumberTextLines}
+					self:skinCheckButton{obj=_G.HealBot_Options_ShowHealthOnBar}
+				end
+				self:skinSlider{obj=_G.HealBot_Options_MaxChars}
+				self:skinDropDown{obj=_G.HealBot_Options_BarHealthIncHeal}
+				self:skinDropDown{obj=_G.HealBot_Options_BarHealthType}
+				self:skinDropDown{obj=_G.HealBot_Options_BarHealthNumFormat1}
+				self:skinDropDown{obj=_G.HealBot_Options_BarHealthNumFormat2}
+				self:skinEditBox{obj=_G.HealBot_Options_DisconnectedTag, regs={6, 7}}
+				self:skinEditBox{obj=_G.HealBot_Options_UnitDeadTag, regs={6}}
+				self:skinEditBox{obj=_G.HealBot_Options_OutOfRangeTag, regs={6}}
+				self:skinEditBox{obj=_G.HealBot_Options_ReserverTag, regs={6}}
+				self:skinSlider{obj=_G.HealBot_Options_FontName}
+				self:skinSlider{obj=_G.HealBot_Options_TextAlign}
+				self:skinSlider{obj=_G.HealBot_Options_FontHeight}
+				self:skinDropDown{obj=_G.HealBot_Options_FontOutline}
+				self:addSkinFrame{obj=_G.HealBot_Options_SkinsFramesBarsText, ft="a", kfs=true, nb=true, ofs=0}
+				-- Sort
+				if self.modChkBtns then
+					self:skinCheckButton{obj=_G.HealBot_Options_SubSortPlayerFirst}
+					self:skinCheckButton{obj=_G.HealBot_Options_SortOutOfRangeLast}
+				end
+				self:skinDropDown{obj=_G.HealBot_Options_ExtraSort}
+				self:skinDropDown{obj=_G.HealBot_Options_ExtraSubSort}
+				self:addSkinFrame{obj=_G.HealBot_Options_SkinsFramesBarsSort, ft="a", kfs=true, nb=true, ofs=0}
+				-- Visibility
+				self:skinSlider{obj=_G.HealBot_Options_AlertLevelIC}
+				self:skinSlider{obj=_G.HealBot_Options_AlertLevelOC}
+				if self.modChkBtns then
+					self:skinCheckButton{obj=_G.HealBot_Options_HideBars}
+				end
+				self:skinDropDown{obj=_G.HealBot_Options_EmergencyFilter}
+				self:addSkinFrame{obj=_G.HealBot_Options_SkinsFramesBarsVisibility, ft="a", kfs=true, nb=true, ofs=0}
+				-- Visibility Enemy
+				if self.modChkBtns then
+					self:skinCheckButton{obj=_G.HealBot_Options_ShowEnemyIncSelf}
+					self:skinCheckButton{obj=_G.HealBot_Options_ShowEnemyIncTanks}
+					self:skinCheckButton{obj=_G.HealBot_Options_ShowEnemyIncArena}
+					self:skinCheckButton{obj=_G.HealBot_Options_ShowEnemyIncArenaPets}
+					self:skinCheckButton{obj=_G.HealBot_Options_ShowEnemyIncMyTargets}
+					self:skinCheckButton{obj=_G.HealBot_Options_HideEnemyOutOfCombat}
+					self:skinCheckButton{obj=_G.HealBot_Options_EnemyExistsPlayerTargets}
+					self:skinCheckButton{obj=_G.HealBot_Options_EnemyExistsArena}
+					self:skinCheckButton{obj=_G.HealBot_Options_EnemyExistsBosses}
+				end
+				self:skinSlider{obj=_G.HealBot_Options_ShowEnemyNumBoss}
+				self:addSkinFrame{obj=_G.HealBot_Options_SkinsFramesBarsVisibilityEnemy, ft="a", kfs=true, nb=true, ofs=0}
+				-- Aggro
+				if self.modChkBtns then
+					self:skinCheckButton{obj=_G.HealBot_Options_AggroTrack}
+					self:skinCheckButton{obj=_G.HealBot_Options_HighlightActiveBar}
+					self:skinCheckButton{obj=_G.HealBot_Options_HighlightTargetBar}
+				end
+				self:skinDropDown{obj=_G.HealBot_Options_BarHealthNumFormatAggro}
+				self:skinDropDown{obj=_G.HealBot_Options_AggroIndAlertLevel}
+				self:skinDropDown{obj=_G.HealBot_Options_AggroAlertLevel}
+				self:skinSlider{obj=_G.HealBot_Options_AggroFlashFreq}
+				self:skinSlider{obj=_G.HealBot_Options_AggroFlashAlphaMin}
+				self:skinSlider{obj=_G.HealBot_Options_AggroFlashAlphaMax}
+				self:addSkinFrame{obj=_G.HealBot_Options_SkinsFramesBarsAggro, ft="a", kfs=true, nb=true, ofs=0}
 			-- Icons
-			if self.modChkBtns then
-				self:skinCheckButton{obj=_G.HealBot_Options_BarButtonShowHoT}
-				self:skinCheckButton{obj=_G.HealBot_Options_ShowDebuffIcon}
-				self:skinCheckButton{obj=_G.HealBot_Options_ShowDirection}
-				self:skinCheckButton{obj=_G.HealBot_Options_ShowReadyCheck}
-				self:skinCheckButton{obj=_G.HealBot_Options_BarButtonShowRaidIcon}
-				self:skinCheckButton{obj=_G.HealBot_BarButtonIconAlwaysEnabled}
+			if self.modBtns then
+				self:skinStdButton{obj=_G.HealBot_Options_SkinsFrameIconsGeneralb}
+				self:skinStdButton{obj=_G.HealBot_Options_SkinsFrameIconsTextb}
 			end
-			self:skinSlider{obj=_G.HealBot_BarButtonIconScale}
-			self:skinDropDown{obj=_G.HealBot_Options_FilterHoTctl}
-			self:skinDropDown{obj=_G.HealBot_Options_Class_HoTctlName}
-			self:skinDropDown{obj=_G.HealBot_Options_Class_HoTctlAction}
 			self:addSkinFrame{obj=_G.HealBot_Options_IconsSkinsFrame, ft="a", kfs=true, nb=true}
-			-- Icon text
-			self:skinSlider{obj=_G.HealBot_BarButtonIconTextDurationTime}
-			self:skinSlider{obj=_G.HealBot_BarButtonIconTextDurationWarn}
-			self:skinSlider{obj=_G.HealBot_BarButtonIconTextScale}
-			self:addSkinFrame{obj = _G.HealBot_Options_IconTextSkinsFrame, ft="a", kfs=true, nb=true}
+				-- General
+				if self.modChkBtns then
+					self:skinCheckButton{obj=_G.HealBot_Options_BarButtonShowHoT}
+					self:skinCheckButton{obj=_G.HealBot_Options_ShowDebuffIcon}
+					self:skinCheckButton{obj=_G.HealBot_Options_ShowDirection}
+					self:skinCheckButton{obj=_G.HealBot_Options_ShowReadyCheck}
+					self:skinCheckButton{obj=_G.HealBot_Options_BarButtonShowRaidIcon}
+					self:skinCheckButton{obj=_G.HealBot_BarButtonIconAlwaysEnabled}
+					self:skinCheckButton{obj=_G.HealBot_BarButtonIconFadeOnExpire}
+				end
+				self:skinSlider{obj=_G.HealBot_BarButtonMaxBuffIcons}
+				self:skinSlider{obj=_G.HealBot_BarButtonMaxDeuffIcons} -- N.B. note spelling !!
+				self:skinSlider{obj=_G.HealBot_BarButtonIconScale}
+				self:addSkinFrame{obj=_G.HealBot_Options_SkinsFrameIconsGeneral, ft="a", kfs=true, nb=true, ofs=0}
+				-- Text
+				self:skinSlider{obj=_G.HealBot_BarButtonIconTextDurationTime}
+				self:skinSlider{obj=_G.HealBot_BarButtonIconTextDurationWarn}
+				self:skinSlider{obj=_G.HealBot_BarButtonIconTextScale}
+				self:addSkinFrame{obj=_G.HealBot_Options_SkinsFrameIconsText, ft="a", kfs=true, nb=true, ofs=0}
 
-	-- Cure [Panel4]
+	-- Debuffs [Panel4]
 	self:skinCheckButton{obj=_G.HealBot_Options_MonitorDebuffs}
 		-- CureDispelCleanse
 		self:skinDropDown{obj=_G.HealBot_Options_CDCTxt1}
@@ -328,14 +345,22 @@ aObj.addonsToSkin.HealBot = function(self) -- v 8.0.1.10
 		self:skinSlider{obj=_G.HealBot_Options_WarningSound}
 		self:addSkinFrame{obj=_G.HealBot_Options_WarningCureFrame, ft="a", kfs=true, nb=true}
 
-	-- Buffs [ Panel 5]
-	for i = 1, 8 do
-		self:skinDropDown{obj=_G["HealBot_Options_BuffTxt".. i]}
-		self:skinDropDown{obj=_G["HealBot_Options_BuffGroups" .. i]}
-	end
-	self:skinSlider{obj=_G.HealBot_Options_ShortBuffTimer}
-	self:skinSlider{obj=_G.HealBot_Options_LongBuffTimer}
-	self:addSkinFrame{obj=_G.HealBot_Options_BuffsPanel, ft="a", kfs=true, nb=true}
+	-- Buffs [Panel5]
+		--General
+		if self.modChkBtns then
+			self:skinCheckButton{obj=_G.HealBot_Options_MonitorBuffs}
+		end
+		for i = 1, 8 do
+			self:skinDropDown{obj=_G["HealBot_Options_BuffTxt".. i]}
+			self:skinDropDown{obj=_G["HealBot_Options_BuffGroups" .. i]}
+		end
+		self:skinSlider{obj=_G.HealBot_Options_ShortBuffTimer}
+		self:skinSlider{obj=_G.HealBot_Options_LongBuffTimer}
+		self:addSkinFrame{obj=_G.HealBot_Options_BuffsPanel, ft="a", kfs=true, nb=true}
+		-- Icons
+		self:skinDropDown{obj=_G.HealBot_Options_FilterHoTctl}
+		self:skinDropDown{obj=_G.HealBot_Options_Class_HoTctlName}
+		self:skinDropDown{obj=_G.HealBot_Options_Class_HoTctlAction}
 
 	-- Tips [Panel6]
 	if self.modChkBtns then
@@ -373,6 +398,15 @@ aObj.addonsToSkin.HealBot = function(self) -- v 8.0.1.10
 	self:skinSlider{obj=_G.HealBot_Options_NumberTestMyTargets}
 	self:skinSlider{obj=_G.HealBot_Options_NumberTestPets}
 	self:skinSlider{obj=_G.HealBot_Options_NumberTestEnemy}
+
+	-- Import / Export [Panel9]
+	self:skinDropDown{obj=_G.HealBot_Options_InOutSkin}
+	if self.modBtns then
+		self:skinStdButton{obj=_G.HealBot_Options_ShareSkinb}
+		self:skinStdButton{obj=_G.HealBot_Options_LoadSkinb}
+	end
+	self:skinSlider{obj=_G.HealBot_Options_ShareExternalScrollScrollBar}
+	self:addSkinFrame{obj=_G.HealBot_Options_ShareExternalEditBoxFrame, ft="a", kfs=true, nb=true}
 
 	-- tooltips
 	if self.db.profile.Tooltips.skin then
