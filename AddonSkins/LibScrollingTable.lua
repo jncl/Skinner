7dd5a533ad16b4fc74c74a2c21f938dc5cf06797
Jncl <jncl@mac.com>
2020-02-06 17:21:42 +0000

    Bugfix for LibScrollingTable

--- AddonSkins/LibScrollingTable.lua
+++ AddonSkins/LibScrollingTable.lua
@@ -6,9 +6,7 @@ aObj.libsToSkin["ScrollingTable"] = function(self) -- v ScrollingTable, 40300
 	if self.initialized.ScrollingTable then return end
 	self.initialized.ScrollingTable = true
 
-	local ST = _G.LibStub:GetLibrary("ScrollingTable", true)
-
-	if ST then
+	if _G.LibStub:GetLibrary("ScrollingTable", true) then
 		-- create function to be used in other skins
 		function self:skinScrollingTable(obj)
 			self:skinSlider{obj=obj.scrollframe.ScrollBar}
@@ -16,13 +14,11 @@ aObj.libsToSkin["ScrollingTable"] = function(self) -- v ScrollingTable, 40300
 			_G[obj.frame:GetName() .. "ScrollTroughBorder"].background:SetTexture(nil)
 			self:addSkinFrame{obj=obj.frame, ft="a", kfs=true, nb=true, y1=2, y2=-1}
 		end
-		self:RawHook(ST, "CreateST", function(this, ...)
-			local st = self.hooks[this].CreateST(this, cols, ...)
+		self:RawHook(_G.LibStub:GetLibrary("ScrollingTable", true), "CreateST", function(this, ...)
+			local st = self.hooks[this].CreateST(this, ...)
 			self:skinScrollingTable(st)
 			return st
 		end, true)
-	else
-		self.skinScrollingTable = _G.nop
 	end
 
 end
