95ce62908f8c5a7b36624fc2ce581a03768d44bc
Jncl <jncl@mac.com>
2020-04-05 17:24:15 +0100

    Updated version numbers

--- Skinner.lua
+++ Skinner.lua
@@ -21,9 +21,9 @@ do
 	local buildInfo = {
 		beta        = {"9.0.0", 99999}, -- Shadowlands 2020
 		classic_ptr = {"1.13.4", 33728},
-		retail_ptr  = {"8.3.0", 33724},
+		retail_ptr  = {"8.3.0", 33775},
 		classic     = {"1.13.4", 33728},
-		retail      = {"8.3.0", 33724},
+		retail      = {"8.3.0", 33941},
 		curr        = {_G.GetBuildInfo()},
    	}
 
