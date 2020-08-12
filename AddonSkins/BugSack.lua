96ea3cf8ee5c1576a615be7420b950b2faddf784
Jncl <jncl@mac.com>
2020-08-12 14:48:32 +0100

    Updated BugSack skin

--- AddonSkins/BugSack.lua
+++ AddonSkins/BugSack.lua
@@ -9,19 +9,20 @@ aObj.addonsToSkin.BugSack = function(self) -- v r300-release
 
 	self:SecureHook(_G.BugSack, "OpenSack", function(this)
 		self:skinSlider{obj=_G.BugSackScroll.ScrollBar, size=3}
-		self:skinStdButton{obj=_G.BugSackPrevButton}
-		self:skinStdButton{obj=_G.BugSackSendButton}
-		self:skinStdButton{obj=_G.BugSackNextButton}
-		self:skinCloseButton{obj=self:getChild(_G.BugSackFrame, 1)}
+		self:moveObject{obj=self:getRegion(_G.BugSackFrame, 11), x=0, y=-8} -- countLabel
 		self:addSkinFrame{obj=_G.BugSackFrame, ft="a", kfs=true, nb=true, y1=-2, x2=-1, y2=2}
 		_G.RaiseFrameLevelByTwo(_G.BugSackFrame)
-		self:moveObject{obj=self:getRegion(_G.BugSackFrame, 11), x=0, y=-8} -- countLabel
+		if self.modBtns then
+			self:skinStdButton{obj=_G.BugSackPrevButton}
+			self:skinStdButton{obj=_G.BugSackSendButton}
+			self:skinStdButton{obj=_G.BugSackNextButton}
+			self:skinCloseButton{obj=self:getChild(_G.BugSackFrame, 1)}
+		end
 
 		-- tabs
 		for _, tabObj in _G.pairs{_G.BugSackTabAll, _G.BugSackTabSession, _G.BugSackTabLast} do
 			self:keepRegions(tabObj, {7, 8}) -- N.B. region 7 is text, 8 is highlight
 			self:addSkinFrame{obj=tabObj, ft="a", nb=true, noBdr=self.isTT, x1=6, y1=0, x2=-6, y2=2}
-			tabObj.sf.ignore = true
 			if self.isTT then
 				if tabObj == _G.BugSackTabAll then
 					self:setActiveTab(tabObj.sf)
