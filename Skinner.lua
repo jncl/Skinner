d159d7b99b336b6e7418bca0db5ba4650140156a
Jncl <jncl@mac.com>
2020-08-30 23:01:54 +0100

    Updated version numbers

--- Skinner.lua
+++ Skinner.lua
@@ -3,11 +3,11 @@ local aName, aObj = ...
 local _G = _G
 
 local buildInfo = {
-	beta        = {"9.0.1", 35598},
+	beta        = {"9.0.1", 35707},
 	classic_ptr = {"1.13.5", 35000},
-	retail_ptr  = {"9.0.1", 35598},
-	retail      = {"8.3.7", 35435},
-	classic     = {"1.13.5", 35395},
+	retail_ptr  = {"9.0.1", 35707},
+	retail      = {"8.3.7", 35662},
+	classic     = {"1.13.5", 35705},
 	curr        = {_G.GetBuildInfo()},
 }
 
