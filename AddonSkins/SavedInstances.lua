3dc8ee2fee25c4612d9faca9cda7ccd37fea9227
Jncl <jncl@mac.com>
2020-10-30 10:36:28 +0000

    Updated Ace3 & SavedInstances skins

--- AddonSkins/SavedInstances.lua
+++ AddonSkins/SavedInstances.lua
@@ -2,10 +2,15 @@ local _, aObj = ...
 if not aObj:isAddonEnabled("SavedInstances") then return end
 local _G = _G
 
-aObj.addonsToSkin.SavedInstances = function(self) -- v 9.0.1
+-- hook this here to stop frames being 'Skinned' if running ElvUI or TukUI
+local SI = _G.LibStub:GetLibrary("AceAddon-3.0"):GetAddon("SavedInstances", true)
+if SI then
+	aObj:RawHook(SI, "SkinFrame", function(this, ...)
+		return
+	end, true)
+end
 
-	local SI = _G.LibStub:GetLibrary("AceAddon-3.0"):GetAddon("SavedInstances", true)
-	if not SI then return end
+aObj.addonsToSkin.SavedInstances = function(self) -- v 9.0.1
 
 	self:SecureHook(SI, "ShowDetached", function(this)
 		self:addSkinFrame{obj=this.detachframe, ft="a", kfs=true}
