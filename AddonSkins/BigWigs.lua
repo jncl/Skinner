53b57bf99ab3cb4854126e25d046016b9a5958ee
Jncl <jncl@mac.com>
2020-12-14 22:08:35 +0000

    Bugfix for BigWigs

--- AddonSkins/BigWigs.lua
+++ AddonSkins/BigWigs.lua
@@ -7,8 +7,10 @@ aObj.addonsToSkin.BigWigs = function(self) -- v 191.2
 	-- skin BigWigs statusbar
 	if _G.BigWigsLoader then
 		_G.BigWigsLoader.RegisterMessage(aObj, "BigWigs_FrameCreated", function(event, frame, name)
-			aObj:removeRegions(frame, {2, 4}) -- background & border textures
-			aObj:skinStatusBar{obj=frame, fi=0}
+			if name == "QueueTimer" then
+				aObj:removeRegions(frame, {2, 4}) -- background & border textures
+				aObj:skinStatusBar{obj=frame, fi=0}
+			end
 		end)
 	end
 
