14a57433390a7e02ce4c1b0596a22e51e381d7b5
Jncl <jncl@mac.com>
2021-01-08 20:29:50 +0000

    Skinned ColorPickerPlus

---
+++ AddonSkins/ColorPickerPlus.lua
@@ -0,0 +1,18 @@
+local _, aObj = ...
+if not aObj:isAddonEnabled("ColorPickerPlus") then return end
+local _G = _G
+
+aObj.addonsToSkin.ColorPickerPlus = function(self) -- r 48
+
+	if not self.isClsc then
+		self:keepFontStrings(_G.ColorPPHeaderTitle)
+		self:moveObject{obj=_G.ColorPPHeaderTitle, y=-4}
+	end
+
+	if self.modBtns then
+		self:skinStdButton{obj=_G.ColorPPCopy}
+		self:skinStdButton{obj=_G.ColorPPPaste}
+		self:skinOtherButton{obj=_G.ColorPPSwitcher, text="@", noSkin=true}
+	end
+
+end
