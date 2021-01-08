5c41d1e43fcccc61a78079e135211d05571905b9
Jncl <jncl@mac.com>
2021-01-08 20:30:10 +0000

    Updated oRA3 & WIM skins

--- AddonSkins/WIM.lua
+++ AddonSkins/WIM.lua
@@ -1,33 +1,18 @@
-local aName, aObj = ...
+local _, aObj = ...
 if not aObj:isAddonEnabled("WIM") then return end
 local _G = _G
 
-aObj.addonsToSkin.WIM = function(self) -- v 3.7.14
+aObj.addonsToSkin.WIM = function(self) -- v 3.8.15
 
 	local function skinWindow(msgFrame)
-
-		if msgFrame.sf then return end
-
-		aObj:keepFontStrings(msgFrame.widgets.Backdrop)
 		msgFrame.widgets.class_icon:SetAlpha(1)
-
-		local eBox = msgFrame.widgets.msg_box
-		eBox:SetWidth(msgFrame:GetWidth() - 20)
-		local xOfs = _G.select(4, msgFrame.widgets.close:GetPoint())
-		xOfs = _G.floor(xOfs)
-		if xOfs == 4 then
-			eBox:SetHeight(eBox:GetHeight() - 5)
-			aObj:skinEditBox(eBox, {9})
-			eBox:SetPoint("TOPLEFT", msgFrame, "BOTTOMLEFT", 10, 30)
-			aObj:moveObject{obj=msgFrame.widgets.close, x=-2}
-			aObj:moveObject{obj=msgFrame.widgets.class_icon, y=-2}
+		aObj:moveObject{obj=msgFrame.widgets.class_icon, x=1, y=-6}
+		aObj:skinObject("editbox", {obj=msgFrame.widgets.msg_box, ofs=0})
+		aObj:skinObject("frame", {obj=msgFrame.widgets.Backdrop, kfs=true, ofs=1, y1=-2})
+		if aObj.modBtns then
+			aObj:skinOtherButton{obj=msgFrame.widgets.close, font=self.fontS, text="⌵"}
 		end
-		eBox, xOfs = nil, nil
-		aObj:addSkinFrame{obj=msgFrame, ft="a", kfs=true}
-
 	end
-
-	-- hook these to skin the Message Frames
 	self:RawHook(_G.WIM, "CreateWhisperWindow", function(playerName)
 		local msgFrame = self.hooks[_G.WIM].CreateWhisperWindow(playerName)
 		skinWindow(msgFrame)
@@ -44,7 +29,6 @@ aObj.addonsToSkin.WIM = function(self) -- v 3.7.14
 		return msgFrame
 	end, true)
 
-	-- skin the history viewer
 	if _G.WIM.modules["History"]
 	and _G.WIM.modules["History"].enabled
 	then
@@ -53,123 +37,136 @@ aObj.addonsToSkin.WIM = function(self) -- v 3.7.14
 			hvFrame.title:SetPoint("TOPLEFT", 50 , -7)
 			hvFrame.close:SetPoint("TOPRIGHT", -4, -4)
 		    hvFrame.resize:SetPoint("BOTTOMRIGHT", 0, 0)
-			self:applySkin(hvFrame)
-			-- navigation panel
-			hvFrame.nav.border:Hide()
-			self:skinDropDown(hvFrame.nav.user)
-			self:skinSlider{obj=hvFrame.nav.filters.scroll.ScrollBar, size=3}
-			hvFrame.nav.filters.border:Hide()
-			self:applySkin(hvFrame.nav.filters)
-			self:skinSlider{obj=hvFrame.nav.userList.scroll.ScrollBar, size=3}
-			hvFrame.nav.userList.border:Hide()
-			self:applySkin(hvFrame.nav.userList)
-			-- search
-			hvFrame.search.bg:Hide()
+			hvFrame.nav.border:SetTexture(nil)
+			self:skinObject("dropdown", {obj=hvFrame.nav.user})
+			self:skinObject("slider", {obj=hvFrame.nav.filters.scroll.ScrollBar})
+			hvFrame.nav.filters.border:SetTexture(nil)
+			self:skinObject("frame", {obj=hvFrame.nav.filters, fb=true})
+			self:skinObject("slider", {obj=hvFrame.nav.userList.scroll.ScrollBar})
+			hvFrame.nav.userList.border:SetTexture(nil)
+			self:skinObject("frame", {obj=hvFrame.nav.userList, fb=true})
+			hvFrame.search.bg:SetTexture(nil)
 			self:keepFontStrings(hvFrame.search.text)
-			self:skinEditBox(hvFrame.search.text, {9})
-			-- content panel
-			hvFrame.content.border:Hide()
-			self:skinSlider{obj=hvFrame.content.textFrame.ScrollBar, size=3}
-			self:applySkin(hvFrame.content)
-			-- progress viewer
+			self:skinObject("editbox", {obj=hvFrame.search.text, ofs=4})
+			hvFrame.content.border:SetTexture(nil)
+			self:skinObject("slider", {obj=hvFrame.content.textFrame.ScrollBar})
+			self:skinObject("frame", {obj=hvFrame.content, fb=true})
+			if self.modBtnBs then
+				self:addButtonBorder{obj=hvFrame.content.chatFrame.up, ofs=-1, x1=0}
+				self:addButtonBorder{obj=hvFrame.content.chatFrame.down, ofs=-1, x1=0}
+				self:SecureHook(hvFrame.content.chatFrame, "update", function(this)
+					self:clrBtnBdr(this.up)
+					self:clrBtnBdr(this.down)
+				end)
+			end
 			self:keepFontStrings(hvFrame.progressBar)
-			self:applySkin(hvFrame.progressBar)
+			self:skinObject("frame", {obj=hvFrame.progressBar})
+			self:skinObject("frame", {obj=hvFrame})
+			hvFrame = nil
+
 			self:Unhook(_G.WIM, "ShowHistoryViewer")
 		end)
 	end
 
-	-- skin the Menu (Minimap/LDB)
 	if _G.WIM.modules["Menu"]
 	and _G.WIM.modules["Menu"].enabled
 	then
 		self:SecureHook(_G.WIM.Menu, "Show", function(this, ...)
-			for _, group in _G.pairs(_G.WIM.Menu.groups) do
+			for _, group in _G.pairs(this.groups) do
 				group:SetBackdrop(nil)
 				group.title:SetPoint("TOPLEFT", 10, -8)
 				group.title:SetPoint("TOPRIGHT", -10, -8);
 			end
-			self:SecureHook(_G.WIM.Menu, "Refresh", function(this)
+			self:SecureHook(this, "Refresh", function(this)
 				self:adjWidth{obj=this, adj=-30}
 				self:adjHeight{obj=this, adj=-20}
 			end)
-			self:adjWidth{obj=_G.WIM.Menu, adj=-30}
-			self:adjHeight{obj=_G.WIM.Menu, adj=-20}
-			self:applySkin(_G.WIM.Menu)
+			self:adjWidth{obj=this, adj=-30}
+			self:adjHeight{obj=this, adj=-20}
+			self:skinObject("frame", {obj=this})
+
 			self:Unhook(_G.WIM.Menu, "Show")
 		end)
 	end
 
-	-- skin any existing chat windows
+	local mFrame
 	for i = 1, 10 do
-		local mFrame = _G["WIM3_msgFrame"..i]
-		if mFrame then skinWindow(mFrame) end
+		mFrame = _G["WIM3_msgFrame" .. i]
+		if mFrame then
+			skinWindow(mFrame)
+		end
 	end
+	mFrame = nil
 
-	-- minimap button
 	self.mmButs["WIM"] = _G.WIM3MinimapButton
 
-	-- skin WIM_Options
 	self:checkAndRun("WIM_Options", "o")
 
 end
 
-aObj.otherAddons.WIM_Options = function(self)
+aObj.otherAddons.WIM_Options = function(self) -- v 3.8.15
 
-	local r, g, b, a = self.bbColour[1], self.bbColour[2], self.bbColour[3], self.bbColour[4]
 	local function checkKids(obj)
-
-		if obj.sknd then return end
-		obj.sknd = true
-
+		if obj.sknd then
+			return
+		else
+			obj.sknd = true
+		end
 		for _, child in _G.ipairs{obj:GetChildren()} do
 			if aObj:isDropDown(child) then
-				aObj:skinDropDown{obj=child, x2= obj.mf and 110 or nil}
+				aObj:skinObject("dropdown", {obj=child, x2=obj.mf and 110})
 			elseif child.backdrop then
-				child.backdrop.top:SetColorTexture(r, g, b, a)
-				child.backdrop.bottom:SetColorTexture(r, g, b, a)
-				child.backdrop.left:SetColorTexture(r, g, b, a)
-				child.backdrop.right:SetColorTexture(r, g, b, a)
+				child.backdrop.top:SetColorTexture(self.bbClr:GetRGBA())
+				child.backdrop.bottom:SetColorTexture(self.bbClr:GetRGBA())
+				child.backdrop.left:SetColorTexture(self.bbClr:GetRGBA())
+				child.backdrop.right:SetColorTexture(self.bbClr:GetRGBA())
 				child.backdrop.bg:SetTexture(nil)
 				aObj:adjHeight{obj=child, adj=4}
 			elseif child:IsObjectType("ScrollFrame") then
-				aObj:skinSlider{obj=child.ScrollBar}
+				aObj:skinObject("slider", {obj=child.ScrollBar})
 			elseif child:IsObjectType("Slider") then
-				aObj:skinSlider{obj=child}
+				aObj:skinObject("slider", {obj=child})
 			elseif child:IsObjectType("CheckButton") then
-				aObj:skinCheckButton{obj=child}
+				aObj:Debug("checkKids: [%s, %s]", child, child.menu)
+				if child.menu
+				and aObj.modBtnBs
+				then
+					aObj:addButtonBorder{obj=child.menu, ofs=-1, x1=0}
+				end
+				if aObj.modChkBtns then
+					aObj:skinCheckButton{obj=child}
+				end
 				checkKids(child)
 			elseif child:IsObjectType("Button") then
 				if child:GetName()
 				and not child:GetNormalTexture()
+				and aObj.modBtns
 				then
 					aObj:skinStdButton{obj=child}
+					aObj:SecureHook(child, "Disable", function(this, _)
+						aObj:clrBtnBdr(this)
+					end)
+					aObj:SecureHook(child, "Enable", function(this, _)
+						aObj:clrBtnBdr(this)
+					end)
 				else
 					checkKids(child)
 				end
 			elseif child:GetParent().list then -- filter list
 				local fl = child:GetParent().list
-				-- move list above buttons, done by moving the associated checkbox up instead
-				local relTo = select(2, fl:GetPoint(1))
-				if relTo:GetNumPoints() == 1 then
-					aObj:moveObject{obj=relTo, y=30}
-				end
-				aObj:skinSlider{obj=fl.scroll.ScrollBar}
+				aObj:skinObject("slider", {obj=fl.scroll.ScrollBar})
 				checkKids(fl)
-				fl, relTo = nil, nil
+				fl = nil
 				checkKids(child)
 			else checkKids(child)
 			end
 		end
-
 	end
-
-	-- hook this to skin the options frame
-	self:SecureHook(_G.WIM.options, "OnShow", function()
-		local optFrame = _G.WIM.options.frame
-		optFrame.title:SetPoint("TOPLEFT", 50 , -7)
-		optFrame.close:SetPoint("TOPRIGHT", -4, -4)
-		optFrame.nav.bg:Hide()
-		self:addSkinFrame{obj=optFrame, ft="a", nb=true, y2=8}
+	self:SecureHook(_G.WIM.options, "OnShow", function(this)
+		this.title:SetPoint("TOPLEFT", 50 , -7)
+		this.close:SetPoint("TOPRIGHT", -4, -4)
+		this.nav.bg:Hide()
+		self:skinObject("frame", {obj=this, y2=8})
 		-- if frame is a function then hook it, otherwise check it
 		for _, cat in _G.pairs(_G.WIM.options.frame.nav.category) do
 			for _, subCat in _G.pairs(cat.info.subCategories) do
@@ -180,64 +177,67 @@ aObj.otherAddons.WIM_Options = function(self)
 						self:Unhook(subCat, "frame")
 						return catFrame
 					end, true)
-				elseif subCat.frame then checkKids(subCat.frame) -- it is a frame
+				elseif subCat.frame then
+					checkKids(subCat.frame) -- it is a frame
 				end
 			end
 		end
-		self:Unhook(_G.WIM.options, "OnShow")
+
+		self:Unhook(this, "OnShow")
 	end)
 
-	-- hook this to skin the dropdown frame
 	self:SecureHook(_G.WIM.options, "createDropDownFrame", function()
-		self:addSkinFrame{obj=_G.WIM_DropDownFrame, ft="a"}
-		-- hook this to ensure the frame is above button borders
+		self:skinObject("frame", {obj=_G.WIM_DropDownFrame})
 		self:SecureHook(_G.WIM_DropDownFrame, "SetParent", function(this, parent)
 			_G.RaiseFrameLevelByTwo(this)
 		end)
+
 		self:Unhook(_G.WIM.options, "createDropDownFrame")
 	end)
 
-	-- hook this to skin the filter frame
 	if _G.WIM.modules["Filters"]
 	and _G.WIM.modules["Filters"].enabled
 	then
+		local function clrBD(obj)
+			for _, tex in _G.pairs(obj.backdrop) do
+				tex:SetTexture(nil)
+			end
+		end
 		self:SecureHook(_G.WIM, "ShowFilterFrame", function(this, ...)
-			if _G.WIM3_FilterFrame then
-				_G.WIM3_FilterFrame.title:SetPoint("TOPLEFT", 50 , -7)
-				_G.WIM3_FilterFrame.close:SetPoint("TOPRIGHT", -4, -4)
-				_G.WIM3_FilterFrame.name.backdrop.top:SetColorTexture(r, g, b, a)
-				_G.WIM3_FilterFrame.name.backdrop.bottom:SetColorTexture(r, g, b, a)
-				_G.WIM3_FilterFrame.name.backdrop.left:SetColorTexture(r, g, b, a)
-				_G.WIM3_FilterFrame.name.backdrop.right:SetColorTexture(r, g, b, a)
-				_G.WIM3_FilterFrame.name.backdrop.bg:SetTexture(nil)
-				self:skinDropDown{obj=_G.WIM3_FilterFrame.by, x2=109}
-				-- pattern box
-				self:skinSlider{obj=_G.WIM3_FilterFrame.patternContainer.ScrollBar, size=3}
-				_G.WIM3_FilterFrame.patternContainer.backdrop.top:SetColorTexture(r, g, b, a)
-				_G.WIM3_FilterFrame.patternContainer.backdrop.bottom:SetColorTexture(r, g, b, a)
-				_G.WIM3_FilterFrame.patternContainer.backdrop.left:SetColorTexture(r, g, b, a)
-				_G.WIM3_FilterFrame.patternContainer.backdrop.right:SetColorTexture(r, g, b, a)
-				_G.WIM3_FilterFrame.patternContainer.backdrop.bg:SetTexture(nil)
-				-- User options
-				self:keepFontStrings(_G.WIM3_FilterFrame.user)
-				self:skinCheckButton{obj=_G.WIM3_FilterFrame.user.friend}
-				self:skinCheckButton{obj=_G.WIM3_FilterFrame.user.guild}
-				self:skinCheckButton{obj=_G.WIM3_FilterFrame.user.party}
-				self:skinCheckButton{obj=_G.WIM3_FilterFrame.user.raid}
-				self:skinCheckButton{obj=_G.WIM3_FilterFrame.user.xrealm}
-				self:skinCheckButton{obj=_G.WIM3_FilterFrame.user.all}
-				-- level options
-				self:keepFontStrings(_G.WIM3_FilterFrame.level)
-				self:skinSlider{obj=_G.WIM3_FilterFrame.level.slider}
-				self:skinDropDown{obj=_G.WIM3_FilterFrame.level.class, x2=109}
-				self:skinCheckButton{obj=_G.WIM3_FilterFrame.received}
-				self:skinCheckButton{obj=_G.WIM3_FilterFrame.sent}
-				self:skinDropDown{obj=_G.WIM3_FilterFrame.action, x2=109}
-				self:skinCheckButton{obj=_G.WIM3_FilterFrame.actionNotify}
-				_G.WIM3_FilterFrame.border:Hide()
-				self:skinStdButton{obj=_G.WIM3_FilterFrame.cancel}
-				self:skinStdButton{obj=_G.WIM3_FilterFrame.save}
-				self:addSkinFrame{obj=_G.WIM3_FilterFrame, ft="a", nb=true}
+			local fFrame = _G.WIM3_FilterFrame
+			if fFrame then
+				fFrame.title:SetPoint("TOPLEFT", 50 , -7)
+				fFrame.close:SetPoint("TOPRIGHT", -4, -4)
+				clrBD(fFrame.name)
+				self:skinObject("editbox", {obj=fFrame.name})
+				self:skinObject("dropdown", {obj=fFrame.by, x2=109})
+				self:skinObject("slider", {obj=fFrame.patternContainer.ScrollBar})
+				clrBD(fFrame.patternContainer)
+				self:skinObject("frame", {obj=fFrame.patternContainer, fb=true, ofs=6})
+				clrBD(fFrame.user)
+				-- clrBD(fFrame.level)
+				-- self:skinObject("slider", {obj=fFrame.level.slider})
+				-- self:skinObject("dropdown", {obj=fFrame.level.class, x2=109})
+				self:skinObject("dropdown", {obj=fFrame.action, x2=109})
+				fFrame.border:SetTexture(nil)
+				self:skinObject("frame", {obj=fFrame})
+				if self.modBtns then
+					self:skinStdButton{obj=fFrame.cancel}
+					self:skinStdButton{obj=fFrame.save}
+				end
+				if self.modChkBtns then
+					self:skinCheckButton{obj=fFrame.user.friend}
+					self:skinCheckButton{obj=fFrame.user.guild}
+					self:skinCheckButton{obj=fFrame.user.party}
+					self:skinCheckButton{obj=fFrame.user.raid}
+					self:skinCheckButton{obj=fFrame.user.xrealm}
+					self:skinCheckButton{obj=fFrame.user.all}
+					self:skinCheckButton{obj=fFrame.received}
+					self:skinCheckButton{obj=fFrame.sent}
+					self:skinCheckButton{obj=fFrame.actionNotify}
+				end
+				fFrame = nil
+
 				self:Unhook(this, "ShowFilterFrame")
 			end
 		end)
--- AddonSkins/oRA3.lua
+++ AddonSkins/oRA3.lua
@@ -1,70 +1,78 @@
-local aName, aObj = ...
+local _, aObj = ...
 if not aObj:isAddonEnabled("oRA3") then return end
 local _G = _G
 
-aObj.addonsToSkin.oRA3 = function(self) -- v 8.2.2
+aObj.addonsToSkin.oRA3 = function(self) -- v 9.0.12
 
-	local oRA3 = _G.LibStub("AceAddon-3.0"):GetAddon("oRA3", true)
+	local oRA3 = _G.LibStub:GetLibrary("AceAddon-3.0"):GetAddon("oRA3", true)
 
-	local function skinFrame()
-		aObj:skinSlider{obj=_G.oRA3ScrollFrame.ScrollBar}
-		aObj:getChild(_G.oRA3ScrollFrameBottom, 1):SetAlpha(0)
-		aObj:getChild(_G.oRA3ScrollFrameTop, 1):SetAlpha(0)
-		-- Tabs
-		aObj:skinTabs{obj=_G.oRA3Frame, lod=true}
-		aObj:moveObject{obj=_G.oRA3FrameTab1, x=-2, y=3}
-		aObj:addSkinFrame{obj=_G.oRA3Frame, ft="a", kfs=true, nb=true, x1=10, y1=-11, x2=-33, y2=73}
-		if aObj.modBtns then
-			 aObj:skinCloseButton{obj=aObj:getChild(_G.oRA3Frame, 1)}
-			 aObj:skinStdButton{obj=_G.oRA3DisbandButton}
-			 aObj:skinStdButton{obj=_G.oRA3CheckButton}
+	self:SecureHook(oRA3, "ShowUIPanel", function(this, ...)
+		self:skinObject("slider", {obj=_G.oRA3ScrollFrame.ScrollBar})
+		self:getChild(_G.oRA3ScrollFrameTop, 1):SetAlpha(0) -- bar texture
+		self:getChild(_G.oRA3ScrollFrameBottom, 1):SetAlpha(0) -- bar texture
+		self:skinObject("tabs", {obj=_G.oRA3Frame, prefix=_G.oRA3Frame:GetName(), numTabs=5, lod=true})
+		self:moveObject{obj=_G.oRA3FrameTab1, x=-2, y=3}
+		self:skinObject("frame", {obj=_G.oRA3Frame, kfs=true, x1=10, y1=-11, x2=-33, y2=73})
+		if self.modBtns then
+			 self:skinCloseButton{obj=aObj:getChild(_G.oRA3Frame, 1)}
+			 self:skinStdButton{obj=_G.oRA3DisbandButton}
+			 self:SecureHook(_G.oRA3DisbandButton, "Disable", function(this, _)
+			 	self:clrBtnBdr(this)
+			 end)
+			 self:SecureHook(_G.oRA3DisbandButton, "Enable", function(this, _)
+			 	self:clrBtnBdr(this)
+			 end)
+			 self:skinStdButton{obj=_G.oRA3CheckButton}
+			 self:SecureHook(_G.oRA3CheckButton, "Disable", function(this, _)
+			 	self:clrBtnBdr(this)
+			 end)
+			 self:SecureHook(_G.oRA3CheckButton, "Enable", function(this, _)
+			 	self:clrBtnBdr(this)
+			 end)
 			 for i = 1, 5 do
 				 btn = _G["oRA3ListButton" .. i]
 				 if btn then
-					 aObj:skinStdButton{obj=btn}
+					 self:skinStdButton{obj=btn}
 				 end
 			 end
 		end
-	end
-
-	-- ScrollHeaders
-	self:SecureHook(oRA3, "CreateScrollEntry", function(this, header)
-		-- self:Debug("oRA3 CreateScrollEntry: [%s, %s]", this, header)
-		if not header.sf
-		and header:GetName()
-		then -- used by Tanks module as well
-			self:removeRegions(header, {1, 2, 3})
-			self:addSkinFrame{obj=header, ft="a", kfs=true, nb=true, x1=-2, x2=2}
-		end
-	end)
-
-	self:SecureHook(oRA3, "ShowUIPanel", function(this, ...)
-		skinFrame()
 
 		self:Unhook(oRA3, "ShowUIPanel")
 	end)
 
-	-- Tanks module
+	self:SecureHook(oRA3, "CreateScrollEntry", function(this, header)
+		if not header.sf
+		and header:GetName()
+		then -- used by Tanks module as well
+			self:removeRegions(header, {1, 2, 3})
+			self:skinObject("frame", {obj=header, kfs=true, x1=-2, x2=2})
+		end
+	end)
+
 	local tanks = oRA3:GetModule("Tanks", true)
 	if tanks then
 		self:SecureHook(tanks, "CreateFrame", function(this)
-			self:skinSlider{obj=_G.oRA3TankTopScrollFrame.ScrollBar}
-			self:skinSlider{obj=_G.oRA3TankBottomScrollFrame.ScrollBar}
+			self:getChild(_G.oRA3TankTopScrollFrame:GetParent(), 1):SetAlpha(0) -- centerBar texture
+			if self:getChild(_G.oRA3TankTopScrollFrame:GetParent(), 2):IsObjectType("Button") then
+				self:getChild(_G.oRA3TankTopScrollFrame:GetParent(), 2):SetAlpha(0) -- topBar texture
+			end
+			self:skinObject("slider", {obj=_G.oRA3TankTopScrollFrame.ScrollBar})
+			self:skinObject("slider", {obj=_G.oRA3TankBottomScrollFrame.ScrollBar})
 
 			self:Unhook(tanks, "CreateFrame")
 		end)
 		tanks = nil
 	end
 
-	-- ReadyCheck Frame
 	local rc = oRA3:GetModule("ReadyCheck", true)
 	if rc then
 		local function skinRCFrame()
-			aObj:addSkinFrame{obj=_G.oRA3ReadyCheck, ft="a", kfs=true, y1=-2, x2=-1}
+			aObj:skinObject("frame", {obj=_G.oRA3ReadyCheck, kfs=true, cb=true, y1=-2, x2=-1})
 		end
-		if _G.oRA3ReadyCheck then skinRCFrame()
+		if _G.oRA3ReadyCheck then
+			skinRCFrame()
 		else
-			self:SecureHook(rc, "READY_CHECK", function(this, ...)
+			self:SecureHook(rc, "READY_CHECK", function(this, _)
 				if _G.oRA3ReadyCheck then
 					skinRCFrame()
 
@@ -75,16 +83,11 @@ aObj.addonsToSkin.oRA3 = function(self) -- v 8.2.2
 		rc = nil
 	end
 
-	-- the version of ACD packed with oRA3 has a tooltip
-	local ACD = LibStub:GetLibrary("AceConfigDialog-3.0", true)
-	if ACD
-	and ACD.tooltip
-	then
-		-- tooltip
-		_G.C_Timer.After(0.1, function()
-			aObj:add2Table(aObj.ttList, ACD.tooltip)
-			ACD = nil
-		end)
+	local cooldowns = oRA3:GetModule("Cooldowns", true)
+	if cooldowns then
+		cooldowns.db.profile.displays.Default.barColor = {self.sbClr:GetRGBA()}
+		cooldowns.db.profile.displays.Default.barTexture = self.prdb.StatusBar.texture
+		cooldowns = nil
 	end
 
 end
