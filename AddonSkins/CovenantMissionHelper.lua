4fb1e2fd343d672f764adc681f3f0a5f47184823
Jncl <jncl@mac.com>
2021-02-25 10:34:05 +0000

    Updated CovenantMissionHelper skin

--- AddonSkins/CovenantMissionHelper.lua
+++ AddonSkins/CovenantMissionHelper.lua
@@ -4,19 +4,19 @@ local _G = _G
 
 aObj.addonsToSkin.CovenantMissionHelper = function(self) -- v 0.1
 
-	-- self:SecureHook(_G.MissionHelper, "createMissionHelperFrame", function(this, ...)
-	self:SecureHookScript(_G.MissionHelperFrame, "OnShow", function(this)
-		self:keepFontStrings(this.RaisedFrameEdges)
-		self:skinObject("frame", {obj=this.missionHeader, kfs=true, fb=true, clr="grey"})
-		this.resultHeader.BaseFrameBackground:SetTexture(nil)
-		self:keepFontStrings(this.resultInfo.RaisedFrameEdges)
-		self:skinObject("frame", {obj=this.resultInfo, kfs=true, fb=true, y2=-6, clr="grey"})
-		self:keepFontStrings(this.combatLogFrame.RaisedFrameEdges)
-		self:skinObject("frame", {obj=this.combatLogFrame, kfs=true, fb=true, y2=-6, clr="grey"})
-		self:skinObject("slider", {obj=this.combatLogFrame.CombatLogMessageFrame.ScrollBar, x1=-1, x2=0.5, y1=5, y2=-10})
-		this.buttonsFrame.bg:SetTexture(nil)
-		this.Tabs = {_G.MissionHelperTab1, _G.MissionHelperTab2}
-		self:skinObject("tabs", {obj=this, tabs=this.Tabs, lod=true, track=false})
+	self:SecureHook(_G.MissionHelper, "createMissionHelperFrame", function(this)
+		local mainFrame = _G.MissionHelperFrame
+		self:keepFontStrings(mainFrame.RaisedFrameEdges)
+		self:skinObject("frame", {obj=mainFrame.missionHeader, kfs=true, fb=true, clr="grey"})
+		mainFrame.resultHeader.BaseFrameBackground:SetTexture(nil)
+		self:keepFontStrings(mainFrame.resultInfo.RaisedFrameEdges)
+		self:skinObject("frame", {obj=mainFrame.resultInfo, kfs=true, fb=true, y2=-6, clr="grey"})
+		self:keepFontStrings(mainFrame.combatLogFrame.RaisedFrameEdges)
+		self:skinObject("frame", {obj=mainFrame.combatLogFrame, kfs=true, fb=true, y2=-6, clr="grey"})
+		self:skinObject("slider", {obj=mainFrame.combatLogFrame.CombatLogMessageFrame.ScrollBar, x1=-1, x2=0.5, y1=5, y2=-10})
+		mainFrame.buttonsFrame.bg:SetTexture(nil)
+		mainFrame.Tabs = {_G.MissionHelperTab1, _G.MissionHelperTab2}
+		self:skinObject("tabs", {obj=mainFrame, tabs=mainFrame.Tabs, lod=true, track=false})
 		if self.isTT then
 			self:SecureHook("MissionHelperFrame_SelectTab", function(tab)
 				for _, btn in _G.pairs(tab:GetParent().Tabs) do
@@ -28,25 +28,26 @@ aObj.addonsToSkin.CovenantMissionHelper = function(self) -- v 0.1
 				end
 			end)
 		end
-		self:skinObject("frame", {obj=this, kfs=true, y2=-6, clr="sepia"})
+		self:skinObject("frame", {obj=mainFrame, kfs=true, y2=-6, clr="sepia"})
 		if self.modBtns then
-			self:skinStdButton{obj=this.buttonsFrame.BestDispositionButton}
-			self:skinStdButton{obj=this.buttonsFrame.predictButton}
-			self:SecureHook(this.buttonsFrame.BestDispositionButton, "Disable", function(this, _)
+			self:skinStdButton{obj=mainFrame.buttonsFrame.BestDispositionButton}
+			self:skinStdButton{obj=mainFrame.buttonsFrame.predictButton}
+			self:SecureHook(mainFrame.buttonsFrame.BestDispositionButton, "Disable", function(this, _)
 				self:clrBtnBdr(this)
 			end)
-			self:SecureHook(this.buttonsFrame.BestDispositionButton, "Enable", function(this, _)
+			self:SecureHook(mainFrame.buttonsFrame.BestDispositionButton, "Enable", function(this, _)
 				self:clrBtnBdr(this)
 			end)
-			self:SecureHook(this.buttonsFrame.predictButton, "Disable", function(this, _)
+			self:SecureHook(mainFrame.buttonsFrame.predictButton, "Disable", function(this, _)
 				self:clrBtnBdr(this)
 			end)
-			self:SecureHook(this.buttonsFrame.predictButton, "Enable", function(this, _)
+			self:SecureHook(mainFrame.buttonsFrame.predictButton, "Enable", function(this, _)
 				self:clrBtnBdr(this)
 			end)
 		end
+		mainFrame = nil
 
-		self:Unhook(this, "OnShow")
+		self:Unhook(thmainFrameis, "OnShow")
 	end)
 
 	if self.modBtnBs then
