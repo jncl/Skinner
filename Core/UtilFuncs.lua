0062528319f49996f6ac51e9745d6af85f9c46c3
Jncl <jncl@mac.com>
2021-03-26 13:49:57 +0000

    Updated version numbers

--- Core/UtilFuncs.lua
+++ Core/UtilFuncs.lua
@@ -407,9 +407,9 @@ end
 local buildInfo = {
 	-- beta        = {"9.0.2", 36734},
 	classic_ptr = {"1.13.7", 37892},
-	retail_ptr  = {"9.0.5", 37899},
+	retail_ptr  = {"9.0.5", 37988},
 	classic     = {"1.13.6", 37497},
-	retail      = {"9.0.5", 37899},
+	retail      = {"9.0.5", 37988},
 	curr        = {_G.GetBuildInfo()},
 }
 function aObj:checkVersion()
