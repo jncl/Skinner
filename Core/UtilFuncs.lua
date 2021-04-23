0d986f71cc2986a05446669b460175232704524c
Jncl <jncl@mac.com>
2021-04-23 13:33:24 +0100

    Updated version numbers

--- Core/UtilFuncs.lua
+++ Core/UtilFuncs.lua
@@ -406,8 +406,8 @@ end
 
 local buildInfo = {
 	-- beta        = {"9.0.2", 36734},
-	classic_ptr = {"1.13.7", 37892},
-	retail_ptr  = {"9.0.5", 38134},
+	classic_ptr = {"1.13.7", 38363},
+	retail_ptr  = {"9.1.0", 38394},
 	classic     = {"1.13.6", 37497},
 	retail      = {"9.0.5", 38134},
 	curr        = {_G.GetBuildInfo()},
