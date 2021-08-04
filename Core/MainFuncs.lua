f6e5e846cefaed88ec0198da942bcf87d3e07e40
Jncl <jncl@mac.com>
2021-08-05 00:20:10 +0100

    Updated skinTooltip code

--- Core/MainFuncs.lua
+++ Core/MainFuncs.lua
@@ -1321,7 +1321,7 @@ function aObj:skinTooltip(tooltip)
 			ttSB = tooltip.SetBackdrop
 			tooltip.SetBackdrop = _G.nop
 		end
-		self:addSkinFrame{obj=tooltip, ft=tooltip.ftype or "a", kfs=true, nb=true, aso={ng=true}, ofs=tooltip.ofs or 0}
+		self:skinObject("frame", {obj=tooltip, fType=tooltip.ftype, kfs=true, ng=true, ofs=tooltip.ofs or 0})
 		if _G.IsAddOnLoaded("ElvUI") then
 			tooltip.SetBackdrop = ttSB
 		end
