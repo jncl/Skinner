8d6f8511e967d69ab59b433c54fa3bed90ec94dd
Jncl <jncl@mac.com>
2021-09-28 13:20:25 +0100

    Skinned EasyMail

---
+++ AddonSkins/EasyMail.lua
@@ -0,0 +1,39 @@
+local _, aObj = ...
+if not aObj:isAddonEnabled("EasyMail") then return end
+local _G = _G
+
+aObj.addonsToSkin.EasyMail = function(self) -- v 4.4.6.b
+
+	self:getRegion(_G.EASYMAIL_L_Sowframe_HistoryFrame, 10):SetTextColor(self.HT:GetRGB())
+	self:skinObject("slider", {obj=self:getChild(_G.EASYMAIL_L_Sowframe_HistoryFrame, 2).ScrollBar})
+	self:skinObject("frame", {obj=_G.EASYMAIL_L_Sowframe_HistoryFrame, kfs=true, ofs=-8})
+	if self.modBtns then
+		self:skinCloseButton{obj=self:getChild(_G.EASYMAIL_L_Sowframe_HistoryFrame, 1)}
+	end
+	if self.modBtnBs then
+		self:addButtonBorder{obj=_G.EasyMail_CheckAllButton}
+		self:addButtonBorder{obj=_G.EasyMail_ClearAllButton}
+		self:addButtonBorder{obj=_G.EasyMail_CheckPageButton}
+		self:addButtonBorder{obj=_G.EasyMail_ClearPageButton}
+		self:addButtonBorder{obj=_G.EasyMail_GetAllButton}
+		self:SecureHook(_G.EasyMail, "InboxUpdate", function(_)
+			self:clrBtnBdr(_G.EasyMail_CheckAllButton)
+			self:clrBtnBdr(_G.EasyMail_ClearAllButton)
+			self:clrBtnBdr(_G.EasyMail_CheckPageButton)
+			self:clrBtnBdr(_G.EasyMail_ClearPageButton)
+		end)
+		-- hook this to colour buttons on update
+		hooksecurefunc("InboxFrame_Update", function()
+			_G.EasyMail.InboxUpdate()
+		end)
+		self:SecureHook(_G.EasyMail, "EnableGetButton", function(_)
+			self:clrBtnBdr(_G.EasyMail_GetAllButton)
+		end)
+	end
+	if self.modChkBtns then
+		for i = 1, _G.INBOXITEMS_TO_DISPLAY do
+			self:skinCheckButton{obj=_G["EasyMail_CheckButton" .. i]}
+		end
+	end
+
+end
