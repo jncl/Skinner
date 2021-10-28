34ab8d3384695dfffbd475d2333316de04561539
Jncl <jncl@mac.com>
2021-10-28 17:40:37 +0100

    Updated version numbers

--- Core/SharedFuncs.lua
+++ Core/SharedFuncs.lua
@@ -13,16 +13,16 @@ function aObj:add2Table(table, value) -- luacheck: ignore self
 end
 
 local buildInfo = {
-	-- Development
+	-- Testing
 	wow_classic_beta    = {"3.0.0",  99999, "Classic BC Beta"},
 	wow_classic_ptr     = {"2.5.2",  40617, "Classic BC PTR"},
 	wow_classic_era_ptr = {"1.14.1", 40688, "Classic Era PTR"}, -- a.k.a. Season of Mastery Beta
 	wow_beta            = {"10.0.0", 99999, "Retail Beta"},
-	wow_ptr             = {"9.1.5",  40696, "Retail PTR"},
+	wow_ptr             = {"9.1.5",  40772, "Retail PTR"},
 	-- Live
 	wow_classic         = {"2.5.2",  40617, "Classic BC"},
 	wow_classic_era     = {"1.14.0", 40618, "Classic Era"},
-	wow                 = {"9.1.0",  40593, "Retail"},
+	wow                 = {"9.1.0",  40725, "Retail"},
 	-- Currently playing
 	curr                = {_G.GetBuildInfo()},
 }
