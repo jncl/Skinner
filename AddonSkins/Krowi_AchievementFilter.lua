a1ad42033914306effc946b38e8e99550ae56d67
Jncl <jncl.wow@gmail.com>
2022-09-24 15:28:45 +0100

    Skinned Krowi_AchievementFilter skin

---
+++ AddonSkins/Krowi_AchievementFilter.lua
@@ -0,0 +1,8 @@
+local _, aObj = ...
+if not aObj:isAddonEnabled("Krowi_AchievementFilter") then return end
+local _G = _G
+
+aObj.addonsToSkin.Krowi_AchievementFilter = function(self) -- v 42.2
+
+
+end
