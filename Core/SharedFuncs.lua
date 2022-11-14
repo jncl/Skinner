281e5c7070a9aca863be6148b322bde685515679
Jncl <jncl.wow@gmail.com>
2022-11-14 10:28:03 +0000

    Updated version numbers

--- Core/SharedFuncs.lua
+++ Core/SharedFuncs.lua
@@ -16,13 +16,13 @@ local buildInfo = {
 	-- Testing
 	wow_classic_beta    = {"3.4.0",  46158, "Classic Beta"},
 	wow_classic_ptr     = {"3.4.0",  46368, "Classic PTR"},
-	wow_classic_era_ptr = {"1.14.3", 45437, "Classic Era PTR"}, -- a.k.a. Season of Mastery PTR
-	wow_beta            = {"10.0.2", 46479, "Retail Beta"}, -- a.k.a. Dragonflight
+	wow_classic_era_ptr = {"1.14.3", 46575, "Classic Era PTR"}, -- a.k.a. Season of Mastery PTR
+	wow_beta            = {"10.0.2", 46619, "Retail Beta"}, -- a.k.a. Dragonflight
 	wow_ptr             = {"10.0.2", 46479, "Retail PTR"},
 	-- Live
 	wow_classic         = {"3.4.0",  46368, "Classic"}, -- a.k.a. Wrath of the Lich King Classic
-	wow_classic_era     = {"1.14.3", 45437, "Classic Era"},
-	wow                 = {"10.0.0", 46455, "Retail"},
+	wow_classic_era     = {"1.14.3", 46575, "Classic Era"},
+	wow                 = {"10.0.0", 46597, "Retail"},
 	-- Currently playing
 	curr                = {_G.GetBuildInfo()},
 }
