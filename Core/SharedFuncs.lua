145f0e6186c8acb47fbc3889826997dc5657828e
Jncl <jncl.wow@gmail.com>
2022-11-19 13:33:27 +0000

    Updated version numbers

--- Core/SharedFuncs.lua
+++ Core/SharedFuncs.lua
@@ -15,7 +15,7 @@ end
 local buildInfo = {
 	-- Testing
 	wow_classic_beta    = {"3.4.0",  46158, "Classic Beta"},
-	wow_classic_ptr     = {"3.4.0",  46368, "Classic PTR"},
+	wow_classic_ptr     = {"3.4.1",  46722, "Classic PTR"},
 	wow_classic_era_ptr = {"1.14.3", 46575, "Classic Era PTR"}, -- a.k.a. Season of Mastery PTR
 	wow_beta            = {"10.0.2", 46702, "Retail Beta"}, -- a.k.a. Dragonflight
 	wow_ptr             = {"10.0.2", 46702, "Retail PTR"},
