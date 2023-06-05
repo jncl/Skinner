cd85f6392a9d6d65468599ad715bc8b07bce3f89
Jon Hogg <hogg.jon+github@gmail.com>
2023-06-05 19:09:14 +0100

    Updated RaiderIO skin

--- AddonSkins/RaiderIO.lua
+++ AddonSkins/RaiderIO.lua
@@ -1,11 +1,12 @@
 local _, aObj = ...
 if not aObj:isAddonEnabled("RaiderIO") then return end
 local _G = _G
+-- luacheck: ignore 631 (line is too long)
 
-aObj.addonsToSkin.RaiderIO = function(self) -- v 9.2.0 (v202205060600)
+aObj.addonsToSkin.RaiderIO = function(self) -- v 10.1.0 (v202306050600)
 
 	-- Config & SearchUI
-	local cPF, sUI -- configParentFrame, SearchUI
+	local cPF, sUI, sT, pT, sB -- configParentFrame, SearchUI, searchTooltip, profileToooltip
 	self.RegisterCallback("RaiderIO", "UIParent_GetChildren", function(_, child)
 		if child.scrollframe
 		and child.scrollbar
@@ -16,8 +17,20 @@ aObj.addonsToSkin.RaiderIO = function(self) -- v 9.2.0 (v202205060600)
 		then
 			sUI = child
 		end
+		if _G.RaiderIO_SearchTooltip then
+			sT = _G.RaiderIO_SearchTooltip
+		end
+		if _G.RaiderIO_ProfileTooltip then
+			pT =_G.RaiderIO_ProfileTooltip
+		end
+		if _G.RaiderIO_SettingsPanelButton then
+			sB = _G.RaiderIO_SettingsPanelButton
+		end
 		if cPF
 		and sUI
+		and sT
+		and pT
+		and sB
 		then
 			self.UnregisterCallback("RaiderIO", "UIParent_GetChildren")
 		end
@@ -33,7 +46,7 @@ aObj.addonsToSkin.RaiderIO = function(self) -- v 9.2.0 (v202205060600)
 			local function skinBtn(id)
 				local btn = aObj:getChild(aObj:getChild(cPF, 3), id)
 				if btn then
-					aObj:addSkinButton{obj=btn, parent=btn, hook=btn, hide=true, ofs=0}
+					aObj:skinStdButton{obj=btn}
 					btn.sb:SetBackdropBorderColor(0.5, 0.5, 0.5, 1)
 					btn:SetScript("OnEnter", function(this) this.sb:SetBackdropBorderColor(aObj.bbClr:GetRGBA()) end)
 					btn:SetScript("OnLeave",  function(this) this.sb:SetBackdropBorderColor(0.5, 0.5, 0.5, 1) end)
@@ -42,6 +55,9 @@ aObj.addonsToSkin.RaiderIO = function(self) -- v 9.2.0 (v202205060600)
 			if self:getChild(cPF, 3) then
 				skinBtn(2)
 				skinBtn(3)
+			end
+			if sB then
+				self:skinStdButton{obj=sB}
 			end
 		end
 		if self.modChkBtns then
@@ -60,10 +76,14 @@ aObj.addonsToSkin.RaiderIO = function(self) -- v 9.2.0 (v202205060600)
 		aObj:skinObject("frame", {obj=sUI, kfs=true, cb=true, ofs=0})
 	end
 
-	-- tooltip
-	_G.C_Timer.After(0.1, function()
-		self:add2Table(self.ttList, _G.RaiderIO_SearchTooltip)
-		self:add2Table(self.ttList, _G.RaiderIO_ProfileTooltip)
-	end)
+	if sT
+	and pT
+	then
+		-- tooltip
+		_G.C_Timer.After(0.1, function()
+			self:add2Table(self.ttList, sT)
+			self:add2Table(self.ttList, pT)
+		end)
+	end
 
 end
