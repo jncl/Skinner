a5207aad096fc7c452fc6bc14c177b2530f54dfc
Jon Hogg <hogg.jon+github@gmail.com>
2023-06-22 17:35:33 +0100

    Updated SavedInstances skin fixes #7

--- AddonSkins/SavedInstances.lua
+++ AddonSkins/SavedInstances.lua
@@ -10,14 +10,38 @@ if SI then
 	end, true)
 end
 
-aObj.addonsToSkin.SavedInstances = function(self) -- v 9.0.3
+aObj.addonsToSkin.SavedInstances = function(self) -- v 10.1.2-10-gb6df957
 
 	if SI then
-		self:SecureHook(SI, "ShowDetached", function(this)
-			self:skinObject("frame", {obj=this.detachframe, kfs=true, cb=true})
+		self:add2Table(self.ttList, SI.ScanTooltip)
+		if SI.ShowDetached then
+			self:SecureHook(SI, "ShowDetached", function(this)
+				self:skinObject("frame", {obj=this.detachframe, kfs=true, cb=true})
 
-			self:Unhook(this, "ShowDetached")
-		end)
+				self:Unhook(this, "ShowDetached")
+			end)
+		else
+			local tt = SI:GetModule("Tooltip", true)
+			if tt then
+				self:SecureHook(tt, "ShowDetached", function(this)
+					self:skinObject("frame", {obj=_G.SavedInstancesDetachHeader, kfs=true, cb=true})
+
+					self:Unhook(this, "ShowDetached")
+				end)
+			end
+		end
+		local mpm = SI:GetModule("MythicPlus", true)
+		if mpm then
+			self:SecureHook(mpm, "ExportKeys", function(this, _)
+				self:skinObject("slider", {obj=self:getChild(this.KeyExportWindow, 2).ScrollBar})
+				self:skinObject("frame", {obj=this.KeyExportWindow, kfs=true, rb=true, ofs=0})
+				if self.modBtns then
+					self:skinStdButton{obj=self:getChild(this.KeyExportWindow, 1)}
+				end
+
+				self:Unhook(this, "ExportKeys")
+			end)
+		end
 	end
 
 end
