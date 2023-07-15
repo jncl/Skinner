600dbb3a625cd9f11693ed4239f8220f75ef3ffc
Jon Hogg <hogg.jon+github@gmail.com>
2023-07-15 15:57:58 +0100

    Updated version numbers

--- Core/SharedFuncs.lua
+++ Core/SharedFuncs.lua
@@ -7,15 +7,15 @@ local buildInfo = {
 	-- Testing
 	-- wow_classic_beta    = {"3.4.0",  46158, "Classic Beta"},
 	-- wow_beta            = {"10.0.2", 47120, "Retail Beta"}, -- a.k.a. Dragonflight
-	wow_classic_ptr     = {"3.4.2",  50250, "Classic PTR"},
+	wow_classic_ptr     = {"3.4.2",  50375, "Classic PTR"},
 	-- wow_classic_era_ptr = {"1.14.3", 49229, "Classic Era PTR"}, -- a.k.a. Season of Mastery PTR
-	-- wow_classic_era_ptr = {"10.1.5", 49595, "Retail PTR2"}, -- a.k.a. PTR DF 10.1.5
-	wow_ptr_x           = {"10.1.5", 50355, "Retail PTRX"}, -- a.k.a. PTR DF 10.1.5 [wowxptr]
-	wow_ptr             = {"10.1.0", 49570, "Retail PTR"}, -- a.k.a. [wowt]
+	-- wow_classic_era_ptr = {"10.1.5", 49595, "Retail PTR2"},
+	wow_ptr_x           = {"10.1.5", 50469, "Retail PTRX"}, -- [wowxptr]
+	wow_ptr             = {"10.1.7", 50442, "Retail PTR"},
 	-- Live
-	wow_classic         = {"3.4.2",  50250, "Classic"}, -- a.k.a. Wrath of the Lich King Classic
+	wow_classic         = {"3.4.2",  50375, "Classic"}, -- a.k.a. Wrath of the Lich King Classic
 	wow_classic_era     = {"1.14.3", 49821, "Classic Era"},
-	wow                 = {"10.1.0", 50000, "Retail"},
+	wow                 = {"10.1.5", 50469, "Retail"},
 	-- Currently playing
 	curr                = {_G.GetBuildInfo()},
 }
