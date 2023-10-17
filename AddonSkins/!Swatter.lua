03213882462428a7c9504e34e866507f5e2fd395
Jon Hogg <hogg.jon+github@gmail.com>
2023-10-17 10:48:17 +0100

    Unobsoleted Swatter, Obsoleted LibExtraTip

---
+++ AddonSkins/!Swatter.lua
@@ -0,0 +1,20 @@
+local _, aObj = ...
+if not aObj:isAddonEnabled("!Swatter") then return end
+local _G = _G
+
+aObj.addonsToSkin["!Swatter"] = function(self) -- v 3.4.6975
+
+	-- handle the fact that the AddOn was loaded but, due to it checking for another debugging aid, didn't initialise
+	if _G.Swatter
+	and _G.Swatter.Version
+	then
+		self:skinObject("slider", {obj=_G.Swatter.Error.Scroll.ScrollBar})
+		self:skinObject("frame", {obj=_G.Swatter.Error})
+		if self.modBtns then
+			self:skinStdButton{obj=_G.Swatter.Error.Done}
+			self:skinStdButton{obj=_G.Swatter.Error.Next}
+			self:skinStdButton{obj=_G.Swatter.Error.Prev}
+		end
+	end
+
+end
