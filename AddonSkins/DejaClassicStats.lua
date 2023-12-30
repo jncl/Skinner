b38b0980855e0585c16b10462da8ad5a711a7d3d
Jon Hogg <hogg.jon+github@gmail.com>
2023-12-30 16:25:13 +0000

    Updated DejaCharacterStats & ExtendedCharacterStats skins

--- AddonSkins/DejaClassicStats.lua
+++ AddonSkins/DejaClassicStats.lua
@@ -2,7 +2,7 @@ local _, aObj = ...
 if not aObj:isAddonEnabled("DejaClassicStats") then return end
 local _G = _G
 
-aObj.addonsToSkin.DejaClassicStats = function(self) -- v 1307r005/205r013
+aObj.addonsToSkin.DejaClassicStats = function(self) -- v 11500r6
 
 	self:SecureHookScript(_G.DCS_StatScrollFrame, "OnShow", function(this)
 		self:skinObject("slider", {obj=this.ScrollBar})
@@ -10,15 +10,32 @@ aObj.addonsToSkin.DejaClassicStats = function(self) -- v 1307r005/205r013
 		_G.DCSMeleeEnhancementsStatsHeader:DisableDrawLayer("ARTWORK")
 		_G.DCSSpellEnhancementsStatsHeader:DisableDrawLayer("ARTWORK")
 		_G.DCSDefenseStatsHeader:DisableDrawLayer("ARTWORK")
-		if self.isClsc then
-			_G.DCSRangedStatsHeader:DisableDrawLayer("ARTWORK")
-		end
+		_G.DCSRangedStatsHeader:DisableDrawLayer("ARTWORK")
 		self:skinObject("frame", {obj=this, kfs=true, x1=-2, y1=14, x2=1, y2=-12})
 		if self.modBtnBs then
 			self:addButtonBorder{obj=_G.PaperDollFrame.ExpandButton, ofs=-3, x1=2, clr="gold"}
 		end
 
 		self:Unhook(this, "OnShow")
+	end)
+
+	self.RegisterCallback("DejaClassicStats", "IOFPanel_Before_Skinning", function(_, panel)
+		if panel.name ~= "DejaClassicStats" then return end
+		self.iofSkinnedPanels[panel] = true
+
+		for _, child in _G.ipairs_reverse{panel:GetChildren()} do
+			if child:IsObjectType("CheckButton")
+			and self.modChkBtns
+			then
+				self:skinCheckButton{obj=child}
+			elseif child:IsObjectType("Button")
+			and self.modBtns
+			then
+				self:skinStdButton{obj=child}
+			end
+		end
+
+		self.UnregisterCallback("DejaClassicStats", "IOFPanel_Before_Skinning")
 	end)
 
 end
--- AddonSkins/ExtendedCharacterStats.lua
+++ AddonSkins/ExtendedCharacterStats.lua
@@ -2,18 +2,23 @@ local _, aObj = ...
 if not aObj:isAddonEnabled("ExtendedCharacterStats") then return end
 local _G = _G
 
-aObj.addonsToSkin.ExtendedCharacterStats = function(self) -- v 2.5.0
+aObj.addonsToSkin.ExtendedCharacterStats = function(self) -- v 3.2.1
 	if not self.prdb.CharacterFrames then return end
 
 	self:SecureHookScript(_G.ECS_StatsFrame, "OnShow", function(this)
-
-		self:skinSlider{obj=this.ScrollFrame.ScrollBar}
-		self:addSkinFrame{obj=_G.ECS_StatsFrame, ft="a", kfs=true, x2=1, y2=-5}
+		self:skinObject("slider", {obj=this.ScrollFrame.ScrollBar})
+		self:skinObject("frame", {obj=this, kfs=true, ri=true, cb=true})
 		if self.modBtns then
 			self:skinStdButton{obj=this.configButton}
 			self:skinStdButton{obj=_G.ECS_ToggleButton}
 		end
 		self:moveObject{obj=this, x=-2, y=1}
+
+		self:Unhook(this, "OnShow")
+	end)
+
+	self:SecureHookScript(_G.ECSConfigFrame, "OnShow", function(this)
+		self:skinAceOptions(this)
 
 		self:Unhook(this, "OnShow")
 	end)
