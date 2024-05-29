7a12697b798fa411b6eed4fb6949d2fccd37fd6d
Jon Hogg <hogg.jon+github@gmail.com>
2024-05-29 14:08:15 +0100

    Updated UtilFuncs

--- Core/UtilFuncs.lua
+++ Core/UtilFuncs.lua
@@ -468,13 +468,13 @@ function aObj:checkDisabledDD(obj, disabled)
 
 	disabled = disabled or obj.isDisabled
 	if obj.sf then
-		self:clrBBC(obj.sf, disabled and "grey")
+		self:clrBBC(obj.sf, disabled and "disabled")
 		if self.modBtnBs then
 			local btn = obj.Button and obj.Button.sbb
 					 or obj.dropButton and obj.dropButton.sbb
 					 or obj:GetName() and _G[obj:GetName() .. "Button"] and _G[obj:GetName() .. "Button"].sbb
 			if btn then
-				self:clrBtnBdr(btn, disabled and "grey")
+				self:clrBtnBdr(btn, disabled and "disabled")
 			end
 		end
 	end
