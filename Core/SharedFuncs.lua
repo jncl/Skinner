ebdb9c0e56163b3410f5d03a4a119bdb7752ecca
Jon Hogg <hogg.jon+github@gmail.com>
2024-08-20 22:27:27 +0100

    Updated version numbers

--- Core/SharedFuncs.lua
+++ Core/SharedFuncs.lua
@@ -9,8 +9,8 @@ local buildInfo = {
 	wow_classic_era_ptr = {"1.15.3", 55917, "Classic Era PTR"},
 	wow_classic_ptr     = {"4.4.0",  55639, "Classic PTR"},
 	wow_beta            = {"11.0.2", 56110, "Retail Beta"}, -- a.k.a. The War Within
-	wow_ptr             = {"11.0.2", 56044, "Retail PTR"},
-	wow                 = {"11.0.2", 56110, "Retail"},
+	wow_ptr             = {"11.0.2", 56162, "Retail PTR"},
+	wow                 = {"11.0.2", 56162, "Retail"},
 	wow_ptr_x           = {"11.0.0", 55960, "Retail PTRX"},
 	curr                = {_G.GetBuildInfo()},
 }
@@ -137,6 +137,7 @@ function aObj:add2Table(tab, val)
 	tab[#tab + 1] = val
 
 end
+
 function aObj:setupOptions(optNames, optIgnore, preLoadFunc, postLoadFunc)
 	local _
 	local db = self.db.profile
