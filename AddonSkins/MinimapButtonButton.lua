4eddb538386ff1fb177e721f2144e9df7ed306b2
Jon Hogg <hogg.jon+github@gmail.com>
2025-04-29 10:21:26 +0100

    Updated MinimapButtonButton skin

--- AddonSkins/MinimapButtonButton.lua
+++ AddonSkins/MinimapButtonButton.lua
@@ -6,7 +6,8 @@ aObj.addonsToSkin.MinimapButtonButton = function(self) -- v 1.21.3
 
 	local buttonContainer = self:getChild(_G.MinimapButtonButtonButton, 1)
 
-	self:skinObject("frame", {obj=_G.MinimapButtonButtonButton, rb="nop", ofs=0, ng=true, bclr="class", ba=0.5})
+	self:skinObject("frame", {obj=_G.MinimapButtonButtonButton, rb="nop", ofs=0, ng=true, ba=0.5})
+	_G.MinimapButtonButtonButton.sf:SetBackdropColor(_G.RAID_CLASS_COLORS[aObj.uCls]:GetRGBA())
 
 	self:SecureHookScript(buttonContainer, "OnShow", function(this)
 
