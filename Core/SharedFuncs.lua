179e4caa9743a47134a3c7cc82a7f9021f489283
Jon Hogg <hogg.jon+github@gmail.com>
2025-06-23 16:28:25 +0100

    Updated Version numbers

--- Core/SharedFuncs.lua
+++ Core/SharedFuncs.lua
@@ -5,13 +5,13 @@ local _G = _G
 local buildInfo = {
 	wow_classic_beta    = {"MOP Classic Beta",          "5.5.0",  61496},
 	wow_beta            = {"The War Within Beta",       "11.0.2", 56313},
-	wow_classic_ptr     = {"PTR (Cataclysm Classic)",   "4.4.2",  59185},
+	wow_classic_ptr     = {"PTR (Cataclysm Classic)",   "5.5.0",  61496},
 	wow_classic_era_ptr = {"PTR (Classic Era)",         "1.15.7", 60141},
 	wow_ptr             = {"PTR (TWW 11.1.7)",          "11.1.7", 61491},
-	wow_ptr_x           = {"PTR (TWW 11.1.0)",          "11.1.0", 59347},
+	wow_ptr_x           = {"PTR (TWW 11.2.0)",          "11.2.0", 61476},
 	wow_classic_era     = {"World of Warcraft Classic", "1.15.7", 61257},
 	wow_classic         = {"Cataclysm Classic",         "4.4.2",  60895},
-	wow                 = {"World of Warcraft",         "11.1.7", 61491},
+	wow                 = {"World of Warcraft",         "11.1.7", 61559},
 	curr                = {"curr", _G.GetBuildInfo()},
 }
 
