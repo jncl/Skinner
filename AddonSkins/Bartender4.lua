4fc1ea7ea505666de628f57ddaef645712eee11d
Jon Hogg <hogg.jon+github@gmail.com>
2025-08-03 15:12:31 +0100

    Updated Bartender4 skin, closes #228

--- AddonSkins/Bartender4.lua
+++ AddonSkins/Bartender4.lua
@@ -2,7 +2,7 @@ local _, aObj = ...
 if not aObj:isAddonEnabled("Bartender4") then return end
 local _G = _G
 
-aObj.addonsToSkin.Bartender4 = function(self) -- v 4.15.0
+aObj.addonsToSkin.Bartender4 = function(self) -- v 4.16.0
 
 	self:SecureHook(_G.Bartender4, "ShowUnlockDialog", function(this)
 		self:skinObject("frame", {obj=this.unlock_dialog, kfs=true, y1=6})
@@ -136,6 +136,42 @@ aObj.addonsToSkin.Bartender4 = function(self) -- v 4.15.0
 
 				self:Unhook(this, "OnEnable")
 			end)
+		end
+	end
+
+	if not self.isMnln then
+		if self.modBtnBs then
+			mod = _G.Bartender4:GetModule("BagBar", true)
+			if mod then
+				local function abb2Bag(bag)
+					aObj:addButtonBorder{obj=bag, ibt=true, ofs=3, clr=bag.icon:GetVertexColor()}
+				end
+				abb2Bag(_G.MainMenuBarBackpackButton)
+				for i = 0, 3 do
+					abb2Bag(_G["CharacterBag" .. i .. "Slot"])
+				end
+				self:addButtonBorder{obj=_G.KeyRingButton, ofs=2}
+			end
+			mod = _G.Bartender4:GetModule("MicroMenu", true)
+			if mod then
+				for _, bName in _G.pairs(_G.MICRO_BUTTONS) do
+					self:addButtonBorder{obj=_G[bName], es=24, ofs=2, y1=-18, reParent={_G[bName].QuickKeybindHighlightTexture}}
+				end
+			end
+		end
+		mod = _G.Bartender4:GetModule("XPBar", true)
+		if mod then
+			mod.bar.content:DisableDrawLayer("OVERLAY")
+			self:skinObject("statusbar", {obj=mod.bar.content, bg=self:getRegion(mod.bar.content, 6), other={_G.ExhaustionLevelFillBar}})
+		end
+		mod = _G.Bartender4:GetModule("RepBar", true)
+		if mod then
+			local rwbSB = mod.bar.content.StatusBar
+			for i = 0, 3 do
+				rwbSB["WatchBarTexture" .. i]:SetTexture(nil)
+				rwbSB["XPBarTexture" .. i]:SetTexture(nil)
+			end
+			self:skinObject("statusbar", {obj=rwbSB, bg=rwbSB.Background, other={rwbSB.Underlay, rwbSB.Overlay}})
 		end
 	end
 
