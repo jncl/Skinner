651cb5ad6f7ba56d234ad1b3984f00a8f41470c3
Jon Hogg <hogg.jon+github@gmail.com>
2025-08-05 20:30:14 +0100

    Updated Classic Support to include FocusFrame skinning for MoP Classic

--- Classic/Support.lua
+++ Classic/Support.lua
@@ -9,16 +9,21 @@ aObj.SetupClassic = function(self)
 	-- remove UnitFrames module options
 	local ufDB = self.db:GetNamespace("UnitFrames", true)
 	if ufDB then
-		ufDB.profile.focus = nil
+		if self.isClscERA then
+			ufDB.profile.focus = nil
+			ufDB.defaults.profile.focus = nil
+			self:GetModule("UnitFrames", true).skinFocusF = _G.nop
+			self.optTables["Modules"].args[aName .. "_UnitFrames"].args.focus = nil
+		end
 		ufDB.profile.arena = nil
-		ufDB.defaults.profile.focus = nil
 		ufDB.defaults.profile.arena = nil
 	end
-	self.optTables["Modules"].args[aName .. "_UnitFrames"].args.focus = nil
 	self.optTables["Modules"].args[aName .. "_UnitFrames"].args.arena = nil
-	self:GetModule("UnitFrames", true).skinFocusF = _G.nop
 
-	-- NOP functions that are not required and cause errors
+	--[===[@non-debug@
+	-- NOP functions that are not required and cause errors in MainFuncs.lua
 	self.skinGlowBox = _G.nop
+	--@end-non-debug@]===]
 
 end
+
