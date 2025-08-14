e9206e791f96e325fcdf46592c0175912e5d614a
Jon Hogg <hogg.jon+github@gmail.com>
2025-08-14 11:59:59 +0100

    Updated Bagnon, closes #233

--- AddonSkins/Bagnon.lua
+++ AddonSkins/Bagnon.lua
@@ -2,163 +2,142 @@ local _, aObj = ...
 if not aObj:isAddonEnabled("Bagnon") then return end
 local _G = _G
 
-aObj.addonsToSkin.Bagnon = function(self) -- v 10.2.17
+aObj.addonsToSkin.Bagnon = function(self) -- v 11.2.2
 	if not self.db.profile.ContainerFrames or self.initialized.Bagnon then return end
 	self.initialized.Bagnon = true
 
-	local function skinBagGrp(frame)
-		for _, child in _G.ipairs{frame.bagGroup:GetChildren()} do
-			if child:IsObjectType("CheckButton") then
-				aObj:addButtonBorder{obj=child, ofs=3, clr="grey"}
-				if child.info
-				and child.info.count == 0 -- no bag equipped
-				then
-					_G.SetItemButtonTexture(child, nil)
-				end
+	local skinMenuBtns, skinBGBtns, skinIGBtns, skinTGBtns =_G.nop, _G.nop, _G.nop, _G.nop
+	if aObj.modBtnBs then
+		function skinMenuBtns(frame)
+			for _, btn in _G.pairs(frame.MenuButtons) do
+				aObj:addButtonBorder{obj=btn, ofs=3, clr="grey"}
+			end
+			-- Options button
+			if frame:HasOptionsToggle() then
+				aObj:addButtonBorder{obj=frame.OptionsToggle or frame.optionsToggle, ofs=3, clr="grey"}
 			end
 		end
-		aObj:SecureHook(frame.bagGroup, "Show", function(fObj)
-			for _, child in _G.ipairs{fObj:GetChildren()} do
-				if child:IsObjectType("CheckButton") then
-					if child.info
-					and child.info.count == 0 -- no bag equipped
-					then
-						_G.SetItemButtonTexture(child, nil)
-					end
-				end
-			end
-		end)
-	end
-	local function updBtn(btn)
-		aObj:setBtnClr(btn, btn.info.quality)
-		local bIT = btn:GetItemButtonIconTexture():GetTexture()
-		-- aObj:Debug("updBtn: [%s, %s, %s]", btn.info.quality, bIT)
-		if bIT == 136509 -- ui-backpack-emptyslot
-		or bIT == 4701874 -- bagitemslot2x
-		then
-			btn:SetItemButtonTexture("")
+		function skinBGBtns(frame)
+		    for bBtn, _ in _G.pairs(frame.__frames) do
+		    	aObj:addButtonBorder{obj=bBtn, ofs=4, clr=bBtn.owned and "grey" or "red"}
+		    	aObj:secureHook(bBtn, "Update", function(bObj)
+					aObj:clrBBC(bObj.sbb, bObj.owned and "grey" or "red")
+		    	end)
+		    end
 		end
-	end
-	-- skin the bag frames
-	local function skinFrame(frame, _)
-		if frame.skinned then return end
-		aObj:SecureHookScript(frame, "OnShow", function(this)
-			aObj:skinObject("editbox", {obj=this.SearchFrame})
-			aObj:skinObject("frame", {obj=this.bg, kfs=true, ofs=1})
-			if aObj.modBtns then
-				aObj:skinCloseButton{obj=this.CloseButton}
-			end
-			if aObj.modBtnBs then
-				for _, btn in _G.pairs(this.MenuButtons) do
-					aObj:addButtonBorder{obj=btn, ofs=3, clr="grey"}
-				end
-				-- Options button
-				if this:HasOptionsToggle() then
-					aObj:addButtonBorder{obj=this.OptionsToggle or this.optionsToggle, ofs=3, clr="grey"}
-				end
-				-- Broker button
-				if this.HasBrokerCarrousel
-				and this:HasBrokerCarrousel()
-				then
-					aObj:addButtonBorder{obj=this.Broker, relTo=this.Broker.Icon, ofs=3, clr="grey"}
-				elseif this.HasBrokerDisplay
-				and this:HasBrokerDisplay()
-				then
-					aObj:addButtonBorder{obj=this.brokerDisplay, relTo=this.brokerDisplay.icon, ofs=3, clr="grey"}
-				end
-				-- VoidStorage Transfer button
-				if this.frameID == "vault" then
-					if this.Money then
-						aObj:addButtonBorder{obj=this.Money.Button, relTo=this.Money.Button.Icon, ofs=3, clr="grey"}
-					else
-						aObj:addButtonBorder{obj=this.moneyFrame.Button, relTo=this.moneyFrame.Button.Icon, ofs=3, clr="grey"}
-					end
-				end
-				-- bag/tab buttons
-				if frame.bagGroup then
-					skinBagGrp(frame)
-				else
-					aObj:SecureHook(frame, "PlaceBagGroup", function(fObj)
-						if fObj.bagGroup then
-							skinBagGrp(fObj)
-							aObj:Unhook(frame, "PlaceBagGroup")
-						end
-					end)
-				end
-				-- bag buttons
-				local function skinBtns(igObj)
-					for _, btn in _G.ipairs(igObj.order) do
-						aObj:addButtonBorder{obj=btn, ibt=true, y2=-3}
-						updBtn(btn)
-						aObj:secureHook(btn, "Update", function(bObj)
-							updBtn(bObj)
-						end)
-					end
-				end
-				aObj:SecureHook(frame.ItemGroup, "Layout", function(igObj)
-					skinBtns(igObj)
+		function skinIGBtns(frame)
+			for _, btn in _G.ipairs(frame.buttons) do
+				aObj:addButtonBorder{obj=btn, ibt=true, y2=-3}
+				aObj:setBtnClr(btn, btn.info.quality)
+				aObj:secureHook(btn, "Update", function(bObj)
+					aObj:setBtnClr(bObj, bObj.info.quality)
 				end)
-				skinBtns(frame.ItemGroup)
+			end
+		end
+		function skinTGBtns(frame)
+			for _, btn in _G.ipairs(frame.buttons) do
+				aObj:addButtonBorder{obj=btn, ofs=4, clr="grey"}
+			end
+		end
+	end
+	local function skinFrame(frame, _)
+		if frame.sf then return end
+		aObj:SecureHookScript(frame, "OnShow", function(this)
+			aObj:skinObject("editbox", {obj=this.SearchBar})
+			aObj:skinObject("frame", {obj=this, kfs=true, cb=true, ofs=1})
+			if aObj.modBtnBs then
+				skinMenuBtns(this)
+
+				-- BagGroup buttons
+				aObj:SecureHook(this.BagButton, "New", function(bGrp, parent)
+					skinBGBtns(parent.frame.BagButton)
+				end)
+				skinBGBtns(this.BagButton)
+
+			    -- ItemGroup buttons
+				aObj:SecureHook(this.ItemGroup, "Layout", function(fObj)
+					skinIGBtns(fObj)
+				end)
+				skinIGBtns(this.ItemGroup)
+
+				-- TabGroup buttons
+				if this.TabGroup then
+					aObj:SecureHook(this.TabGroup, "Update", function(tGrp)
+						skinTGBtns(tGrp)
+					end)
+					skinTGBtns(this.TabGroup)
+				end
+
+				-- hook to handle Button & TabGroup toggles
+				aObj:SecureHook(this, "Layout", function(fObj)
+					skinMenuBtns(fObj)
+					if fObj:HasSidebar() then
+						skinTGBtns(fObj.TabGroup)
+					end
+
+				end)
+
 			end
 
 			aObj:Unhook(this, "OnShow")
 		end)
-		frame.skinned = true
+		aObj:checkShown(frame)
 	end
+
 	-- hook this to skin new frames
 	self:RawHook(_G.Bagnon.Frames, "New", function(this, id)
 		local frame = self.hooks[this].New(this, id)
-		if frame then
+		-- add a slight delay for tables to be populated
+		_G.C_Timer.After(0.05, function()
 			skinFrame(frame, id)
-			return frame
-		else
-			return
-		end
-	end)
-
-	-- skin the Search EditBox
-	self:RawHook(_G.Bagnon["SearchFrame"], "New", function(this, ...)
-		local eb = self.hooks[this].New(this, ...)
-		self:skinObject("editbox", {obj=eb, x1=4, y1=-2, x2=-4, y2=2})
-		return eb
-	end)
+		end)
+		return frame
+	end, true)
 
 end
 
--- Bagnon_GuildBank frame handled in above skinFrame function
--- Bagnon_VoidStorage frame handled in above skinFrame function
+aObj.lodAddons.Bagnon_Config = function(self) -- v 11.2.2
+	if self.initialized.Bagnon_Config then return end
+	self.initialized.Bagnon_Config = true
 
-aObj.lodAddons.Bagnon_Config = function(self) -- v 10.0.8
-
-	-- register callback to skin elements
-	self.RegisterCallback("Bagnon_Config", "IOFPanel_After_Skinning", function(_, panel)
-		local function skinKids(pObj)
-			for _, child in _G.ipairs{pObj:GetChildren()} do
-				if aObj:isDropDown(child) then
-					aObj:skinObject("dropdown", {obj=child, x2=-1})
-				elseif child:IsObjectType("Slider") then
-					aObj:skinObject("slider", {obj=child})
-				elseif child:IsObjectType("Frame") then
-					skinKids(child)
+	local function skinKids(frame)
+		for _, child in _G.ipairs_reverse{frame:GetChildren()} do
+			if child:GetName() then
+				if child:GetName():find("DropChoice") then
+					self:skinObject("dropdown", {obj=child, x2=-1})
+				elseif child:GetName():find("Slider") then
+					self:skinObject("slider", {obj=child})
 				end
+			elseif child:IsObjectType("Frame") then
+				skinKids(child)
 			end
+		end
+	end
+	self.RegisterCallback("Bagnon_Config", "SettingsPanel_DisplayCategory", function(this, panel)
+		if not panel:GetNumChildren() == 1
+		or not self:getChild(panel, 1)
+		or not self:getChild(panel, 1).Tag
+		or not self:getChild(panel, 1).Tag == "Bagnon."
+		then
+			return
 		end
 
-		if not aObj.isRtl then
-			if panel.name:find("Bagnon$")
-			or panel.parent
-			and panel.parent:find("Bagnon$")
-			then
-				skinKids(self:getChild(panel, 1)) -- options on sub panel
-			end
-		else
-			if _G.tostring(panel.name):find("Bagnon$")
-			or panel.parent
-			and panel.parent:find("Bagnon$")
-			then
-				skinKids(panel)
-			end
+		skinKids(self:getChild(panel, 1))
+
+	end)
+
+	self:SecureHookScript(_G.Bagnon.RuleEdit, "OnShow", function(fObj)
+
+		self:skinIconSelector(fObj)
+
+		fObj.Code:DisableDrawLayer("BACKGROUND")
+		self:skinObject("frame", {obj=fObj.Code, fb=true, ofs=8})
+		if self.modBtns then
+			self:skinStdButton{obj=fObj.BorderBox.DeleteButton}
+			self:skinStdButton{obj=fObj.BorderBox.ShareButton, sechk=true}
 		end
+
+		self:Unhook(fObj, "OnShow")
 	end)
 
 end
