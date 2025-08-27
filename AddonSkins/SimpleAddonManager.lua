38dd62d9103419ec1144759719815c61599cf1a5
Jon Hogg <hogg.jon+github@gmail.com>
2025-08-27 17:01:45 +0100

    Updated SimpleAddonManager skin, closes #238

--- AddonSkins/SimpleAddonManager.lua
+++ AddonSkins/SimpleAddonManager.lua
@@ -2,13 +2,12 @@ local _, aObj = ...
 if not aObj:isAddonEnabled("SimpleAddonManager") then return end
 local _G = _G
 
-aObj.addonsToSkin.SimpleAddonManager = function(self) -- v 1.34
+aObj.addonsToSkin.SimpleAddonManager = function(self) -- v 1.54
 
 	self:SecureHookScript(_G.SimpleAddonManager, "OnShow", function(this)
 		self:skinObject("dropdown", {obj=this.CharacterDropDown, x2=109})
-		self:skinObject("frame", {obj=this, kfs=true, cb=true})
-		self:skinObject("slider", {obj=this.ScrollFrame.ScrollBar})
 		self:skinObject("editbox", {obj=this.SearchBox, si=true})
+		self:skinObject("frame", {obj=this, kfs=true, cb=true, ofs=0})
 		if self.modBtns then
 			self:skinStdButton{obj=this.CancelButton}
 			self:skinStdButton{obj=this.OkButton}
@@ -19,11 +18,22 @@ aObj.addonsToSkin.SimpleAddonManager = function(self) -- v 1.34
 			self:skinStdButton{obj=this.ConfigButton}
 			self:skinStdButton{obj=this.CategoryButton}
 		end
-		if self.modChkBtns then
-			for _, cBtn in ipairs(this.ScrollFrame.buttons) do
-				self:skinCheckButton{obj=cBtn.EnabledButton}
+
+		self:SecureHookScript(this.AddonListFrame, "OnShow", function(fObj)
+			self:skinObject("slider", {obj=fObj.ScrollFrame.ScrollBar})
+
+			for _, btn in _G.ipairs(fObj.ScrollFrame.buttons) do
+				if self.modBtns then
+					self:skinExpandButton{obj=btn.ExpandOrCollapseButton, onSB=true}
+				end
+				if self.modChkBtns then
+					self:skinCheckButton{obj=btn.EnabledButton}
+				end
 			end
-		end
+
+			self:Unhook(fObj, "OnShow")
+		end)
+		self:checkShown(this.AddonListFrame)
 
 		self:SecureHookScript(this.CategoryFrame, "OnShow", function(fObj)
 			self:skinObject("slider", {obj=fObj.ScrollFrame.ScrollBar})
@@ -33,7 +43,7 @@ aObj.addonsToSkin.SimpleAddonManager = function(self) -- v 1.34
 				self:skinStdButton{obj=fObj.ClearSelectionButton}
 			end
 			if self.modChkBtns then
-				for _, cBtn in ipairs(fObj.ScrollFrame.buttons) do
+				for _, cBtn in _G.ipairs(fObj.ScrollFrame.buttons) do
 					self:skinCheckButton{obj=cBtn.EnabledButton}
 				end
 			end
@@ -41,6 +51,14 @@ aObj.addonsToSkin.SimpleAddonManager = function(self) -- v 1.34
 			self:Unhook(fObj, "OnShow")
 		end)
 		self:checkShown(this.CategoryFrame)
+
+		self:SecureHookScript(_G.SAM_ADDON_LIST_TOOLTIP, "OnShow", function(fObj)
+			self:skinObject("frame", {obj=fObj, kfs=true, ofs=0})
+
+			self:Unhook(fObj, "OnShow")
+		end)
+		self:checkShown(_G.SAM_ADDON_LIST_TOOLTIP)
+
 
 		self:Unhook(this, "OnShow")
 	end)
