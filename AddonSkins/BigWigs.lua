d519d801dd9c907d1130c35cf24f9e996e72fa84
Jon Hogg <hogg.jon+github@gmail.com>
2025-08-27 17:01:12 +0100

    Updated BigWigs skin, closes #237

--- AddonSkins/BigWigs.lua
+++ AddonSkins/BigWigs.lua
@@ -2,9 +2,7 @@ local _, aObj = ...
 if not aObj:isAddonEnabled("BigWigs") then return end
 local _G = _G
 
-if not aObj.isRtl then return end
-
-aObj.addonsToSkin.BigWigs = function(self) -- v 250.1
+aObj.addonsToSkin.BigWigs = function(self) -- v 394.3
 
 	-- skin BigWigs statusbar on the LFGDungeonReadyPopup
 	if _G.BigWigsLoader then
@@ -15,5 +13,36 @@ aObj.addonsToSkin.BigWigs = function(self) -- v 250.1
 			end
 		end)
 	end
+
+	local function skinKeystonesFrame(frame)
+		aObj:skinObject("tabs", {obj=frame, tabs=frame.Tabs, ignoreSize=true, lod=aObj.isTT and true, offsets={x1=nil, y1=nil, x2=nil, y2=-2}, track=false})
+		if aObj.isTT then
+			for _, tab in _G.pairs(frame.Tabs) do
+				aObj:SecureHookScript(tab, "OnClick", function(this)
+					for _, tab in _G.pairs(frame.Tabs) do
+						aObj:setInactiveTab(tab.sf)
+					end
+					aObj:setActiveTab(this.sf)
+				end)
+			end
+		end
+		aObj:skinObject("scrollbar", {obj=aObj:getChild(frame, 6).ScrollBar})
+		aObj:skinObject("frame", {obj=frame, kfs=true, cb=true})
+	end
+	-- find keystones frame
+	aObj.RegisterCallback("BigWigs", "UIParent_GetChildren", function(self, child, _)
+		if _G.Round(child:GetWidth()) == 350
+		and child:GetFrameStrata() == "DIALOG"
+		and child.Tabs
+		and #child.Tabs == 4
+		and child.GetSourceLocation
+		and child:GetSourceLocation()
+		and child:GetSourceLocation():find("BigWigs")
+		then
+			skinKeystonesFrame(child)
+			aObj.UnregisterCallback("BigWigs", "UIParent_GetChildren")
+		end
+	end)
+	aObj:scanChildren{obj=_G.UIParent, cbstr="UIParent_GetChildren"}
 
 end
