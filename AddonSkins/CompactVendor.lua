5a73f999c025bcbb901ee63991a2cef26f9f4eed
Jon Hogg <hogg.jon+github@gmail.com>
2026-02-01 17:39:52 +0000

    Updated CompactVendor skin, closes #270

--- AddonSkins/CompactVendor.lua
+++ AddonSkins/CompactVendor.lua
@@ -2,14 +2,29 @@ local _, aObj = ...
 if not aObj:isAddonEnabled("CompactVendor") then return end
 local _G = _G
 
-aObj.addonsToSkin.CompactVendor = function(self) -- v 10.0.2.221230
+aObj.addonsToSkin.CompactVendor = function(self) -- v 12.0.0.260124
 
 	self:SecureHookScript(_G.CompactVendorFrame, "OnShow", function(this)
 		self:skinObject("editbox", {obj=this.Search, si=true, y1=-4, y2=4})
-		self:skinObject("scrollbar", {obj=this.ScrollBar})
 		if self.modBtnBs then
 			self:addButtonBorder{obj=_G.CompactVendorFilterButton, ofs=-2, x1=1, clr="grey"}
 		end
+		-- colour the Item price white, fixes #270
+		self:skinObject("scrollbar", {obj=this.ScrollBar})
+		local function skinElement(...)
+			local _, element, elementData
+			if _G.select("#", ...) == 2 then
+				element, elementData = ...
+			else
+				_, element, elementData = ...
+			end
+			for _, frame in _G.pairs(element.Cost.Costs) do
+				if frame.costType == "Item" then
+					frame.Icon.Count:SetTextColor(1, 1, 1, 1)
+				end
+			end
+		end
+		_G.ScrollUtil.AddInitializedFrameCallback(this.ScrollBox, skinElement, aObj, true)
 
 		self:Unhook(this, "OnShow")
 	end)
