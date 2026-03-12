f1bfb9838b0c6c3cce784a7a9db50c48ed9371c9
Jon Hogg <hogg.jon+github@gmail.com>
2026-03-12 23:25:52 +0000

    Updated BaudBag skin, closes #300

--- AddonSkins/BaudBag.lua
+++ AddonSkins/BaudBag.lua
@@ -1,19 +1,16 @@
-local aName, aObj = ...
+local _, aObj = ...
 if not aObj:isAddonEnabled("BaudBag") then return end
 local _G = _G
 
-aObj.addonsToSkin.BaudBag = function(self) -- v 9.0.5
+aObj.addonsToSkin.BaudBag = function(self) -- v 12.0.0.2
 	if not self.db.profile.ContainerFrames.skin then return end
 
-	-- hook this to skin the bag frames
-	self:SecureHook(_G.BaudBag, "Container_Updated", function(this, bagSet, containerId)
-		-- aObj:Debug("BaudBag Container_Updated: [%s, %s, %s]", this, bagSet, containerId)
-		local bsTab
+	local bsTab
+	self:SecureHook(_G.BaudBag, "Container_Updated", function(this, bagSet, _)
 		for i = 1, this.Sets[bagSet.Id].MaxContainerNumber do
 			bsTab = this.Sets[bagSet.Id].Containers[i]
 			self:keepFontStrings(bsTab.Frame.Backdrop.Textures)
 			if not bsTab.Frame.Backdrop.sf then
-				-- self:addSkinFrame{obj=bsTab.Frame.Backdrop, ft="a", kfs=true, nb=true, ofs=0}
 				self:skinObject("frame", {obj=bsTab.Frame.Backdrop, kfs=true, ofs=0})
 				if self.modBtns then
 					self:skinCloseButton{obj=bsTab.Frame.CloseButton}
@@ -25,10 +22,7 @@ aObj.addonsToSkin.BaudBag = function(self) -- v 9.0.5
 	self:skinObject("frame", {obj=_G.BaudBagContainer1_1BagsFrame, ofs=0})
 	self:skinObject("frame", {obj=_G.BaudBagContainer2_1BagsFrame, ofs=0})
 	if self.modBtns then
-		self:skinStdButton{obj=_G.BaudBagBankSlotPurchaseButton}
-		self:SecureHook("BaudBagBankBags_UpdateContent", function(_)
-			self:clrBtnBdr(_G.BaudBagBankSlotPurchaseButton)
-		end)
+		self:skinStdButton{obj=_G.BaudBagBankSlotPurchaseButton, sechk=true}
 	end
 	if self.modBtnBs then
 		self:addButtonBorder{obj=_G.BaudBagContainer1_1.BagsButton, ofs=-1, x1=0, y1=-1}
@@ -44,40 +38,63 @@ aObj.addonsToSkin.BaudBag = function(self) -- v 9.0.5
 	self:SecureHook("BaudBagSearchFrame_ShowFrame", function(_)
 		self:keepFontStrings(_G.BaudBagSearchFrame.Backdrop.Textures)
 	end)
-	-- Options Frame
-	-- register callback to indicate already skinned
-	self.RegisterCallback("BaudBag", "IOFPanel_Before_Skinning", function(this, panel)
+
+	local function skinCB(cBtn)
+		aObj:skinCheckButton{obj=cBtn}
+		cBtn:SetWidth(26)
+	end
+	local function skinContainers()
+		for i = 1, 7 do
+			aObj:skinCheckButton{obj=_G["BaudBagOptionsJoinCheck" .. i]}
+			aObj:skinObject("frame", {obj=_G["BaudBagOptionsContainer" .. i], kfs=true, fb=true, ofs=0})
+		end
+	end
+	self.RegisterCallback("BaudBag", "SettingsPanel_DisplayCategory", function(_, panel)
 		if panel.name ~= "Baud Bag" then return end
-		self.iofSkinnedPanels[panel] = true
+		self.spSkinnedPanels[panel] = true
 
-		self:skinObject("dropdown", {obj=_G.BaudBagOptions.GroupContainer.SetSelection, x2=-11})
-		self:skinObject("editbox", {obj=_G.BaudBagOptions.GroupContainer.NameInput})
-		self:skinObject("dropdown", {obj=_G.BaudBagOptions.GroupContainer.BackgroundSelection, x2=-11})
-		self:skinObject("slider", {obj=_G.BaudBagOptionsGroupGlobal.Slider1})
-		self:skinObject("slider", {obj=_G.BaudBagOptionsGroupContainer.Slider1})
-		self:skinObject("slider", {obj=_G.BaudBagOptionsGroupContainer.Slider2})
+		self:skinObject("slider", {obj=panel.GroupGlobal.Slider1})
+		if self.modBtns then
+			self:skinStdButton{obj=panel.GroupGlobal.ResetPositionsButton}
+		end
 		if self.modChkBtns then
-			self:skinCheckButton{obj=_G.BaudBagOptions.GroupGlobal.CheckButton1}
-			self:skinCheckButton{obj=_G.BaudBagOptions.GroupGlobal.CheckButton2}
-			self:skinCheckButton{obj=_G.BaudBagOptions.GroupGlobal.CheckButton3}
-			self:skinCheckButton{obj=_G.BaudBagOptions.GroupGlobal.CheckButton4}
-			self:skinCheckButton{obj=_G.BaudBagOptions.GroupContainer.EnabledCheck}
-			self:skinCheckButton{obj=_G.BaudBagOptions.GroupContainer.CloseAllCheck}
-			self:skinCheckButton{obj=_G.BaudBagOptionsGroupContainerCheckButton1}
-			self:skinCheckButton{obj=_G.BaudBagOptionsGroupContainerCheckButton2}
-			self:skinCheckButton{obj=_G.BaudBagOptionsGroupContainerCheckButton3}
+			skinCB(panel.GroupGlobal.CheckButton1)
+			skinCB(panel.GroupGlobal.CheckButton2)
+			skinCB(panel.GroupGlobal.CheckButton3)
+			skinCB(panel.GroupGlobal.CheckButton4)
+			skinCB(panel.GroupGlobal.CheckButton5)
+			skinCB(panel.GroupGlobal.CheckButton6)
 		end
 
-		for i = 1, _G.NUM_BANKBAGSLOTS + 2 do
-			if self.modChkBtns then
-				if i ~= 1 then -- first bag doesn't have a checkbox
-					self:skinCheckButton{obj=_G["BaudBagOptionsJoinCheck" .. i]}
-				end
-			end
-			self:skinObject("frame", {obj=_G["BaudBagOptionsContainer" .. i], kfs=true, ofs=0})
+		self:skinObject("tabs", {obj=panel.GroupContainer, tabs=panel.GroupContainer.tabButtons, ignoreSize=true, lod=self.isTT and true, upwards=true, regions={4}, offsets={x1=6, y1=-6, x2=-6, y2=-6}, track=false, func=self.isTT and function(tab)
+				aObj:SecureHook(tab, "UpdateAtlas", function(tObj)
+					if tObj:IsSelected() then
+						aObj:setActiveTab(tObj.sf)
+					else
+						aObj:setInactiveTab(tObj.sf)
+					end
+				end)
+			end})
+		skinContainers()
+		self:skinObject("editbox", {obj=panel.GroupContainer.BagSet.NameInput})
+		self:skinObject("slider", {obj=panel.GroupContainer.BagSet.Slider1})
+		self:skinObject("slider", {obj=panel.GroupContainer.BagSet.Slider2})
+		self:skinObject("frame", {obj=panel.GroupContainer.BagSet, kfs=true, fb=true, ofs=0})
+
+		if self.modBtns then
+			self:skinStdButton{obj=panel.GroupContainer.BagSet.BackgroundSelection.Dropdown, ignoreHLTex=true, sechk=true, y1=1, y2=-1}
+			self:skinStdButton{obj=panel.GroupContainer.BagSet.ResetPositionButton}
+		end
+		if self.modChkBtns then
+			skinCB(panel.GroupContainer.BagSet.EnabledCheck)
+			self:moveObject{obj=panel.GroupContainer.BagSet.LinkedSet, x=50}
+			skinCB(panel.GroupContainer.BagSet.CloseAllCheck)
+			skinCB(panel.GroupContainer.BagSet.CheckButton1)
+			skinCB(panel.GroupContainer.BagSet.CheckButton2)
+			skinCB(panel.GroupContainer.BagSet.CheckButton3)
 		end
 
-		self.UnregisterCallback("BaudBag", "IOFPanel_Before_Skinning")
+		self.UnregisterCallback("BaudBag", "SettingsPanel_DisplayCategory")
 	end)
 
 end
