1b1e74bd900519deeb430dadab1c435973abca4e
Jon Hogg <hogg.jon+github@gmail.com>
2026-03-27 13:02:02 +0000

    Updated Version numbers

--- Core/SharedFuncs.lua
+++ Core/SharedFuncs.lua
@@ -5,14 +5,14 @@ local _G = _G
 local buildInfo = {
 	-- wow_classic_beta    = {"MOP Classic Beta",          "5.5.0",  62071},
 	wow_beta            	= {"Midnight Beta",             "12.0.1", 66220},
-	wow_classic_ptr     	= {"PTR (MoP Classic)",			"5.5.3",  66290},
+	wow_classic_ptr     	= {"PTR (MoP Classic)",			"5.5.3",  66382},
 	wow_classic_era_ptr 	= {"PTR (Classic Era)",         "2.5.5",  65295},
-	wow_ptr             	= {"PTR (Midnight)",			"12.0.0", 65614},
+	wow_ptr             	= {"PTR (Midnight 12.0.5)",		"12.0.5", 66591},
 	wow_ptr_x           	= {"PTR (Midnight 12.0.1)",     "12.0.1", 66220},
-	wow_classic_anniversary	= {"BC Anniversary", 			"2.5.5",  66265},
-	wow_classic_era     	= {"World of Warcraft Classic", "1.15.8", 66129},
-	wow_classic         	= {"Mists of Panderia Classic", "5.5.3",  66290},
-	wow                 	= {"World of Warcraft",         "12.0.1", 66337},
+	wow_classic_anniversary	= {"BC Anniversary", 			"2.5.5",  66567},
+	wow_classic_era     	= {"World of Warcraft Classic", "1.15.8", 66564},
+	wow_classic         	= {"Mists of Panderia Classic", "5.5.3",  66565},
+	wow                 	= {"World of Warcraft",         "12.0.1", 66666},
 	curr                	= {"curr", _G.GetBuildInfo()},
 }
 
