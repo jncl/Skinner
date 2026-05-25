2b91aa081b3c0c23377c29f98c726879a833aa81
Jon Hogg <hogg.jon+github@gmail.com>
2026-05-25 10:36:05 +0100

    Updated DeathRecap skin

--- Mainline/UIFrames.lua
+++ Mainline/UIFrames.lua
@@ -1174,7 +1174,7 @@ aObj.SetupMainline_UIFrames = function()
 			this:DisableDrawLayer("BORDER")
 			this.Background:SetTexture(nil)
 			_G.RaiseFrameLevelByTwo(this)
-			self:skinObject("frame", {obj=this, fType=ftype, kfs=true, ofs=-1, y1=-2})
+			self:skinObject("frame", {obj=this, fType=ftype, kfs=true, ofs=0, x2=1})
 			if self.modBtns then
 				self:skinCloseButton{obj=this.CloseXButton}
 				self:skinStdButton{obj=this.CloseButton}
