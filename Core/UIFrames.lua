97f71c1cbbb93ee37bca4b62f4f10a03e5dfc52f
Jon Hogg <hogg.jon+github@gmail.com>
2026-05-25 10:33:52 +0100

    Updated AlertFrames code

--- Core/UIFrames.lua
+++ Core/UIFrames.lua
@@ -135,7 +135,7 @@ aObj.blizzFrames[ftype].AlertFrames = function(self)
 		alertType["Achievement"].y2         = 12
 		alertType["EntitlementDelivered"]   = {ofs = -10}
 		alertType["GuildRename"]            = {ofs = -10}
-		alertType["HousingItemEarned"]		= {ofs = -8, ddl = {"background", "border"}, stn = {"Divider"}, icon = {tex = "Icon"}, sysType = "AlertFrameSystem"}
+		alertType["HousingItemEarned"]		= {ofs = -12, ddl = {"background", "border"}, stn = {"Divider"}, icon = {tex = "Icon"}, sysType = "AlertFrameSystem"}
 		alertType["InitiativeTaskComplete"]	= {ofs = -12, ddl = {"background", "border"}, stn = {"Divider"}, sysType = "AlertFrameSystem"} -- a.k.a. endeavour task completion alert
 		alertType["Loot"].icon              = {obj = "lootItem", stn = {"SpecRing"}, ib = true, tex =  "Icon"}
 		alertType["MonthlyActivity"]        = {ofs = 0, nt = {"Background"}, stc = "Unlocked", icon = {obj = "Icon", ddl = {"border", "overlay"}, tex ="Texture"}}
