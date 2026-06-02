00cc7e35738af90c8ea3c0e17ac2fede203d59c0
Jon Hogg <hogg.jon+github@gmail.com>
2026-06-02 19:32:01 +0100

    Updated WorldQuestTab skin, closes #340

---
+++ AddonSkins/LibKrowiWorldMapButtons.lua
@@ -0,0 +1,32 @@
+local _, aObj = ...
+local _G = _G
+-- This is a Library
+
+aObj.libsToSkin["Krowi_WorldMapButtons-1.4"] = function(self) -- v Krowi_WorldMapButtons-1.4, 9
+	if self.initialized.Krowi_WorldMapButtons then return end
+	self.initialized.Krowi_WorldMapButtons = true
+
+	local lKWMB = _G.LibStub:GetLibrary("Krowi_WorldMapButtons-1.4", true)
+
+	if lKWMB then
+		local function skinButton(oFrame)
+			oFrame:DisableDrawLayer("BACKGROUND")
+			oFrame.Border:SetTexture(nil)
+			if oFrame.ResetButton then
+				aObj.modUIBtns:skinCloseButton{obj=oFrame.ResetButton, noSkin=true}
+			end
+			if aObj.modBtns then
+				aObj:skinStdButton{obj=oFrame, ofs=-2, clr="gold"}
+			end
+		end
+		self:RawHook(lKWMB, "Add", function(this, ...)
+			local btn = self.hooks[this].Add(this, ...)
+			skinButton(btn)
+			return btn
+		end, true)
+		for _, btn in _G.pairs(lKWMB.Buttons) do
+			skinButton(btn)
+		end
+	end
+
+end
--- AddonSkins/WorldQuestTab.lua
+++ AddonSkins/WorldQuestTab.lua
@@ -2,99 +2,130 @@ local _, aObj = ...
 if not aObj:isAddonEnabled("WorldQuestTab") then return end
 local _G = _G
 
-aObj.addonsToSkin.WorldQuestTab = function(self) -- v 9.0.03
+aObj.addonsToSkin.WorldQuestTab = function(self) -- v 12.0.11
 
-	-- tabs added to QuestLogFrame
-	local tab
-	for _, tabName in _G.pairs{"Normal", "World", "Details"} do
-		tab = _G["WQT_Tab" .. tabName]
-		tab.TabBg:SetAlpha(0)
-		tab.Hider:SetAlpha(0)
-		-- use a button border to indicate the active tab
-		self.modUIBtns:addButtonBorder{obj=tab, relTo=tab.Icon, ofs=3} -- use module function here to force creation
-		self:clrBtnBdr(tab, "selected")
-		tab.Icon:SetSize(28, 28)
-		tab.sbb:SetShown(tab == _G.WQT_WorldQuestFrame.selectedTa)
-	end
-	tab = nil
-	-- hook this to manage the active tab
-	self:SecureHook(_G.WQT_WorldQuestFrame, "SelectTab", function(this, tab)
-		for _, tabName in _G.pairs{"Normal", "World", "Details"} do
-			_G["WQT_Tab" .. tabName].sbb:SetShown(_G["WQT_Tab" .. tabName] == tab)
+	local frame, btn
+	for _, name in _G.pairs{"OldTaxi", "Flight", "World"} do
+		frame = _G["WQT_" .. name .. "MapContainer"]
+		self:SecureHookScript(frame, "OnShow", function(this)
+			if _G.InCombatLockdown() then
+				self:add2Table(self.oocTab, {self.checkShown, {self, this}})
+				return
+			end
+
+			self:skinObject("frame", {obj=this, kfs=true})
+
+			self:Unhook(this, "OnShow")
+		end)
+		self:checkShown(frame)
+		btn = _G["WQT_" .. name .. "MapContainerButton"]
+		if self.modBtns
+		and btn
+		then
+			self:skinStdButton{obj=btn, ofs=0, y1=-4, y2=4}
 		end
+	end
+
+	-- tab on QuestLog
+	_G.WQT_QuestMapTab.Background:SetTexture(nil)
+	self:skinObject("button", {obj=_G.WQT_QuestMapTab, ofs=-1, x1=-1, y2=2})
+
+	self:SecureHookScript(_G.WQT_WorldQuestFrame, "OnShow", function(this)
+		if _G.InCombatLockdown() then
+			self:add2Table(self.oocTab, {self.checkShown, {self, this}})
+			return
+		end
+
+		this.ScrollFrame:DisableDrawLayer("BACKGROUND")
+		-- .TopBar
+			-- .ProgressBar
+		self:skinObject("ddbutton", {obj=this.ScrollFrame.TopBar.SortDropdown})
+		self:skinObject("ddbutton", {obj=this.ScrollFrame.TopBar.FilterDropdown, filter=true})
+		self:skinObject("editbox", {obj=this.ScrollFrame.TopBar.SearchBox, si=true})
+			-- .SearchToggle
+		-- SettingsButton
+		self:keepFontStrings(this.ScrollFrame.BorderFrame)
+		-- .BorderContainer
+			-- .FilterBar
+			-- .QuestScrollBox
+		self:skinObject("scrollbar", {obj=this.ScrollFrame.ScrollBar})
+
+		self:Unhook(this, "OnShow")
 	end)
+	self:checkShown(_G.WQT_WorldQuestFrame)
 
-	self:keepFontStrings(_G.WQT_QuestLogFiller)
+	self:SecureHookScript(_G.WQT_SettingsFrame, "OnShow", function(this)
+		if _G.InCombatLockdown() then
+			self:add2Table(self.oocTab, {self.checkShown, {self, this}})
+			return
+		end
 
-	_G.WQT_WorldQuestFrame:DisableDrawLayer("BACKGROUND")
-	self:skinObject("dropdown", {obj=_G.WQT_WorldQuestFrame.sortButton, x1=0, x2=-1, y2=-3})
-	self:skinObject("slider", {obj=_G.WQT_WorldQuestFrame.ScrollFrame.scrollBar, x1=4, y1=-2, x2=-4, y2=2})
-	self:keepFontStrings(_G.WQT_WorldQuestFrame.ScrollFrame.DetailFrame)
-	self:keepFontStrings(_G.WQT_OverlayFrame)
-	-- .CloseButton
-	self:keepFontStrings(_G.WQT_OverlayFrame.DetailFrame)
-	if self.modBtns then
-		self:skinStdButton{obj=_G.WQT_WorldQuestFrame.FilterButton}
-	end
-	if self.modBtnBs then
-		self:addButtonBorder{obj=_G.WQT_WorldQuestFrame.SettingsButton, ofs=1}
-	end
-	self:SecureHook(_G.WQT_WorldQuestFrame, "SetCustomEnabled", function(this, value)
-		self:checkDisabledDD(_G.WQT_WorldQuestFrame.sortButton, not _G.WQT_WorldQuestFrame.sortButton:IsEnabled())
-		if self.modBtns then
-			self:clrBtnBdr(this.FilterButton)
+		this:DisableDrawLayer("BACKGROUND")
+		self:keepFontStrings(this.BorderFrame)
+		self:skinObject("scrollbar", {obj=this.ScrollBar})
+		local function skinSettings(...)
+			local _, element, elementData
+			if _G.select("#", ...) == 2 then
+				element, elementData = ...
+			else
+				_, element, elementData = ...
+			end
+			if elementData.template == "WQT_SettingCategoryTemplate" then
+				aObj:removeRegions(element, {1, 2, 3, 5, 6, 7})
+				aObj:changeHdrExpandTex(element.BGRight)
+				-- force the change
+				if element.UpdateState then
+					element:UpdateState()
+				end
+			elseif elementData.template == "WQT_SettingSubCategoryTemplate" then
+				aObj:removeRegions(element, {1, 4})
+			elseif elementData.template == "WQT_SettingsQuestListPreviewTemplate" then
+				element.Background:SetTexture(nil)
+			-- "WQT_SettingsBaseTemplate"
+			-- "WQT_SettingsChildTemplate"
+			elseif elementData.template == "WQT_SettingCheckboxTemplate" then
+				if aObj.modChkBtns then
+					aObj:skinCheckButton{obj=element.CheckBox}
+				end
+			elseif elementData.template == "WQT_SettingSliderTemplate" then
+				aObj:skinObject("slider", {obj=element.SliderWithSteppers.Slider})
+				aObj:skinObject("editbox", {obj=element.TextBox})
+			elseif elementData.template == "WQT_SettingTextTemplate" then
+				_G.nop()
+			elseif elementData.template == "WQT_SettingColorTemplate" then
+				if aObj.modBtnBs then
+					aObj:addButtonBorder{obj=element.Picker, ofs=0}
+					aObj:addButtonBorder{obj=element.ResetButton, ofs=0}
+				end
+			elseif elementData.template == "WQT_SettingDropDownTemplate" then
+				aObj:skinObject("ddbutton", {obj=element.Dropdown})
+			-- "WQT_SettingFunctionButtonTemplate"
+			elseif elementData.template == "WQT_SettingButtonTemplate" then
+				if aObj.modBtns then
+					aObj:skinStdButton{obj=element.Button, schk=true}
+				end
+			elseif elementData.template == "WQT_SettingConfirmButtonTemplate" then
+				if aObj.modBtns then
+					aObj:skinStdButton{obj=element.Button, schk=true}
+					aObj:skinStdButton{obj=element.ButtonConfirm, schk=true}
+					aObj:skinStdButton{obj=element.ButtonDecline, schk=true}
+				end
+			elseif elementData.template == "WQT_SettingTextInputTemplate" then
+				aObj:skinObject("editbox", {obj=element.TextBox})
+			elseif elementData.template == "WQT_SettingSeparatorTemplate" then
+				_G.nop()
+			end
 		end
-		if self.modBtnBs then
-			self:clrBtnBdr(this.SettingsButton)
-		end
+		_G.ScrollUtil.AddInitializedFrameCallback(this.ScrollBox, skinSettings, aObj, true)
+
+		self:Unhook(this, "OnShow")
 	end)
+	self:checkShown(_G.WQT_SettingsFrame)
 
-	-- TODO: WQT_GroupSearch
-
-	-- Settings
-	self:skinObject("slider", {obj=_G.WQT_VersionFrame.scrollBar, x1=-6, x2=5})
-	self:skinObject("slider", {obj=_G.WQT_SettingsFrame.ScrollFrame.ScrollBar, x1=-6, x2=5})
-	for category in _G.WQT_SettingsFrame.categoryPool:EnumerateActive() do
-		category:DisableDrawLayer("BACKGROUND")
-		category:DisableDrawLayer("BORDER")
-	end
-	for template in _G.WQT_SettingsFrame.templatePools["WQT_SettingSliderTemplate"]:EnumerateActive() do
-		self:skinObject("slider", {obj=template.Slider})
-		self:skinObject("editbox", {obj=template.TextBox})
-	end
-	for template in _G.WQT_SettingsFrame.templatePools["WQT_SettingDropDownTemplate"]:EnumerateActive() do
-		self:skinObject("dropdown", {obj=template.DropDown, x1=0, x2=-1, y2=-3})
-	end
-	for template in _G.WQT_SettingsFrame.templatePools["WQT_SettingTextInputTemplate"]:EnumerateActive() do
-		self:skinObject("editbox", {obj=template.TextBox})
-	end
-	if self.modBtns then
-		for template in _G.WQT_SettingsFrame.templatePools["WQT_SettingButtonTemplate"]:EnumerateActive() do
-			self:skinStdButton{obj=template.Button}
-			self:SecureHook(template, "SetDisabled", function(this, value)
-				self:clrBtnBdr(this.Button)
-			end)
-		end
-		for template in _G.WQT_SettingsFrame.templatePools["WQT_SettingConfirmButtonTemplate"]:EnumerateActive() do
-			self:skinStdButton{obj=template.Button}
-			self:SecureHook(template, "SetDisabled", function(this, value)
-				self:clrBtnBdr(this.Button)
-			end)
-			self:skinStdButton{obj=template.ButtonConfirm}
-			self:skinStdButton{obj=template.ButtonDecline}
-		end
-	end
-	if self.modChkBtns then
-		for template in _G.WQT_SettingsFrame.templatePools["WQT_SettingCheckboxTemplate"]:EnumerateActive() do
-			self:skinCheckButton{obj=template.CheckBox}
-		end
-	end
-
-	self:skinObject("frame", {obj=_G.WQT_OldTaxiMapContainer, kfs=true, ri=true, ofs=0})
-	_G.WQT_FlightMapContainerButton.Border:SetTexture(nil) -- ring texture
-	self:skinObject("frame", {obj=_G.WQT_FlightMapContainer, kfs=true, ri=true, ofs=0})
-	_G.WQT_FlightMapContainerButton.Border:SetTexture(nil) -- ring texture
-	self:skinObject("frame", {obj=_G.WQT_WorldMapContainer, kfs=true, ri=true, ofs=0})
-	_G.WQT_WorldMapContainerButton.Border:SetTexture(nil) -- ring texture
+	_G.RunNextFrame(function()
+	    self:add2Table(self.ttList, "a", _G.WQT_GameTooltip)
+	    self:add2Table(self.ttList, "a", _G.WQT_ShoppingTooltip1)
+	    self:add2Table(self.ttList, "a", _G.WQT_ShoppingTooltip2)
+	end)
 
 end
