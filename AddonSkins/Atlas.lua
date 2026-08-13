a5ae991c7a72c3c7a2d2ccf06565d096e9451cbf
Jon Hogg <hogg.jon+github@gmail.com>
2026-08-13 09:47:05 +0100

    Updated Atlas skin, closes #348

--- AddonSkins/Atlas.lua
+++ AddonSkins/Atlas.lua
@@ -2,80 +2,63 @@ local _, aObj = ...
 if not aObj:isAddonEnabled("Atlas") then return end
 local _G = _G
 
-aObj.addonsToSkin.Atlas = function(self) -- v1.52.05/v1.49.01
+aObj.addonsToSkin.Atlas = function(self) -- v3.15.9
 
 	self:SecureHookScript(_G.AtlasFrame, "OnShow", function(this)
-		self:skinObject("dropdown", {obj=_G.AtlasFrameDropDownType})
-		self:skinObject("dropdown", {obj=_G.AtlasFrameDropDown})
-		self:removeRegions(this, {1, 2, 3, 4})
-		self:skinObject("editbox", {obj=_G.AtlasSearchEditBox, y1=-4, y2=4})
-		self:skinObject("slider", {obj=_G.AtlasScrollBar.ScrollBar})
-		self:skinObject("frame", {obj=this, cb=true, x1=10, y1=-10})
+		self:skinObject("ddbutton", {obj=_G.AtlasFrameDropDownType})
+		self:skinObject("ddbutton", {obj=_G.AtlasFrameDropDown})
+		self:removeInset(this.MapFrame)
+		-- make sure map texture is visible
+		this.MapFrame:SetFrameStrata("HIGH")
+		_G.AtlasFrameCollapseButton:SetFrameStrata("HIGH")
+		self:removeInset(_G.AtlasFrameTopInset)
+		self:removeInset(_G.AtlasFrameBottomInset)
+		self:skinObject("scrollbar", {obj=_G.AtlasFrameBottomInset.ScrollBox.registeredScrollBar, x1=2, x2=4})
+		self:skinObject("ddbutton", {obj=_G.AtlasFrameSwitchDropdown})
+		self:skinObject("editbox", {obj=_G.AtlasSearchEditBox, si=true, y1=-4, y2=4})
+		self:skinObject("frame", {obj=this, kfs=true, cb=true, y1=5})
 		if self.modBtns then
 			_G.AtlasFrameCloseButton:SetSize(28, 28)
 			-- TODO: skin LockButton
 			-- self:skinStdButton{obj=_G.AtlasFrameLockButton}
 			self:skinStdButton{obj=_G.AtlasFrameOptionsButton}
-			self:skinStdButton{obj=_G.AtlasSwitchButton}
-			self:skinStdButton{obj=_G.AtlasSearchButton}
-			self:skinStdButton{obj=_G.AtlasSearchClearButton}
-			-- .AdventureJournalMap
-			-- .AdventureJournal
-			-- .AtlasLoot
-		end
-		if self.modBtnBs then
-			self:addButtonBorder{obj=this.NextMap, ofs=-2, x1=1, clr="gold"}
-			self:addButtonBorder{obj=this.PrevMap, ofs=-2, x1=1, clr="gold"}
-			self:skinOtherButton{obj=_G.AtlasFrameSizeUpButton, font=self.fontS, text=self.nearrow}
 			self:skinOtherButton{obj=_G.AtlasFrameCollapseButton, font=self.fontS, text=self.larrow}
-		end
-
-		self:Unhook(this, "OnShow")
-	end)
-
-	self:SecureHookScript(_G.AtlasFrameLarge, "OnShow", function(this)
-		self:skinObject("dropdown", {obj=_G.AtlasFrameLargeDropDownType})
-		self:skinObject("dropdown", {obj=_G.AtlasFrameLargeDropDown})
-		self:removeRegions(this, {1, 2, 3, 4, 5, 6})
-		-- .ZoneName
-		-- BossButtonFrame
-		self:skinObject("frame", {obj=this, cb=true, x1=10, y1=-10})
-		if self.modBtns then
-			-- TODO: skin LockButton
-			-- self:skinStdButton{obj=_G.AtlasFrameLargeLockButton}
-			self:skinStdButton{obj=_G.AtlasFrameLargeOptionsButton}
-			-- .AdventureJournalMap
-			-- .AdventureJournal
-			-- .AtlasLoot
+			self:skinStdButton{obj=_G.AtlasFrameSwitchButton}
 		end
 		if self.modBtnBs then
-			self:addButtonBorder{obj=this.NextMap, ofs=-2, x1=1, clr="gold"}
-			self:addButtonBorder{obj=this.PrevMap, ofs=-2, x1=1, clr="gold"}
-			self:skinOtherButton{obj=_G.AtlasFrameLargeSizeDownButton, font=self.fontS, text=self.swarrow}
+			self:addButtonBorder{obj=_G.AtlasFrameLFGButton, clr="gold", ofs=-1, x1=0}
+			self:addButtonBorder{obj=_G.AtlasFramePrevNextContainer.NextMap, ofs=-2, x1=1, clr="gold", schk=true}
+			self:addButtonBorder{obj=_G.AtlasFramePrevNextContainer.PrevMap, ofs=-2, x1=1, clr="gold", schk=true}
+			self:addButtonBorder{obj=this.AdventureJournalMap}
+			self:addButtonBorder{obj=this.AdventureJournal}
+			self:addButtonBorder{obj=this.AtlasLoot}
 		end
 
 		self:Unhook(this, "OnShow")
 	end)
 
 	self:SecureHookScript(_G.AtlasFrameSmall, "OnShow", function(this)
-		self:skinObject("dropdown", {obj=_G.AtlasFrameSmallDropDownType})
-		self:skinObject("dropdown", {obj=_G.AtlasFrameSmallDropDown})
-		self:removeRegions(this, {1, 2, 3, 4})
-		-- .ZoneName
-		self:skinObject("frame", {obj=this, cb=true, x1=10, y1=-10})
+		self:skinObject("ddbutton", {obj=_G.AtlasFrameSmallDropDownType})
+		self:skinObject("ddbutton", {obj=_G.AtlasFrameSmallDropDown})
+		self:removeInset(this.MapFrame)
+		-- make sure map texture is visible
+		this.MapFrame:SetFrameStrata("HIGH")
+		_G.AtlasFrameSmallExpandButton:SetFrameStrata("HIGH")
+		self:skinObject("ddbutton", {obj=_G.AtlasFrameSmallSwitchDropdown})
+		self:skinObject("frame", {obj=this, kfs=true, cb=true--[[, x1=10, y1=-10]]})
 		if self.modBtns then
 			-- TODO: skin LockButton
 			-- self:skinStdButton{obj=_G.AtlasFrameSmallLockButton}
 			self:skinStdButton{obj=_G.AtlasFrameSmallOptionsButton}
-			-- .AdventureJournalMap
-			-- .AdventureJournal
-			-- .AtlasLoot
+			self:skinOtherButton{obj=_G.AtlasFrameSmallExpandButton, font=self.fontS, text=self.rarrow}
+			self:skinStdButton{obj=_G.AtlasFrameSmallSwitchButton}
 		end
 		if self.modBtnBs then
-			self:addButtonBorder{obj=this.NextMap, ofs=-2, x1=1, clr="gold"}
-			self:addButtonBorder{obj=this.PrevMap, ofs=-2, x1=1, clr="gold"}
-			self:skinOtherButton{obj=_G.AtlasFrameSmallExpandButton, font=self.fontS, text=self.rarrow}
-			self:skinOtherButton{obj=_G.AtlasFrameSmallSizeUpButton, font=self.fontS, text=self.nearrow}
+			self:addButtonBorder{obj=_G.AtlasFrameSmallPrevNextContainer.NextMap, ofs=-2, x1=1, clr="gold", schk=true}
+			self:addButtonBorder{obj=_G.AtlasFrameSmallPrevNextContainer.PrevMap, ofs=-2, x1=1, clr="gold", schk=true}
+			self:addButtonBorder{obj=this.AdventureJournalMap}
+			self:addButtonBorder{obj=this.AdventureJournal}
+			self:addButtonBorder{obj=this.AtlasLoot}
 		end
 
 		self:Unhook(this, "OnShow")
@@ -93,18 +76,10 @@ aObj.addonsToSkin.Atlas = function(self) -- v1.52.05/v1.49.01
 		end)
 	end
 
-	if self.isMnln then
-		_G.Krowi_WorldMapButtons1.Background:SetTexture(nil)
-		_G.Krowi_WorldMapButtons1.Border:SetTexture(nil)
-		if self.modBtns then
-			self:skinStdButton{obj=_G.Krowi_WorldMapButtons1, ofs=-3, clr="gold"}
-		end
-	else
-		if _G.Atlas.WorldMap
-		and _G.AtlasToggleFromWorldMap
-		then
-			_G.AtlasToggleFromWorldMap.Border:SetTexture(nil)
-		end
+	if _G.Atlas.WorldMap
+	and _G.AtlasToggleFromWorldMap
+	then
+		_G.AtlasToggleFromWorldMap.Border:SetTexture(nil)
 	end
 
 end
